b7b911a148f77fe74b48e0e2165dc7a387b071f2
Chely Ho <adept_inept@yahoo.com>
2018-04-20 04:53:09 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :user_id
+    redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def show
     require_login
-    @user = User.find_by(params[:id])
+    @user = User.find(params[:id])
   end
 
   private
