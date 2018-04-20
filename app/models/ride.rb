09d7542cd1eb69e15dbbff91d9c9f0c9b724e687
Chely Ho <adept_inept@yahoo.com>
2018-04-20 01:43:21 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,47 +3,20 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-    user_has_enough_tickets, user_is_tall_enough = check_if_user_meets_the_requirements
-    if user_has_enough_tickets && user_is_tall_enough
-      start_ride
-    elsif user_is_tall_enough && !user_has_enough_tickets
-      "Sorry. " + not_enough_tickets
-    elsif user_has_enough_tickets && !user_is_tall_enough
-      "Sorry. " + not_tall_enough
+    if allowed?
+      user.tickets -= attraction.tickets
+      user.nausea += attraction.nausea_rating
+      user.happiness += attraction.happiness_rating
+      user.save
     else
-      "Sorry. " + not_enough_tickets + " " + not_tall_enough
+      @errors.join(" ")
     end
   end
 
-  def check_if_user_meets_the_requirements
-    user_has_enough_tickets, user_is_tall_enough = false
-    if self.user.tickets >= self.attraction.tickets
-      user_has_enough_tickets = true
-    end
-    if self.user.height >= self.attraction.min_height
-      user_is_tall_enough = true
-    end
-    return [user_has_enough_tickets, user_is_tall_enough]
+  def allowed?
+    @errors = ["Sorry."]
+    @errors << "You do not have enough tickets to ride the #{attraction.name}." if user.tickets < attraction.tickets
+    @errors << "You are not tall enough to ride the Roller Coaster." if user.height < attraction.min_height
+    @errors.length <= 1 ? true : false
   end
-
-  def start_ride
-    new_happiness = self.user.happiness + self.attraction.happiness_rating
-    new_nausea = self.user.nausea + self.attraction.nausea_rating
-    new_tickes_count =  self.user.tickets - self.attraction.tickets
-    self.user.update(
-      :happiness => new_happiness,
-      :nausea => new_nausea,
-      :tickets => new_tickes_count
-    )
-    "Thanks for riding the #{self.attraction.name}!"
-  end
-
-  def not_enough_tickets
-    "You do not have enough tickets to ride the #{self.attraction.name}."
-  end
-
-  def not_tall_enough
-    "You are not tall enough to ride the #{self.attraction.name}."
-  end
-
 end
--- db/migrate/20180420005223_create_attractions.rb
+++ db/migrate/20180420005223_create_attractions.rb
@@ -1,4 +1,4 @@
-class CreateAttractions < ActiveRecord::Migration[5.1]
+class CreateAttractions < ActiveRecord::Migration[5.2]
   def change
     create_table :attractions do |t|
       t.string :name
