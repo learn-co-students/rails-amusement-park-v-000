3016cfc203e573e063c301c19d4b5ade49db93c6
Chely Ho <adept_inept@yahoo.com>
2018-04-20 05:00:19 +0000

    Done.

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,4 @@
+class AttractionsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'sessions#new'
-  resources :users, only: [:new, :create, :show]
-
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create, :show]
+  resources :attractions, only: [:index]
 end
