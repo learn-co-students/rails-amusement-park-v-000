c873b93490dad7c0542968852e13812252ea39d0
github emanao <github emanao@me.com>
2018-04-23 13:47:55 -0400

    Update nav links depending on the current user status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :log_in?
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
     root "static_pages#home"
     resources :users, only: [:new, :create, :edit, :update, :show]
     get "/signin" => "sessions#new"
-    post "sessions" => "sessions#create"  
+    post "/sessions" => "sessions#create"  
+    get "/logout" => "sessions#destroy"
 end
