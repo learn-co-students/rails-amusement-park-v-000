100da85db69cf4468e16aa93d6ffeb7a2f0d4417
github emanao <github emanao@me.com>
2018-04-23 14:27:11 -0400

    User can signup and signout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
         end
     end
     def show
-       @user =  User.find_by(params[:id])
+        @user =  User.find_by(id: params[:id])
     end
     private
     def user_params
