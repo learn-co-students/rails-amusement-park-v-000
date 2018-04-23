4b884c109e6f1e97dc2477e8bf48b6443af9d24f
github emanao <github emanao@me.com>
2018-04-23 11:43:41 -0400

    successfully logs in as non-admin and on log in, successfully adds a session hash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,15 @@
 class SessionsController < ApplicationController
     def new
+        @user = User.new
+    end
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
     end
     def destroy
         session.clear
