184b6b686de83613ba2ae751a37b8defeaf0f584
github emanao <github emanao@me.com>
2018-04-24 11:34:11 -0400

    Add migrations and assocciations for users, rides and attractions

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,5 @@
 class Attraction < ApplicationRecord
+    has_many :users
+    has_many :rides, through: :users
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_22_163559) do
+ActiveRecord::Schema.define(version: 2018_04_24_142345) do
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "name"
+    t.integer "tickets"
+    t.integer "nausea_rating"
+    t.integer "happiness_rating"
+    t.integer "min_height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "attraction_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,7 +35,7 @@ ActiveRecord::Schema.define(version: 2018_04_22_163559) do
     t.integer "happiness"
     t.integer "nausea"
     t.integer "tickets"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
