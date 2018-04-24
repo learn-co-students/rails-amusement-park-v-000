0b315edaa0e16a1fd06e6ee8f5baab87c7c705c4
github emanao <github emanao@me.com>
2018-04-24 11:35:31 -0400

    Set admin default value to false

--- db/migrate/20180422163559_create_users.rb
+++ db/migrate/20180422163559_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.integer :happiness
       t.integer :nausea
       t.integer :tickets
-      t.boolean :admin
+      t.boolean :admin, :default => false 
       t.string :password_digest
 
       t.timestamps
