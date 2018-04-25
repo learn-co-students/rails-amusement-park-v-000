46f29e592a52ba5a30f422d8dbd0bd2b23dd5344
github emanao <github emanao@me.com>
2018-04-24 23:34:30 -0400

    Add new attraction action, view and link

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,6 +4,13 @@ class AttractionsController < ApplicationController
         @attractions = Attraction.all
         @user= current_user
     end
+    def new
+        @attraction = Attraction.new
+    end
+    def create
+        @attraction = Attraction.create(attraction_params)
+        redirect_to attraction_path(@attraction)
+    end
     def show
         @user= current_user
     end
@@ -11,6 +18,13 @@ class AttractionsController < ApplicationController
     private
     def set_attraction
         @attraction = Attraction.find_by(id: params[:id])
+    end
+    def attraction_params
+        params.require(:attraction).permit(:name, 
+                                            :min_height,
+                                            :happiness_rating,
+                                            :nausea_rating,
+                                            :tickets)
     end
 
 end
