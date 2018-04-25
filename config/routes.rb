208218b39a6bdcbde09f759dc48cc26196623663
github emanao <github emanao@me.com>
2018-04-24 21:44:20 -0400

    Add rides controller and create action and link to the action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :attractions
     root "static_pages#home"
     resources :users, only: [:new, :create, :edit, :update, :show]
     get "/signin" => "sessions#new"
     post "/sessions" => "sessions#create"  
     get "/logout" => "sessions#destroy"
+    resources :attractions
+    post "/rides" => "rides#create"
+
 end
