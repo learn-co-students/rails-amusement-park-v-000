aed5bf8861025d1fa10623fda78511dc5d849d91
badlychadly <chadmontoya21@yahoo.com>
2018-05-02 17:11:42 -0400

    Done.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,10 +5,12 @@ class SessionController < ApplicationController
   end
 
   def create
-    if @user = User.find_by(name:params[:user][:name])
+    @user = User.find_by(name:params[:user][:name])
+    if @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
+      flash[:error] = "invalid Password"
       render 'new'
     end
   end
