c656518f48ed9cad692d10ef51d81f314afc0a5c
github username <github email address>
2018-05-03 01:37:37 -0400

    Done.

--- db/migrate/20180503052244_change_column_to_password_digest_from_password.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToPasswordDigestFromPassword < ActiveRecord::Migration[5.2]
-  change_table :users do |t|
-    t.change :password_digest, :string
-  end
-end
--- db/migrate/20180503052911_change_column_name.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :users, :password, :password_digest
-  end
-end
