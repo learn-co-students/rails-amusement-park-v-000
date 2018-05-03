1de7ba0d2fbaceee06a006a3d2b5b7babc963830
github username <github email address>
2018-05-03 01:48:59 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_03_051557) do
+ActiveRecord::Schema.define(version: 2018_05_03_054502) do
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "name"
+    t.integer "tickets"
+    t.integer "nausea_rating"
+    t.integer "happiness_rating"
+    t.integer "min_height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.integer "nausea"
+    t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
-    t.integer "happiness"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
