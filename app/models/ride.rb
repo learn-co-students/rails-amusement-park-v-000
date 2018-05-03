dc3aec584002a2ef041f0f366cf420eee539897b
github username <github email address>
2018-05-03 02:06:23 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,7 @@
-class Ride < ApplicationRecord
+class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  validates :user_id, presence: true
+  validates :attraction_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+
+  has_many :rides
 end
