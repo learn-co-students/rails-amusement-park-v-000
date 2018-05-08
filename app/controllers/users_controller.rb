9c2815ef8f0ccb0cc30c5544b9a87403ffbe2817
MargaritaKhodzka <mkhodzka@gmail.com>
2018-05-08 18:47:05 -0400

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,44 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        session[:user_id] = @user.id
+        format.html {redirect_to user_path(@user)}
+      else
+        format.html{render :new}
+      end
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html {redirect_to @user}
+      else
+        format.html {render :edit}
+      end
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :height, :tickets, :happiness, :nausea, :admin)
+  end
 end
