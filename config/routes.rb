df8eae585e06dbb6621006e174251b3d44de7215
Krhorsch <krhorsch@yahoo.com>
2018-05-10 10:07:06 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+  root to: 'rides#index'
+  resources :users, only: [:new, :create, :show]
+  resources :attractions
+  resources :rides, only: [:show, :new, :create]
+  get '/signin', to: "users#signin"
+  post '/signin', to: "users#signedin"
+  get '/logout', to: "users#logout"
 end
