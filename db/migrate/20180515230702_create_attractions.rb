95cad4dce8cdde942ee848532395a68d1b7a22cf
Sean Oughton <seanoughton@gmail.com>
2018-05-15 23:08:18 +0000

    Done.

---
+++ db/migrate/20180515230702_create_attractions.rb
@@ -0,0 +1,6 @@
+class CreateAttractions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :attractions do |t|
+    end
+  end
+end
---
+++ db/migrate/20180515230709_create_rides.rb
@@ -0,0 +1,6 @@
+class CreateRides < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rides do |t|
+    end
+  end
+end
