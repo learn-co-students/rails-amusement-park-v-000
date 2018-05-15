10f667d542073dfd33191dae85a01bca4f7f3b3b
Sean Oughton <seanoughton@gmail.com>
2018-05-15 23:15:57 +0000

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,4 @@
 class Attraction < ApplicationRecord
+  has_many :rides
+  has_many_through :users, through: :rides
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  has_many :rides
+  has_many_through :attractions, through: :rides
   has_secure_password
 end
