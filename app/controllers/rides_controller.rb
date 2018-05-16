e44ee76d47c9357d873c278d111a488fea64cc83
Sean Oughton <seanoughton@gmail.com>
2018-05-16 23:02:01 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,5 +1,13 @@
 class RidesController < ApplicationController
   def create
-    byebug
+    @ride = Ride.create(ride_params)
+    @user = User.find(current_user)
+    redirect_to @user
+  end
+
+  private
+
+  def ride_params
+    params.require(:ride).permit(:user_id, :attraction_id)
   end
 end
