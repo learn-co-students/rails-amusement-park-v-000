c6a1573558a8da1948ed1e67d614978d39a136e5
Sean Oughton <seanoughton@gmail.com>
2018-05-16 18:21:52 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,13 @@ class UsersController < ApplicationController
 
   def create
     byebug
+    User.create(user_params)
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :attractions
   resources :rides
 
+  root 'users#new'
+
 end
