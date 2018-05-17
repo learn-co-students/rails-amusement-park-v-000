7bff9b12619141a780e936fd59f04b394a42178c
Sean Oughton <seanoughton@gmail.com>
2018-05-17 18:36:47 +0000

    Done.

--- app/helpers/attractions_helper.rb
+++ app/helpers/attractions_helper.rb
@@ -1,6 +1,21 @@
 module AttractionsHelper
 
-  def not_admin_link(user)
-      link_to_if(!@user.admin, "Go on #{attraction.name}", attraction_path(attraction) )
+  def not_admin_link(user,attraction)
+    if !user.admin
+      link_to "Go on #{attraction.name}", attraction_path(attraction) 
+    end
+  end
+
+  def admin_link(user,attraction)
+    if user.admin 
+      link_to "Show #{attraction.name}", attraction_path(attraction) 
+    end
+  end
+
+  def new_attraction_link(user)
+    if user.admin 
+      link_to "New Attraction", { controller: "attractions", action: "new" }
+    end
+    
   end
 end
