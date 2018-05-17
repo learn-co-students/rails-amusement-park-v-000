75ddfdcc48048840a67d19370950677f381bd4d7
Sean Oughton <seanoughton@gmail.com>
2018-05-17 21:29:48 +0000

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,15 +1,4 @@
 class Attraction < ApplicationRecord
   has_many :rides
   has_many :users, through: :rides
-
-  def tall_enough?(user)
-    true unless user.height < self.min_height
-  end
-
-  def tickets_enough?(user)
-    true unless user.tickets < self.tickets
-  end
-
-  
-
 end
