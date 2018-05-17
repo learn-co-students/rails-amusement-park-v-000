0cb09d77fbe177b838502823f2072819497d0ea0
Sean Oughton <seanoughton@gmail.com>
2018-05-17 18:03:09 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,15 +2,26 @@ class AttractionsController < ApplicationController
 
   def index
     @attractions = Attraction.all
+    @user = User.find(current_user)
   end
 
   def new 
-    byebug
+    
+  end
+
+  def create
+    #what if there is already an attraction with that name?
+    @attraction = Attraction.create(attraction_params)
+    redirect_to @attraction
   end
 
   def show
     @attraction = Attraction.find(params[:id])
     @user = User.find(current_user)
+  end
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 
 
