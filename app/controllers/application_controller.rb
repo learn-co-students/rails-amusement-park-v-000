19b095ed2ad12454f6e7cf5e908bc04a3a29adb2
Joanne Lee <vernapii@gmail.com>
2018-05-22 14:59:35 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def logged_in?
+    session.include?(:user_id)
+  end
+  def current_user
+    User.find(session[:user_id])
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def show
+    if !logged_in?
+      redirect_to root_url
+    else
+      @user = User.find(params[:id])
+    end
+  end
+
+
+  def new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to signup_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @attraction = Attraction.find(params[:attraction_id])
+
+    ride = Ride.create(user: @user, attraction: @attraction)
+    flash[:message] = ride.take_ride
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
+  end
+end
