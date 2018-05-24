c025919c04babec5f6ecbc73bd0dc1c4fed82d3c
Sam Kalum <stkalum@gmail.com>
2018-05-24 13:52:26 -0400

    Pass models tests

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,38 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    if enough_tickets? && tall_enough?
+      ride_ride
+    else
+      error_message = ""
+      if !enough_tickets?
+        error_message << " You do not have enough tickets to ride the #{self.attraction.name}."
+      end
+      if !tall_enough?
+        error_message << " You are not tall enough to ride the #{self.attraction.name}."
+      end
+
+      "Sorry." + error_message
+    end
+  end
+
+  private
+
+  def enough_tickets?
+    self.user.tickets >= self.attraction.tickets
+  end
+
+  def tall_enough?
+    self.user.height >= self.attraction.min_height
+  end
+
+  def ride_ride
+    self.user.tickets -= self.attraction.tickets
+    self.user.nausea += self.attraction.nausea_rating
+    self.user.happiness += self.attraction.happiness_rating
+    self.user.save
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_secure_password
+
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    (self.happiness - self.nausea) > 0 ? "happy" : "sad"
+  end
+
 end
