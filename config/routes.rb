ba19cd8aa0f409f8da80237f59460dd12708f11b
Sam Kalum <stkalum@gmail.com>
2018-05-25 12:29:11 -0400

    Pass all User Signout tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
-  post '/signout', to: 'sessions#destroy'
+  delete '/signout', to: 'sessions#destroy'
 
   resources :users
   resources :attractions
