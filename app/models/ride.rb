272698d30a155c7a83f4bc8808cfb1c999d91167
Emily Howard <howard.emilya@gmail.com>
2018-05-25 20:48:48 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,16 @@
 class Ride < ApplicationRecord
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    if self.user.height < self.attraction.min_height && self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+      "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    else
+      self.user.update(tickets: self.user.tickets - self.attraction.tickets, nausea: self.user.nausea + self.attraction.nausea_rating, happiness: self.user.happiness + self.attraction.happiness_rating)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ApplicationRecord
   has_secure_password
   has_many :rides
   has_many :attractions, through: :rides
+
+  def mood
+    if self.nausea && self.happiness
+      if self.nausea > self.happiness
+        "sad"
+      else
+        "happy"
+      end
+    end
+  end
 end
