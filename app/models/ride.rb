2544d7a8c6b48272d029fb24afca1021daf39e9d
miriam <miriam5000@gmail.com>
2018-05-25 21:59:10 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,16 @@
 class Ride < ActiveRecord::Base
+    belongs_to :attraction
+    belongs_to :user
+    # is this the same as belongs to 1 attraction and 1 user? 
+    # should it be has_one? 
+    validates :user_id, attraction_id, presence: true
+
+    # def take_ride 
+    #   if self.t
+    # else
+    #     Sorry. You do not have enough tickets to ride the #{attraction.name}.
+    # end 
+    # end
+
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    validates :name, :password, :nausea, :happiness, :tickets, :height,   presence: true
+
+    def mood
+        if nausea > happiness
+            mood = "sad"
+        else 
+            mood = "happy"
+            # what about equal? 
+        end
+    end
 end
