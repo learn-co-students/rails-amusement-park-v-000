f5c4622fd62097bc45f045903b5cfc821a7d1053
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-29 16:22:37 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if self.nausea > self.happiness
+      mood = "sad"
+    elsif self.nausea < self.happiness
+      mood = "happy"
+    end
+    mood
+  end
+
 end
