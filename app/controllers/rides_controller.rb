7d2db5c254b6749192818b6b2a1468e9eeaa53f1
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-31 10:36:22 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,13 +6,7 @@ class RidesController < ApplicationController
     #raise params.inspect
     @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
     @attraction = Attraction.find(params[:attraction_id])
-    message = @ride.take_ride
-    binding.pry
-    if message
-      @message = "Thanks for riding the #{@attraction.name}!"
-    else
-      @message = message
-    end
+    flash[:notice] = @ride.take_ride
     redirect_to user_path(params[:user_id])
   end
 
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,16 +5,21 @@ class Ride < ActiveRecord::Base
   def take_ride
     @user = User.find(self.user_id)
     @attraction = Attraction.find(self.attraction_id)
-    @user.tickets = @user.tickets - @attraction.tickets
-    @user.nausea = @user.nausea + @attraction.nausea_rating
-    @user.happiness = @user.happiness + @attraction.happiness_rating
-    @user.save
-    if @user.tickets < @attraction.tickets && @user.height < @attraction.min_height
-      message = "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    if @user.tickets > @attraction.tickets && @user.height > @attraction.min_height
+      @user.tickets = @user.tickets - @attraction.tickets
+      @user.nausea = @user.nausea + @attraction.nausea_rating
+      @user.happiness = @user.happiness + @attraction.happiness_rating
+      @user.save
+      "Thanks for riding the #{@attraction.name}!"
+    elsif @user.tickets < @attraction.tickets && @user.height < @attraction.min_height
+      @user.save
+      "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{attraction.name}."
     elsif @user.tickets < @attraction.tickets
-      message = "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+      @user.save
+      "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
     elsif @user.height < @attraction.min_height
-      message = "Sorry. You are not tall enough to ride the #{attraction.name}."
+      @user.save
+      "Sorry. You are not tall enough to ride the #{@attraction.name}."
     end
   end
 end
