21597c1ad8d7bdf2adef34f6748ed45ffe794f63
jennianelson <jennifernelson118@gmail.com>
2018-06-02 15:53:53 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+  before_action :require_logged_in, except: [:new, :create, :home]
+
+  def logged_in?
+    !!current_user
+  end
+
+  private
+    def require_logged_in
+      redirect_to root_path unless logged_in?
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+    helper_method :current_user
 end
