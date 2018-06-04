6f9ac99c6713f6d3aaf4e5ef1b507757eebc7d0f
Lisa Huang <lisa.yc.huang@gmail.com>
2018-06-03 21:06:46 -0700

    Added resources to routes, #create and #user_params in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          session[:user_id] = @user.id
+          redirect_to '/welcome'
+        else
+          redirect_to '/users/new'
+        end
+    end
+
+    def welcome
+        @user = User.find_by(name: params[:user][:name])
+      render 'application/welcome'
+    end
+    
+    private
+    
+    def user_params
+        params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   
   devise_for :users
+
+  resources :users
+  resources :sessions
+  resources :attractions
 end
