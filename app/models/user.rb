aa6b276bf625fbdd632fa664e9e7245b704b3341
Lisa Huang <lisa.yc.huang@gmail.com>
2018-06-03 21:02:40 -0700

    Updated migration files, added association rules to User, Attraction, Ride models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if self.nausea > self.happiness
+      mood = "sad"
+    else
+      mood = "happy"
+    end
+  end
 end
