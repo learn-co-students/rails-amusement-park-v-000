6a27ec68576955db78dc8c49573e7df4fa878e4a
Raiet Intraub <raietsaeed@gmail.com>
2018-06-05 00:40:23 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
 end
+
+
+# rspec spec/models/user_spec.rb
+# rspec spec/models/ride_spec.rb
+# rspec spec/models/attraction_spec.rb
+
+# rspec spec/features/users_features_spec.rb
