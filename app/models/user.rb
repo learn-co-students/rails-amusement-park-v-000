a2fdcaa164f38c5369fd706cb793fa227e33e968
Raiet Intraub <raietsaeed@gmail.com>
2018-06-05 00:48:26 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
 
   def mood
+    if self.nausea > self.happiness
+      "sad"
+    else
+      "happy"
+    end
   end
 
 end
