306c500c3a574efc05e53507a9345b049565010e
Rygel-XVI <micha232@umn.edu>
2018-06-05 21:25:45 -0700

    Done.

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,42 @@
+class AttractionsController < ApplicationController
+
+  def index
+    @attractions = Attraction.all
+    @user = User.find(session[:user_id])
+  end
+
+  def show
+    @attraction = Attraction.find(params[:id])
+    @user = User.find(session[:user_id])
+  end
+
+  def new
+    @attraction  = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to attraction_path(@attraction)
+    else
+      redirect_to attractions_path
+    end
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params(params[:attraction]))
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params(*args)
+    params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :tickets, :min_height)
+  end
+
+end
