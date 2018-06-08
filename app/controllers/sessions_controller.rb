167c25609d61e1f2ca305d73587c50b1e106ffa6
kristinreddington <kristinreddington@gmail.com>
2018-06-07 22:31:58 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by
+    @user = User.find_by(:id => params[:id])
     redirect_to user_path(@user)
   end
 
