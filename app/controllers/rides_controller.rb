314a2f9b57258cce676ceec5faef6eb02fdf8d54
Susan Fowler <infopro94@gmail.com>
2018-06-09 20:57:10 -0500

    resolved errors with users signup

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,11 +3,10 @@ class RidesController < ApplicationController
   def new
     # binding.pry
 
-  	@ride = Ride.new(
+  	@ride = Ride.create(
       :user_id => params[:user_id],
       :attraction_id => params[:attraction_id]
       )
-    @ride.save
   	@message = @ride.take_ride
   	redirect_to user_path(@ride.user, :message => @message)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
   
-  # get '/users/new', to: 'users#new', as: 'new_user' 
+  
   root 'static_pages#home'  
-  # get '/users', to:'users#index', as: 'users' 
-  # post '/users', to:'users#create'  
-  # get '/users/:id', to: 'users#show', as: 'user'  
-  # patch '/users/:id/edit', to: 'users#edit', as: 'edit_user'  
-  # get '/attractions', to: 'attractions#index', as: 'attractions'  
+  
   get '/signin', to: 'sessions#new'
   post '/session', to: 'sessions#create', as: 'session' 
   delete '/signout', to: 'sessions#destroy' 
-  # get '/attractions/new', to: 'attractions#new', as: 'new_attraction' 
-  # get '/attractions/:id', to: 'attractions#show', as: 'attraction'  
-  # get '/attractions/:id/edit', to: 'attractions#edit', as: 'edit_attraction'  
-  # patch '/attractions/:id', to: 'attractions#update'  
-  # post '/attractions', to: 'attractions#create' 
+  
   post '/rides/new', to:"rides#new"
   
   resources :users
