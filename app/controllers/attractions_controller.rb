ccee41929228c4a85dd80b3b61f9f4b0fc8b6cfc
MiriamPeskowitz <miriam5000@gmail.com>
2018-06-11 23:42:59 -0400

    working on attractions index page

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,2 +1,7 @@
 class AttractionsController < ApplicationController
+
+def index
+	@attractions = Attraction.all
+end 
+
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,26 @@ class User < ApplicationRecord
     # validates presence_of :name,  :nausea, :happiness, :tickets, :height,   presence: true
 
   def mood
-    if self.nausea >= self.happiness
-        return "sad"
-    else 
-        return "happy"
-        # what about equal? 
+    if self.happiness && self.nausea
+      if self.nausea > self.happiness
+        mood =  "sad"
+      else 
+        mood =  "happy"
+       end
     end
-    # or: one minus the other, mood > 0 ? happy : sad
   end
 
+  # def mof
+  #   if self.happiness && self.nausea
+  #     mood = self.happiness - self.nausea
+  #     mood > 0 ? "happy" : "sad"
+  #   end 
+  # end 
+
+    # or: one minus the other, mood > 0 ? happy : sad
+
+
 # what am i trying to do: compare @user.nausea and @user.happiness
- 
+ # happiness > nausea ? happy : sad 
 
 end
