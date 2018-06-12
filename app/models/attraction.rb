1c5d32b1d0c227197802830552569527f0f7e24c
Aubrey Neimeier <neimeier.1@osu.edu>
2018-06-12 20:23:20 +0000

    models have attributes and proper associations

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,10 @@
 class Attraction < ApplicationRecord
+  validates :name, presence: true
+  validates :tickets, presence: true
+  validates :nausea_rating, presence: true
+  validates :happiness_rating, presence: true
+  validates :min_height, presence: true
+
+  has_many :rides
+  has_many :users, through: :rides
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    #returns sad when the user is more nauseaous than happy, or happy when the opposite
+  end
+
+
 end
