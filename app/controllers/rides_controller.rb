51bb49bd1fcae12200264dff7b76d38f3b3b4779
Yujun Sung <yujunsung@gmail.com>
2018-06-13 14:31:35 +0000

    modified rides view and controller

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,10 @@
 class RidesController < ApplicationController
   def new
+    @ride = Ride.new
   end
 
   def create
+    @ride = Ride.new(:user_id => params[:user_id], :attraction_id => params[:attraction_id])
+    redirect_to attractions_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'rides/new'
-  get 'rides/create'
+
   root 'sessions#index'
 
-  get '/secrets' => 'secrets#show'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   get '/signout' => 'sessions#destroy'
   resource :sessions, only: [:new]
   resources :users
   resources :attractions
-
+  resource :rides, only: [:new, :create]
 end
