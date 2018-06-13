8ca0b1b4c77519bcfb59b1fbf025e795a76a3724
Aubrey Neimeier <neimeier.1@osu.edu>
2018-06-13 03:21:16 +0000

    views and routes for sessions/users working for new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,7 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new' => 'users#new'
+  post 'users' => 'users#create'
+  get 'sessions/new' => 'sessions#new'
+  post 'sessions' => 'sessions#create'
+
+  root 'welcome#home'
 end
