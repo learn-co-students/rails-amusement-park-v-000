37a8635355fe9b4dcfe84afb96d067e156c4c6e2
Yujun Sung <yujunsung@gmail.com>
2018-06-15 12:21:58 +0000

    modified ride controller

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,7 +4,7 @@ class RidesController < ApplicationController
   end
 
   def create
-   binding.pry
+
     @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
     redirect_to user_path(@ride.user), alert: "#{@ride.take_ride}"
   end
