43457378418c59bdef4c9d85b5cb3865645bee41
AspenJames <aspenjames@tqca.org>
2018-06-18 16:02:00 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+
+  def welcome
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
 end
--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,2 +1,9 @@
 class Attraction < ApplicationRecord
+  has_many :rides
+  has_many :users, :through => :rides
+
+  def user_count
+    users.count
+  end
+
 end
