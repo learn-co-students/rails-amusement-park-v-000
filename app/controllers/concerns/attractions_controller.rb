4b4f9920b7afdc7a2ed30031a9e2ef4a44b14f11
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-18 19:26:37 +0000

    Done.

---
+++ app/controllers/concerns/attractions_controller.rb
@@ -0,0 +1,10 @@
+class AttractionsController < ApplicationController
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def show
+
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,5 +20,5 @@ private
 def user_params
   params.require(:user).permit(:name, :password, :password_confirmation)
 end
-
+end
 end
