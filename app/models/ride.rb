e80ce78106851d6f97d635136df19c47ffa89a91
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-27 18:19:59 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,21 @@
+require 'pry'
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if self.user.height < self.attraction.min_height && self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    else
+      tickets = self.user.tickets - self.attraction.tickets
+      nausea =  self.user.nausea + self.attraction.nausea_rating
+      happiness = self.user.happiness + self.attraction.happiness_rating
+      self.user.update(tickets: tickets, nausea: nausea, happiness: happiness)
+      "Thanks for riding the #{self.attraction.name}!"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+  validates_presence_of :name, :password, :happiness, :nausea, :height, :tickets
+
+  def mood
+    return "happy" if self.happiness > self.nausea
+    "sad"
+  end
 end
