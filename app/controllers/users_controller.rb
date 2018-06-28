5becf78dae3ea51337c334d0e6439f216b07a623
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-28 18:54:43 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+  end
+
+
+  private
+
+  def user_params
+    
   end
 
 end
