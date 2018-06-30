a6bea46ef6b20457324a16282de65ddee97a887f
Carol Greene <carolgreene3@gmail.com>
2018-06-30 00:19:12 +0000

    completed Ride methods & added Admin to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_28_234525) do
+ActiveRecord::Schema.define(version: 2018_06_30_000944) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_06_28_234525) do
     t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
