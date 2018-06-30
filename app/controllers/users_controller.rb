93692b754a1e53b49abf75eb080c5d42b93ce104
Carol Greene <carolgreene3@gmail.com>
2018-06-30 21:57:24 +0000

    made user new form & show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
-end 
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+
+    redirect_to user_path(@user)
+
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :admin)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  root 'welcome#home'
   resources :rides
   resources :users
   resources :attractions
