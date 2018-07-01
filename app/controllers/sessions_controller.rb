b3fd901bb82aa2b804fee3d3d43318279ec673af
Carol Greene <carolgreene3@gmail.com>
2018-07-01 17:49:02 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      redirect_to user_path(@user)
+      render :new
     end
   end
 
