86fb5ec116c8d0776de56d475b3a4451958e798d
Carol Greene <carolgreene3@gmail.com>
2018-07-01 00:03:52 +0000

    corrected sign on

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:user][:name])
-    authenticated = user.try(:authenticate, params[:user][:password])
-    return redirect_to signin_path unless authenticated
+    #authenticated = user.try(:authenticate, params[:user][:password])
+    #return redirect_to signin_path unless authenticated
       session[:user_id] = user.id
     @user = user
     redirect_to user_path(@user)
