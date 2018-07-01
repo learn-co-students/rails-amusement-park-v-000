0598c8c77d18b92d0b7f5f91bda10b055be06e6e
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-01 19:17:12 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -11,13 +11,14 @@ class AttractionsController < ApplicationController
 
   def create
     @attraction = Attraction.create(attraction_params)
-
+    redirect_to attraction_path(@attraction)
   end
 
   def show
     @attraction = Attraction.find_by(id: params[:id])
   	@user =  User.find_by(id: session[:user_id])
     @ride = Ride.new
+
   end
 
   private
