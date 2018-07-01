51f7329eed2749469d410f625a4b3037850aaf32
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-01 18:54:36 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,4 +4,25 @@ class AttractionsController < ApplicationController
     @attractions = Attraction.all
     @user = current_user
   end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+
+  end
+
+  def show
+    @attraction = Attraction.find_by(id: params[:id])
+  	@user =  User.find_by(id: session[:user_id])
+    @ride = Ride.new
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :nausea_rating, :happiness_rating, :tickets)
+  end
 end
--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,5 +1,5 @@
 class Attraction < ApplicationRecord
-  validates_presence_of :name, :min_height, :nausea_rating, :happiness_rating, :tickets
+  #validates_presence_of :name, :min_height, :nausea_rating, :happiness_rating, :tickets
   has_many :rides
   has_many :users, through: :rides
 end
