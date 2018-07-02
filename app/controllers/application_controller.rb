04a68c8ed0e22257ca1abd746663ae62cc29d896
roitblatari <roitblatari@gmail.com>
2018-07-02 12:06:21 -0400

    Merge pull request #2 from learn-co-curriculum/wip-rails-shrinking-solution
    Wip rails shrinking solution

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
-  # skip_before_action :is_logged_in, only: [:new, :create, :index, :home]
-
-  # def home
-    
-  # end
- 
-  # def current_user
-  #    !!session[:user_id]
-  # end
   
 
   def current_user
@@ -22,7 +13,4 @@ class ApplicationController < ActionController::Base
      redirect_to root_path  unless current_user
   end
 
-  # def current_user
-  #   session[:user] ||= []
-  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  # validates_presence_of :tickets
   has_many :rides
   has_many :attractions, through: :rides
 
