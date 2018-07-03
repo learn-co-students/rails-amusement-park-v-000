f2645043aedf15c6a126f6fb4829495d6f772378
Carol Greene <carolgreene3@gmail.com>
2018-07-03 23:24:09 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,6 +1,7 @@
 class AttractionsController < ApplicationController
 
   def new
+    @attraction = Attraction.new
   end
 
   def index
@@ -8,7 +9,30 @@ class AttractionsController < ApplicationController
     @user = current_user
   end
 
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
   def show
     @attraction = Attraction.find_by(id: params[:id])
+    @user = current_user
+  end
+
+  def edit
+    @attraction = Attraction.find_by(id:params[:id])
+    @user = current_user
+  end
+
+  def update
+    @attraction = Attraction.find_by(id: params[:id])
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params
+      params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 end
