a65dec06522dc5e573efb1acfbbd1082d8093925
github username <github email address>
2018-07-10 18:24:21 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -10,7 +10,7 @@ class Ride < ActiveRecord::Base
     elsif user.tickets < attraction.tickets
       "Sorry. You do not have enough tickets to ride the #{attraction.name}."
     elsif user.height < attraction.min_height
-      "Sorry. You are not tall enough to ride the #{attraction.name}!"
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
     else
     user.tickets -= attraction.tickets
     user.nausea += attraction.nausea_rating
