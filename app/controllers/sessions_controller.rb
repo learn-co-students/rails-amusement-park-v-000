15b4bd5bec90be3f2566204e295d18a288d92772
Nicholas Powers <njpowers7915@gmail.com>
2018-07-12 15:10:50 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:session][:name])
-    if user && user.authenticate(params[:session][:password])
-      #Log In
+    if @user && @user.authenticate(params[:session][:password])
+      log_in(@user)
       redirect_to user_path(@user)
     else
       #Error Message
