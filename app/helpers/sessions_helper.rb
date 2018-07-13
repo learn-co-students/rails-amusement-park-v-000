154954ffbc98de990eca43c6fc012427fb7a4603
Nicholas Powers <njpowers7915@gmail.com>
2018-07-13 18:43:14 +0000

    Done.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,7 +4,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) 
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   # Returns true if the user is logged in, false otherwise.
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,22 @@
 class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    @user = User.find_by_id(self.user.id)
+    binding.pry
+    @attraction = Attraction.find_by_id(self.attraction.id)
+    if @user.height < @attraction.min_height && @user.tickets < @attraction.tickets
+      flash[:ride_success] = "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{@attraction.name}."
+    elsif @user.height < @attraction.min_height
+      flash[:too_short] = "Sorry. You are not tall enough to ride the #{@attraction.name}."
+    elsif @user.tickets < @attraction.tickets
+      flash[:too_few_tickets] = "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
+    else
+      @user.tickets = @user.tickets - @attraction.tickets
+      @user.nausea = @user.nausea - @attraction.nausea_rating
+      @user.happiness = @user.happiness - @attraction.happiness_rating
+      @user.save
+    end
+  end
 end
