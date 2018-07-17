6c0995ef332d922b533c20cdedb12c55b9eb01b6
Almighty-Mose <branden.mosley1337@gmail.com>
2018-07-17 15:17:17 -0700

    passing 16 feature tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,5 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session.delete :user_id
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  delete '/signout' => 'sessions#destroy'
   resources :users
   resources :attractions
 end
