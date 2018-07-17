0a6f5ac6636a9758c514e6028f4de649f354b484
Ashley Schultz <ashleyeschultz@gmail.com>
2018-07-17 23:00:49 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+
+  def new
+    @users = User.all.pluck(:name)
+  end
+
+  def create
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to signin_path
+    end
+  end
 end
