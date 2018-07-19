5b40e6d14ebd725550ddb80adecbacb69b35b410
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-18 18:27:16 -0700

    application#current_user&&#logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @user ||= session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  root 'user#welcome'
+
   resources :users
   resources :attractions
+  resources :rides, only: [:new]
 
   get '/login', to: 'sessions#new'
-  post '/signup', to: 'sessions#create'
+  post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
 end
