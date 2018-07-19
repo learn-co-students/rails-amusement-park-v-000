b21176042efb03576b73e0c06b3ebb09090ca302
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-19 16:54:03 -0700

    attractions#index & #show & #new

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,6 +1,27 @@
 class AttractionsController < ApplicationController
   before_action :set_attraction, only: [:show, :edit, :update]
 
+  def index
+    @attractions = Attraction.all
+  end
+
+  def show
+    @ride = Ride.new
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
   private
 
   def attraction_set
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-
-
   resources :users
   resources :attractions
   resources :rides, only: [:new]
