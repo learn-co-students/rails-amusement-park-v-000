210c702ca640d8a8bc2f44f8034511c07d70c9ae
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-19 16:10:43 -0700

    users#update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,11 @@ class UsersController < ApplicationController
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
