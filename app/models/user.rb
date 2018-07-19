ecab6fab4426943afdcca27134937093f59694f4
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-18 17:36:58 -0700

    User #mood method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,14 @@ class User < ActiveRecord::Base
 
   has_many :rides
   has_many :attractions, :through => :rides
-  
+
+  def mood
+    if self.nausea && self.happiness
+      if self.nausea > self.happiness
+        "sad"
+      elsif self.nausea < self.happiness
+        "happy"
+      end
+    end
+  end
 end
