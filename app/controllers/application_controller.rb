48371cf1838fe214792a959e19742444dd78b837
alcesalces11331 <rollin.metzger@gmail.com>
2018-07-19 16:40:46 -0700

    Application #login_required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :login_required, except: [:welcome, :new, :create]
   helper_method :current_user
+
+  private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -8,5 +11,9 @@ class ApplicationController < ActionController::Base
 
   def logged_in?
     !!current_user
+  end
+
+  def login_required
+    redirect_to root_path unless logged_in?
   end
 end
