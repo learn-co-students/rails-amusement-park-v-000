4cbbeaaa4a19ef70dc2db0c3b77d116d79f7d4cf
Matt Farmer <mfarmer@d120.org>
2018-07-22 01:38:51 +0000

    all user specs passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
     has_secure_password
     has_many :rides
     has_many :attractions, through: :rides
+    
+    
+    def mood
+        self.happiness > self.nausea ? 'happy' : 'sad'
+    end
 end
