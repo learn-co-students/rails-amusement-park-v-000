dbe660db7d3b7ec81a71514c0b520865a77aaa83
Matt Farmer <mfarmer@d120.org>
2018-07-22 04:42:58 +0000

    sign out specs are passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,11 @@ class SessionsController < ApplicationController
         end
     end
     
+    def destroy
+        session.delete :user_id
+        redirect_to root_path
+    end
+    
     private
     
     def session_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
     root to: "welcome#home"
     
     resources :users, only: [:new, :create, :show, :destroy]
+    resources :attractions, only: [:new, :create, :index]
     
     get '/signin', to: 'sessions#new'
     post '/sessions', to: 'sessions#create'
+    get '/logout', to: 'sessions#destroy'
   
 end
