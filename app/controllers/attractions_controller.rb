6d652f596ad2bcfa8a7995e92ca8f9e49ea92e72
Matt Farmer <mfarmer@d120.org>
2018-07-22 06:15:05 +0000

    implementing add attraction form

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,14 +1,18 @@
 class AttractionsController < ApplicationController
 
     def new
-        @attraction=Attraction.new
+        
     end
     
     def create
+        @attraction=Attraction.new(attraction_params)
+        @attraction.save
+        redirect_to attraction_path(@attraction)
     
     end
     
     def index
+        @user=User.find(session[:user_id])
         @attractions=Attraction.all
     end
     
@@ -17,4 +21,9 @@ class AttractionsController < ApplicationController
         @user=User.find(session[:user_id])
     end
     
+    private
+    
+    def attraction_params
+        params.require(:attraction).permit(:name, :tickets, :min_height, :happiness_rating, :nausea_rating)
+    end
 end
