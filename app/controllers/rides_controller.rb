dbb42f067781091cbac7f2523f55acd1010c19fb
ktamoguis <ktamoguis@gmail.com>
2018-07-23 17:51:40 +0800

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -5,13 +5,15 @@ class RidesController < ApplicationController
 
   def create
     binding.pry
-    user = User.find(session[:user_id])
-    attraction = Attraction.find(params[:ride][:attraction_id])
+    @user = User.find(session[:user_id])
+    @attraction = Attraction.find(params[:ride][:attraction_id])
     @ride = Ride.new
-    @ride.user = user
-    @ride.attraction = attraction
-    binding.pry
+    @ride.user = @user
+    @ride.attraction = @attraction
     @ride.take_ride
-    redirect_to user_path(user)
+    @user.rides << @ride
+    #@user.save
+    binding.pry
+    redirect_to user_path(@user)
   end
 end
