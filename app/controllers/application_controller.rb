f286c1d4a5bba2f0d5da738713769f70fce027dd
github username <github email address>
2018-07-22 23:10:42 -0400

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?, :authenticate_user
+
+  def home
+  end
+
+  def current_user
+    @current_user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def authenticate_user
+    if !logged_in?
+      redirect_to 'root_path'
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+  root 'application#home'
+  resources :users
+  get '/signin', to: 'sessions#sign_in', as: :signin
+  post '/signin', to: 'sessions#create'
 end
