c68e19be5e232631c341638a3a974566bd89cf13
ktamoguis <ktamoguis@gmail.com>
2018-07-23 15:50:05 +0800

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,6 +1,14 @@
 class AttractionsController < ApplicationController
 
   def index
+    #binding.pry
+    @attractions = Attraction.all
+  end
+
+  def show
+    binding.pry
+    @attraction = Attraction.find_by(id: params[:id])
+    @ride = @attraction.ride
   end
 
 end
