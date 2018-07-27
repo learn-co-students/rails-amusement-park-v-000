8938162e3e774db4668df2a342b8902b872900b6
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-27 19:43:17 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def new
+    binding.pry
     @user = User.new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
+  resources :users
   get '/signin', to: "sessions#new"
   post '/signin', to: "sessions#create"
-  root 'users#new'
-  get '/signup', to: "users#new"
-  post '/signup', to: "users#create"
 
 end
