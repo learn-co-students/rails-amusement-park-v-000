0faf48b3cbcd5c6bbdbd0ad121499272db09967c
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-27 19:07:39 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:show]
-  get '/signin', to: "session#new"
-  post '/signin', to: "session#create"
-  root "users#new"
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
+  root 'users#new'
   get '/signup', to: "users#new"
   post '/signup', to: "users#create"
 
