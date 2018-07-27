4a245f2ae8d22bc6fe7891da592fd2bb11bd3b07
Victoria Meng <nights1ststar@protonmail.com>
2018-07-27 02:23:33 +0000

    Done.

--- db/migrate/20180727021532_create_attractions.rb
+++ db/migrate/20180727021532_create_attractions.rb
@@ -1,8 +1,13 @@
 class CreateAttractions < ActiveRecord::Migration[5.2]
   def change
     create_table :attractions do |t|
-
+      t.string :name
+      t.string :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
       t.timestamps
     end
   end
+
 end
