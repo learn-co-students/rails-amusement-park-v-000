70a298513f7a362304f2335aee5361909b343bb8
Emily Zhao <tong.zhao.92@gmail.com>
2018-07-30 16:54:16 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:name])
-    return head(:forbidden) unless @user.authenticate(params[:password])
+    @user = User.find_by(name: params[:session][:user_name])
+    return head(:forbidden) unless @user.try(:authenticate, params[:session][:password])
     session[:user_id] = @user.id
     redirect_to user_path(@user)
   end
