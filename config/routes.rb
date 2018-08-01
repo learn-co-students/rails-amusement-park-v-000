65cece5768bdde2aed8762687a4ce10cc038a98a
Aellon Krider <aellonkrider@gmail.com>
2018-08-01 19:25:58 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+  root :to => 'welcome#index'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/ride' => 'attractions#ride'
+  resources :users
+  resources :attractions
 end
