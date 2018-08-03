b1224ddebe0d72e09e8229de9a136269c69f3341
lauraBo <BodellLaura@gmail.com>
2018-08-03 12:37:32 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  before_action :current_user
+
+  before_action :required_to_be_logged_in, except: [:new, :create, :home]
+
+
+
+  def logged_in?
+    current_user != nil
+  end
+
+    private
+
+       def required_to_be_logged_in
+          redirect_to root_path unless logged_in?
+       end
+
+       def current_user
+         @current_user ||= User.find(session[:user_id]) if session[:user_id]
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+   root 'site#home'
+
+
+  get '/signin' => 'sessions#new'
   
+
+  post '/sessions/new', to: "sessions#create"
+  delete '/logout' => 'sessions#destroy'
+
+
+   post '/rides/new', to: 'rides#new'
+
+
+  resources :attractions
+  resources :users
+  resources :rides
+
 end
