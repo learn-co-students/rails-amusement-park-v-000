ca93ebff759fe63218f40bc89270e759d5ae79d4
jebediahelliott <jebediahelliott@gmail.com>
2018-08-04 13:10:14 -0500

    Done.

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,6 @@
+class AdminController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if self.happiness && self.nausea
+      if self.happiness > self.nausea
+        "happy"
+      elsif self.happiness < self.nausea
+        "sad"
+      end
+    end
+  end
 end
