c5ca6d3bae6e479995b70e4502f799917aee9e0b
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 21:04:15 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_05_201826) do
+ActiveRecord::Schema.define(version: 2018_08_05_202629) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_08_05_201826) do
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
