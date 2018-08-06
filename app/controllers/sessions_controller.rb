29e90b3c6e37b34b41d3472cd18692c9c659c3c1
Zac Willmington <zacwillmington@Zacs-Air.attlocal.net>
2018-08-06 11:34:30 -0700

    collection_select method changed values to search for name.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,8 @@ class SessionsController < ApplicationController
     end
 
     def create
-        binding.pry
-        @user = User.find(params[:id])
+        # binding.pry
+        @user = User.find_by(:name => params[:user][:name])
         if @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
