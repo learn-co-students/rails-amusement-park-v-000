cd2a8096850b90fcb9d43c6e0c057977a7858926
Zac Willmington <zac.willmington@gmail.com>
2018-08-06 17:37:03 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,17 @@ class SessionsController < ApplicationController
     end
 
     def new
-        binding.pry
+        @users = User.all
     end
 
     def create
-
+        binding.pry
+        @user = User.find(params[:id])
+        if @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to '/users/new'
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   root 'sessions#home'
- #change to signin. Need post and get routes for it
+
   resources :users
 
   get '/users/new' => 'users#new', :as => '/signup'
   post '/users/create' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+
 
 end
