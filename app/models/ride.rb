0cf276ea9c779cdff5603127f91a7f0afdc87cd6
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-08-06 17:02:36 -0700

    Add take_ride (needs refactoring).  All model specs pass

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,33 @@
+require 'pry'
 class Ride < ApplicationRecord
     belongs_to :user
     belongs_to :attraction
+
+    def take_ride
+        msg = String.new
+        user = User.find(self.user_id)
+        attraction = Attraction.find(self.attraction_id)
+        if user.tickets < attraction.tickets 
+            msg= add_error(msg,"You do not have enough tickets to ride the #{attraction.name}.")
+        end
+        if user.height < attraction.min_height
+            msg= add_error(msg,"You are not tall enough to ride the #{attraction.name}.")
+        end
+        if msg.length==0
+            user.update(:tickets => user.tickets - attraction.tickets,
+                        :nausea => user.nausea + attraction.nausea_rating,
+                        :happiness => user.happiness + attraction.happiness_rating)
+        end
+        msg
+    end
+
+    private
+
+    def add_error(msg, type)
+        msg.concat("Sorry.") if msg.length == 0
+        msg.concat(" ") 
+        msg.concat(type)
+        msg
+    end
+
 end
