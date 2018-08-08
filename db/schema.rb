5bc7e559cc344d7814d36f289297af2b599dfc62
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-08 15:10:12 -0400

    new user form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_08_183910) do
+ActiveRecord::Schema.define(version: 2018_08_08_190634) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_08_08_183910) do
     t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
