243ca9a9854b296c7ef1433aae3b98c371ede455
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-08 14:47:05 -0400

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+  resources :users
+  resources :attractions
+  resources :rides
+
+  namespace :admin do
+    resources :attractions, only [:new]
+  end
+
+
 end
