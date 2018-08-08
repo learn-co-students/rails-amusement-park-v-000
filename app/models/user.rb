32a886c9866aa35399ace5b6dd3384136f0d864a
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-08 16:08:50 -0400

    mood method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
-  has_many :attractions
+  has_many :attractions, through: :rides
+  has_many :rides
   has_secure_password
+
+  validates :password, presence: true
+
+
+  def mood
+    if happiness > nausea
+        "happy"
+    else
+       "sad"
+    end
+  end
+
 end
