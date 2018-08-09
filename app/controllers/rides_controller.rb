8ad7fbab68925c173368280c44f936340cd9d403
BarbaraPruz <barbara.prusiewicz@gmail.com>
2018-08-09 10:56:03 -0700

    Show take_ride result message

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -7,7 +7,8 @@ class RidesController < ApplicationController
         # error check : user should match logged in user
         ride = Ride.new(:user_id => @user.id, :attraction_id => params[:attraction_id])
         errors = ride.take_ride
-        errors = "Thanks for riding #{@attraction.name}!" if !errors
-        redirect_to user_path(@user), alert: errors
+        errors = "Thanks for riding the #{@attraction.name}!" if errors.empty?
+        flash[:notice] = errors
+        redirect_to user_path(@user)
     end
 end
