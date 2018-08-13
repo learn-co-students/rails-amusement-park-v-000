aa71f450272278547735c28f2e54b29c43d6ebc5
Elaine Anderson <eparie@bu.edu>
2018-08-13 21:05:05 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,9 +3,9 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
-    binding.pry
     @user = User.find_by(id: user_id)
-    if user[:tickets] < ride[:tickets]
+    @ride = Ride.find_by(id: self.id)
+    if user[:tickets] < @ride[:tickets]
       puts "Sorry. You do not have enough tickets to ride the Roller Coaster."
     end
   end
