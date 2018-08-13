9cd5edaac3fcd56136759db6debebcd7d72f26b2
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-13 12:33:05 -0400

    more styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in, :user_signup, :user_login
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
 
   def user_signup
     @user = User.new do |u|
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
 
   def destroy
 	   session.delete :name
+     redirect_to root_url
 	end
 
 
