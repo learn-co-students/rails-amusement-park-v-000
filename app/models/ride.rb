fccbd923117d93ec6626f50d2143bed3d64290af
Elaine Anderson <eparie@bu.edu>
2018-08-13 20:54:57 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,12 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    binding.pry
+    @user = User.find_by(id: user_id)
+    if user[:tickets] < ride[:tickets]
+      puts "Sorry. You do not have enough tickets to ride the Roller Coaster."
+    end
+  end
 end
---
+++ db/migrate/20180813204007_create_rides.rb
@@ -0,0 +1,11 @@
+class CreateRides < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rides do |t|
+      t.string :name
+      t.integer :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
+    end
+  end
+end
