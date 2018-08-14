24f637534f5b5fc136803fd5e2eb4817ac4f7019
Elaine Anderson <eparie@bu.edu>
2018-08-14 17:39:30 +0000

    working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  #before_action :require_login
+#  skip_before_action :require_login, only: [:new, :create]
 
   def new
     @user = User.new
@@ -12,6 +14,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    redirect_to '/' unless session.include? :user_id
     @user = User.find_by(id: params[:id])
   end
 
@@ -21,4 +24,8 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets, :admin)
   end
+
+#  def require_login
+#  redirect_to '/' unless session.include? :user_id
+#end
 end
