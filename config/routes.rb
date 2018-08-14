985126ed708ee712d27d3556e2c96ae83d0bb9d5
Elaine Anderson <eparie@bu.edu>
2018-08-14 18:11:24 +0000

    add attraction controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'users#index'
   resources :users
   resources :sessions
+  resources :attractions
 
   get '/signin', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
