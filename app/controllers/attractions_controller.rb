cc5a93e524d9b6335b0108c7f56d49ea31ca1d7d
Elaine Anderson <eparie@bu.edu>
2018-08-15 20:40:11 +0000

    no more errors

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -21,6 +21,16 @@ class AttractionsController < ApplicationController
     redirect_to attraction_path(@attraction)
   end
 
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
   private
 
   def attraction_params
