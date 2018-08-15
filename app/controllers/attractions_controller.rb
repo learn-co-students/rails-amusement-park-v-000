2585e99866002b508e54770cf322ddde8d85932c
Elaine Anderson <eparie@bu.edu>
2018-08-15 19:53:50 +0000

    almost there

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -10,4 +10,22 @@ class AttractionsController < ApplicationController
     @user = current_user
 
   end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    @attraction.save
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :nausea_rating, :happiness_rating, :tickets)
+  end
+
+
 end
