4119044f2de1e1d7a631f16881d5d69600e715dd
ElleeB <laura.trager@gmail.com>
2018-08-16 20:37:47 -0400

    Pass model specs

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,44 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    # accounts for the user not having enough tickets & not being tall enough
+    if !enough_tickets? && !tall_enough?
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif
+      # accounts for the user not having enough tickets
+      !enough_tickets?
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif
+      # accounts for the user not being tall enough
+      !tall_enough?
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      subtract_tickets
+      update_nausea
+      update_happiness
+    end
+  end
+
+  def enough_tickets?
+    user.tickets >= attraction.tickets
+  end
+
+  def tall_enough?
+    user.height >= attraction.min_height
+  end
+
+  def subtract_tickets
+    user.update(tickets: user.tickets - attraction.tickets)
+  end
+
+  def update_nausea
+    user.update(nausea: user.nausea + attraction.nausea_rating)
+  end
+
+  def update_happiness
+    user.update(happiness: user.happiness + attraction.happiness_rating)
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    # User has a method 'mood' that returns 'sad' when
+    # the user is more nauseous than happy
+    self.nausea > self.happiness ? 'sad' : 'happy'
+  end
 end
