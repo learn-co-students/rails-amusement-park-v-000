a6111e6bcf4486b70cff52a8d501c025285e3947
Jul Jen <juliajen.prog@gmail.com>
2018-08-18 03:36:50 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,8 +2,7 @@ class RidesController < ApplicationController
 
   def new
     @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
-    session[:ride_message] = @ride.take_ride
-    redirect_to user_path(@ride.user.id)
+    redirect_to user_path(@ride.user.id, session[:ride_message] => @ride.take_ride)
   end
 
 end
