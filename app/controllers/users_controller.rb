21fd61bf5b71786dc70a0ceae603f3eefd0d9d22
Jul Jen <juliajen.prog@gmail.com>
2018-08-18 02:16:11 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,11 @@ class UsersController < ApplicationController
   end
 
   def create
-binding.pry
+# binding.pry
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      
+
       session[:notice] = "User account created successfully!"
       redirect_to user_path(@user)
     else
--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,12 @@ Rails.application.routes.draw do
 
   root 'application#main'
 
-  namespace :admin do
-    resources :stats, only: [:index]
-  end
-
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
-
-  # get '/signup' => 'users#new'
-  # post '/signup' => 'users#create'
-
-
-  resources :users, only: [:index, :new, :create, :show]
-  resources :attractions, only: [:index, :new, :show, :edit]
-
   post '/logout' => 'sessions#destroy'
 
 
+  resources :users
+  resources :attractions
 
 end
