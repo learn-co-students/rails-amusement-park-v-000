245adce51c71b66036f0691ddb0233f7af58e410
ElleeB <laura.trager@gmail.com>
2018-08-19 13:25:13 -0400

    Pass feature spec 193

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,10 @@
+class AttractionsController < ApplicationController
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def show
+    @attraction = Attraction.find_by(id: params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+
+  get '/logout', to: 'sessions#destroy'
+
+  resources :attractions
 end
