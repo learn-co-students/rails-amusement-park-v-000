a309029bb31d7ea4eeb6db8942e2df2352709dd1
ElleeB <laura.trager@gmail.com>
2018-08-19 10:26:41 -0400

    Problem solve for features spec 47

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,32 @@ class SessionsController < ApplicationController
     # logs you in with the correct password
     # user has entered login info
     # check the login info (name + password) against database
+    user = User.find_by(name: params[:user][:name])
     # if match, that is the user - create a session
+    if user.authenticate(params[:password])
+      session[:user_id] = user.id
+
+      redirect_to user_path(user.id)
     # if no match, redirect to login
+    else
+      redirect_to root_path
+    end
   end
 end
+
+# if user = current_user
+#
+#   redirect_to user_path(user.id)
+# # logs you in with the correct password
+# # user has entered login info
+# # check the login info (name + password) against database
+# else user = User.find_by(name: params[:user][:name])
+# # if match, that is the user - create a session
+#   if user.authenticate(params[:password])
+#     session[:user_id] = user.id
+#
+#     redirect_to user_path(user.id)
+#   # if no match, redirect to login
+#   else
+#     redirect_to root_path
+#   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   resources :users
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
 end
