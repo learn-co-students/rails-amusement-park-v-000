f2262eacc9bfdca75839444ba4130a4fa3ed4731
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-08-21 17:26:42 -0400

    Add mood method to User. All User model tests passing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
     has_many :rides
     has_many :attractions, through: :rides
-
     has_secure_password
+
+    def mood
+        nausea > happiness ? "sad" : "happy"
+    end
 end
