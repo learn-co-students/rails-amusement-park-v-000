101d6435ec84a8813ceed273a8573aace99e9b3c
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-08-21 17:23:37 -0400

    Add admin boolean to User model, default to false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_211116) do
+ActiveRecord::Schema.define(version: 2018_08_21_212021) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_08_21_211116) do
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
+    t.boolean "admin", default: false
   end
 
 end
