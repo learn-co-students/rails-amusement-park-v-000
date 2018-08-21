4efadd466f56c656ec4b3afe156db6d30c1a75c7
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-08-21 17:49:51 -0400

    Add Ride logic to stop User from going on Attraction if they are not tall enough and or do not have enough tix

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,27 @@
 class Ride < ActiveRecord::Base
     belongs_to :user
     belongs_to :attraction
+
+    def take_ride
+        disqualifiers = []
+
+        if attraction.tickets > user.tickets
+            disqualifiers << disqualifier("do not have enough tickets")
+        end
+        if attraction.min_height > user.height
+            disqualifiers << disqualifier("are not tall enough")
+        end
+
+        if !disqualifiers.empty?
+            "Sorry. " + disqualifiers.join(" ")
+        end
+            
+
+    end
+
+    private
+
+    def disqualifier(are_unable_for_this_reason)
+        "You #{are_unable_for_this_reason} to ride the #{attraction.name}."
+    end
 end
