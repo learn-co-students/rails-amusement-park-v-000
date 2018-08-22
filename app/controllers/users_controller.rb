8e79032d1085fea721e6d6389c1d7e375ce17153
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-08-22 18:48:05 -0400

    Implement working draft of new user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+
+    render html: "You made a user wow! Their name is: #{@user.name}"
   end
 
   def new
@@ -7,6 +10,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    raise(params.inspect)
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password)
   end
 end
