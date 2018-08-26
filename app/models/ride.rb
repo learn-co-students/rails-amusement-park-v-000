a332ae379881d88fabfe2326d79ccc9aa8fe5571
Veronica Bustabad <vbustabad@gmail.com>
2018-08-26 04:11:16 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,7 +1,29 @@
+require 'pry'
+
 class Ride < ActiveRecord::Base
   belongs_to :attraction
   belongs_to :user
 
   def take_ride
+    if !sufficient_tickets && tall_enough
+      puts "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    elsif !tall_enough && sufficient_tickets
+      puts "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    elsif !sufficient_tickets && !tall_enough
+      puts "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    else
+      self.user.tickets -= self.attraction.tickets
+      self.user.happiness += self.attraction.happiness_rating
+      self.user.nausea += self.attraction.nausea_rating
+    end
   end
+
+  def sufficient_tickets
+    self.user.tickets >= self.attraction.tickets
+  end
+
+  def tall_enough
+    self.user.height >= self.attraction.min_height
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  
+  get '/users/new' => 'users#new'
 end
