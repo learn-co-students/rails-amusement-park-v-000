42cc6c1bbe4adaf0301a54f2bb2a257c7abeec01
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-08-26 23:05:32 -0400

    Factor new ride form out to partial. Add logic to conditioanlly show go on or edit button on attraction detail page based on whether user is admin. Add edit and update routes to attractions resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   delete '/session', to: "sessions#signout"
 
   resources :users, only: [:show, :new, :create]
-  resources :attractions, only: [:index, :show, :new, :create]
+  resources :attractions, only: [:index, :show, :new, :edit, :create, :update]
   resources :rides, only: [:create]
 
   root "welcome#home"
