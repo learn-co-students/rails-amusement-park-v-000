b8e0b082d9ad45e0e9336dfaaa5d4f552dd459da
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-08-26 22:53:02 -0400

    Implement attractions#create

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -11,4 +11,20 @@ class AttractionsController < ApplicationController
   def new
     @attraction = Attraction.new
   end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+
+    if @attraction.save
+      redirect_to @attraction
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+  end
 end
