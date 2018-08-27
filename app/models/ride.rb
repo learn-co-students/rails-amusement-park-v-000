de6e35ea44cffc9c3c4135f0c0765ba7a8c4e48d
Sara Stanton <sara.mpa13@gmail.com>
2018-08-27 00:11:46 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,6 +3,16 @@ class Ride < ActiveRecord::Base
   belongs_to :user
 
   def take_ride
+    # updates ticket number
+    self.user.tickets -= self.attraction.tickets
+    # updates user's nausea
+    self.user.nausea += self.attraction.nausea_rating
+    # updates user's happiness
+    self.user.happiness += self.attraction.happiness_rating
+    self.user.save
+    return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}." if self.user.height < self.attraction.min_height && self.user.tickets < self.attraction.tickets
+    return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}." if self.user.tickets < self.attraction.tickets
+    return "Sorry. You are not tall enough to ride the #{self.attraction.name}." if self.user.height < self.attraction.min_height
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
   has_many :attractions, through: :rides
 
   def mood
+    if self.nausea && self.happiness
+      case
+      when self.happiness > self.nausea
+        return "happy"
+      when self.happiness < self.nausea
+        return "sad"
+      end
+    end
   end
 
 end
