836baca2130f1ecbc402423ae679057a3a250588
Sara Stanton <sara.mpa13@gmail.com>
2018-08-28 02:08:02 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,2 +1,11 @@
 class AttractionsController < ApplicationController
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def show
+    @attraction = Attraction.find_by(params[:id])
+    @ride = Ride.find_by(attraction_id: @attraction.id)
+  end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,11 +4,11 @@ class Ride < ActiveRecord::Base
 
   def take_ride
     # updates ticket number
-    self.user.tickets -= self.attraction.tickets
+    self.user.update(tickets: self.user.tickets -= self.attraction.tickets)
     # updates user's nausea
-    self.user.nausea += self.attraction.nausea_rating
+    self.user.update(nausea: self.user.nausea += self.attraction.nausea_rating)
     # updates user's happiness
-    self.user.happiness += self.attraction.happiness_rating
+    self.user.update(happiness: self.user.happiness += self.attraction.happiness_rating)
     self.user.save
     return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}." if self.user.height < self.attraction.min_height && self.user.tickets < self.attraction.tickets
     return "Sorry. You do not have enough tickets to ride the #{self.attraction.name}." if self.user.tickets < self.attraction.tickets
