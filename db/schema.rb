d2b21357e33776aab083e06d25c687120c2c4f84
Suchitra Naik <susanaik@gmail.com>
2018-08-28 18:29:59 +0000

    add column admin,add method mood and take_ride

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_171544) do
+ActiveRecord::Schema.define(version: 2018_08_28_180212) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_171544) do
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
-    t.boolean "admin"
+    t.boolean "admin", default: false
   end
 
 end
