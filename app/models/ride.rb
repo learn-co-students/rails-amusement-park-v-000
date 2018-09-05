28181128c392c7ad5a1e6a480521b99721a20c34
arianaberger <ariana.berger@gmail.com>
2018-09-05 14:26:21 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,23 @@
+require 'pry'
+
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    user = User.find(self.user_id)
+    attraction = Attraction.find(self.attraction_id)
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    elsif user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    elsif user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    else
+      user.tickets = user.tickets - attraction.tickets
+      user.nausea = user.nausea + attraction.nausea_rating
+      user.happiness = user.happiness + attraction.happiness_rating
+      user.save
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  
+
+  root 'welcome#home'
+
+  resources :users
+  resources :sessions, only: [:create, :show, :destroy]
+
+  get '/signin' => 'sessions#new'
+
+
 end
