83549878ac999181c959eb7c146f23f19cef6dae
NeedsAUsername <kevinchi118@gmail.com>
2018-09-06 17:39:17 -0400

    all users can log in and log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,9 @@ class SessionsController < ApplicationController
     end
   end
 
-  private
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
