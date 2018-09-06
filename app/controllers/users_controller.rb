958e7c1c36074072ae08516e8f6b0049b6ca4cb6
NeedsAUsername <kevinchi118@gmail.com>
2018-09-06 14:20:59 -0400

    User can sign in

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+  def show
+    if session[:user_id].present?
+      @user = User.find(session[:user_id])
+      @attributes = attributes_list.delete_if {|att, v| att == "password" || att == "name"}
+    else
+      redirect_to new_user_path
+    end
+  end
+
+  def new
+    @user = User.new
+    @attributes = attributes_list
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :happiness, :nausea, :height, :tickets, :admin)
+  end
+
+  def attributes_list
+    {"name"=>nil, "password"=>nil, "happiness"=>nil, "nausea"=>nil, "height"=>nil, "tickets"=>nil, "admin"=>false}
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+  resources :users, only: [:new, :create, :show]
+
+  root to: 'users#show'
 end
