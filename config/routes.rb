679ea96083b724f447327652240ac2cdeab85ea7
Suchitra Naik <susanaik@gmail.com>
2018-09-08 12:35:04 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'attractions/new'
+  get 'attractions/create'
+  get 'attractions/edit'
+  get 'attractions/update'
+  get 'attractions/destroy'
+  get 'attractions/index'
+  get 'attractions/show'
   root 'application#home'
   get '/signin', to: 'sessions#sign_in'
   post '/signin', to: 'sessions#create'
