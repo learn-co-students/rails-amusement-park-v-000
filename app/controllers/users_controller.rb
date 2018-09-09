79b71e4285c22a7c41aaa71bf09db5fc640ac54d
Yina Zhu <yinagit@gmail.com>
2018-09-09 00:16:01 +0000

    sessions, users, new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,21 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    respond_to do |format|
-      if @user.save
-        sessions[:user_id] = @user.id
-        format.html { render :new }
-      end
-    end
+    return render :new unless user.save
+
+    session[:user_id] = user.id
+    redirect_to user_path(current_user)
   end
 
-  
+  def show
+    @user = User.find(params[:id])
+    current_user ? (render :show) : (redirect_to root_path)
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :height, :admin, :tickets, :nausea, :happiness)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root "static_pages#home"
 
+  get 'signin' => 'sessions#new'
+  post 'signin' => 'sessions#create'
+  post 'logout' = 'sessions#destroy'
+
   resources :users
 
 end
