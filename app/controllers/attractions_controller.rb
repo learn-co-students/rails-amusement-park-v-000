829c5c0abfd526cee16af4a7e3ee03eebdce742f
Yina Zhu <yinagit@gmail.com>
2018-09-09 21:56:15 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,4 +1,5 @@
 class AttractionsController < ApplicationController
+  before_action :set_attraction, only: [:show, :edit, :update]
 
   def new
     @attraction = Attraction.new
@@ -10,4 +11,29 @@ class AttractionsController < ApplicationController
       redirect_to attraction_path(@attraction)
     end
   end
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def update
+    if current_user.admin
+      @attraction.update(attraction_params)
+      redirect_to attraction_path(@attraction)
+    else
+      redirect_to user_path(current_user)
+    end
+  end
+
+private
+
+  def set_attraction
+    @attraction = Attraction.find_by(id: params[:id])
+  end
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
+  end
+
+
 end
