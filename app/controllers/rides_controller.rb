bceaf9beaed6a08007cc31f2f7c82a4752e8bdd8
Shirley Collins <shirley@m104.us>
2018-09-13 21:33:20 -0700

    updates

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,2 +1,11 @@
 class RidesController < ApplicationController
+  def create
+    attraction = Attraction.find(params[:attraction_id])
+    user = User.find(session[:user_id])
+
+    @ride = Ride.create(:user_id => user.id, :attraction_id => attraction.id)
+    @message = @ride.take_ride
+
+    redirect_to user_path(user.id)
+  end
 end
