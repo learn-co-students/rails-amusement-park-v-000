24da119d27d890c601f369ce47a76460de96f354
ryanwevans <ryanwevans@icloud.com>
2018-09-16 17:25:07 -0700

    move #take_ride to RidesController

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,7 +6,28 @@ class RidesController < ApplicationController
 
   def create
     @ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
+    @ride.take_ride
     redirect_to user_path(@ride.user)
   end
+
+  private
+
+  def take_ride
+    @user = self.user
+    @attraction = self.attraction
+    if (@user.height < @attraction.min_height) && (@user.tickets < @attraction.tickets)
+      return "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{@attraction.name}."
+    elsif @user.tickets < @attraction.tickets
+      return "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
+    elsif @user.height < @attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{@attraction.name}."
+    else
+      @new_tickets = (@user.tickets - @attraction.tickets)
+      @new_nausea = (@user.nausea + @attraction.nausea_rating)
+      @new_happiness = (@user.happiness + @attraction.happiness_rating)
+      @user.update(:tickets => @new_tickets, :nausea => @new_nausea, :happiness => @new_happiness)
+    end
+  end
+
 
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,21 +2,4 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
   belongs_to :user
 
-  def take_ride
-    user = User.find_by(id: self.user_id)
-    attraction = Attraction.find_by(id: self.attraction_id)
-    if (user.height < attraction.min_height) && (user.tickets < attraction.tickets)
-      return "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
-    elsif user.tickets < attraction.tickets
-      return "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-    elsif user.height < attraction.min_height
-      return "Sorry. You are not tall enough to ride the #{attraction.name}."
-    else
-      new_tickets = (user.tickets - attraction.tickets)
-      new_nausea = (user.nausea + attraction.nausea_rating)
-      new_happiness = (user.happiness + attraction.happiness_rating)
-      user.update(:tickets => new_tickets, :nausea => new_nausea, :happiness => new_happiness)
-    end
-  end
-
 end
