cc60c2fed5471b3ae5ffecf438093986529b17e2
github username <github email address>
2018-09-20 16:17:27 -0500

    Done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find(params[:user][:name])
+        @user.try(:authenticate, params[:password])
+        if @user
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to signin_path
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to '/'
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+    root 'sessions#new'
+    resources :users
+    resources :attractions
+    resources :rides, only: [:new, :create]
+    get '/signin' => 'sessions#new'
+    post '/signin' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
 end
