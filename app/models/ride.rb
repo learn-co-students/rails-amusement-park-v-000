0426a1df9cce03fe61f0b2f095566c47e367c397
Aly Alidina <aly_alidina@hotmail.com>
2018-09-23 13:17:07 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,18 +5,18 @@ class Ride < ActiveRecord::Base
   def take_ride
     @user = self.user
     @attraction = self.attraction
-    if (@user.height < @attraction.min_height) && (@user.tickets < @attraction.tickets)
-      return "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{@attraction.name}."
+    if @user.height < @attraction.min_height && @user.tickets < @attraction.tickets
+       "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{@attraction.name}."
     elsif @user.tickets < @attraction.tickets
-      return "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
+       "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
     elsif @user.height < @attraction.min_height
-      return "Sorry. You are not tall enough to ride the #{@attraction.name}."
+       "Sorry. You are not tall enough to ride the #{@attraction.name}."
     else
-      @new_tickets = (@user.tickets - @attraction.tickets)
-      @new_nausea = (@user.nausea + @attraction.nausea_rating)
-      @new_happiness = (@user.happiness + @attraction.happiness_rating)
+      @new_tickets = @user.tickets - @attraction.tickets
+      @new_nausea = @user.nausea + @attraction.nausea_rating
+      @new_happiness = @user.happiness + @attraction.happiness_rating
       @user.update(:tickets => @new_tickets, :nausea => @new_nausea, :happiness => @new_happiness)
-      return "Thanks for riding the #{@attraction.name}!"
+      "Thanks for riding the #{@attraction.name}!"
     end
   end
 
