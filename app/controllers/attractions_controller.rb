bceecac2fb4b70e4b16eef1059316178e561faa1
Ariel Zhen <pzhen001@g.ucla.edu>
2018-10-06 18:21:58 -0700

    done

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,36 @@
+class AttractionsController < ApplicationController
+
+  def index
+    @attractions = Attraction.all
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  def show
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def edit
+    @attraction = Attraction.find_by(id: params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find_by(id: params[:id])
+    @attraction.update(attraction_params)
+    redirect_to @attraction
+  end
+  
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+  end
+
+end
---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,9 @@
+class RidesController < ApplicationController
+
+  def create
+    @attraction = Attraction.find_by(id: params[:attraction_id])
+    @ride = current_user.rides.create(attraction: @attraction)
+    flash[:message] = @ride.take_ride
+    redirect_to user_path(@user)
+  end
+end
