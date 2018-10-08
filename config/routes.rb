d970b34e271f791ee855c76fce4785c455bf0a91
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-10-08 18:31:02 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :attractions
 
-  get '/login', to: 'users#login'
+  get '/signin', to: 'users#login'
 
   root 'users#welcome'
 end
