c559c94832e0ff94a8cc2cf65d84c29968d29d2b
pracniscate <lorena.giddens@outlook.com>
2018-10-13 20:02:49 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :current_user
+  before_action :require_login
+
+  def current_user
+    @user = User.find_by(id: session[:user_id]) || User.new
+  end
+  
+  helper_method :current_user
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    redirect_to root_path unless logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  
+
+    root "welcome#homepage"
+
+    get "/signin", to: "sessions#new"
+
+    post "/sessions/create", to: "sessions#create"
+
+    delete "/signout", to: "sessions#destroy"
+
+    resources :users
+
+    resources :attractions
 end
