2fd82effbe3e7bc628fbbfc194674831ca43a662
Everett <everett.bowline@gmail.com>
2018-10-16 19:31:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def logged_in?
+    !!session[:user_id]
+  end
+
+  def current_user
+    User.find_by(id: session[:user_id]) if session[:user_id]
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  
+  root 'users#new'
+  get '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
+  resources :attractions
+  resources :users
 end
