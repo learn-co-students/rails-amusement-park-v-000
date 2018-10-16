fb0907f84f949bdec2fe0c3d774d86b3c5f262e7
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-15 20:52:27 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    binding.pry
+  
     @user = User.new(user_params)
 
     if @user.save
--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2018_10_15_224542) do
     t.integer "happiness"
     t.integer "tickets"
     t.integer "height"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
