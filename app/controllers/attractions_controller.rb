9939647a1f60f0a5a648ce5dadac67e787a34ffc
github username <github email address>
2018-10-16 17:35:38 -0400

    Attraction new and edit

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,11 +1,12 @@
 class AttractionsController < ApplicationController
-  before_action :set_attaction, only: [:show, :edit, :update, :destroy]
+  before_action :set_attraction, only: [:show, :edit, :update, :destroy]
 
   def index
     @attractions = Attraction.all
   end
 
   def show
+    @ride = Ride.new
   end
 
   def new
@@ -22,5 +23,34 @@ class AttractionsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    if @attraction.update(attraction_params)
+      redirect_to attraction_path(@attraction)
+    else
+      render :edit
+    end
   end 
+
+  def destroy
+    @attraction.destroy
+    redirect_to attractions_path
+  end
+
+  private
+
+  def set_attraction
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def attraction_params
+    params.require(:attraction).permit(
+      :name,
+      :tickets,
+      :nausea_rating,
+      :happiness_rating,
+      :min_height
+    )
+  end
 end
---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,10 @@
+class RidesController < ApplicationController
+
+  def new
+    @ride = Ride.create(
+      :user_id => params[:user_id],
+      :attraction_id => params[:attraction_id]
+    )
+    redirect_to user_path(@ride.user)
+  end
+end
