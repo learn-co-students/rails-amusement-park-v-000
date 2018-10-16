419ce64f04f6c9e894ea295fb7cd49b539d317ac
veloblank <veloblank@gmail.com>
2018-10-16 08:50:39 -0400

    Passing model tests.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,22 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+
+  def take_ride
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      user.happiness += attraction.happiness_rating
+      user.nausea += attraction.nausea_rating
+      user.tickets -= attraction.tickets
+      user.save
+    end
+
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :rides
+  has_many :attractions, through: :rides
+  validates :password, presence: true
+  
+  
+  def mood
+    if nausea > happiness 
+      "sad"
+    else
+      "happy"
+    end    
+  end
 end
