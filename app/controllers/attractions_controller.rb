1ee0abe3794433838d4e7103c63903596de5bdbf
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-16 22:01:09 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,8 +1,10 @@
 class AttractionsController < ApplicationController
+
   def index
     @attractions = Attraction.all
   end
 
   def show
+    @attraction = Attraction.find(params[:id])
   end
 end
