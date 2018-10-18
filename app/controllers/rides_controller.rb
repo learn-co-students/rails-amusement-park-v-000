6d967b0bc741914a14e5115c8a48cd161ea0a00d
BrettSWeisberg <weisberg.brett@gmail.com>
2018-10-17 23:27:08 -0400

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,17 +2,16 @@ class RidesController < ApplicationController
   def create
 
     @ride = Ride.create(ride_params)
-    @ride.take_ride
+  
+    flash[:message] = @ride.take_ride
 
-    #if @ride.user.tickets < @ride.attraction.tickets && @ride.user.height < @ride.attraction.min_height
-    #  flash[:notice] = "Sorry. You do not have enough tickets to ride the #{@ride.attraction.name}. You are not tall enough to ride the #{@attraction.name}."
-    if @ride.user.tickets < @ride.attraction.tickets
-      flash[:notice] =  "You do not have enough tickets to ride the #{@ride.attraction.name}".
-    elsif @ride.user.height < @ride.attraction.min_height
-      flash[:notice] =   "You are not tall enough to ride the #{@ride.attraction.name}"
-    else
-       flash[:notice] = "Thanks for riding the #{@ride.attraction.name}!"
-    end
+  #if @ride.user.tickets < @ride.attraction.tickets
+  #    flash[:notice] =  "You do not have enough tickets to ride the #{@ride.attraction.name}"
+  #  elsif @ride.user.height < @ride.attraction.min_height
+  #    flash[:notice] =   "You are not tall enough to ride the #{@ride.attraction.name}"
+  #  else
+  #     flash[:notice] = "Thanks for riding the #{@ride.attraction.name}!"
+  #  end
       redirect_to user_path(@ride.user)
     end
 
