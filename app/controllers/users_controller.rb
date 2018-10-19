5b0577f165c41c8ad6d80deaf46bd4a793de3c32
Wasif Khan <waasif.khaan@gmail.com>
2018-10-19 05:03:55 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  def index
+  end
+  def show
+  end
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :password_confirmation)
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  
+  root 'users#new'
   resources :rides
   resources :users
+
 end
