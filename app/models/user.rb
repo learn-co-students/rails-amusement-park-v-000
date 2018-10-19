911ed383c9f164b753c6d0a3b46a7fffcd59d93c
github username <github email address>
2018-10-19 02:16:53 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
 
   has_many :rides
   has_many :attractions, through: :rides
+
+  def mood
+    if self.happiness && self.nausea
+      mood = self.happiness - self.nausea
+      mood >= 0 ? "happy" : "sad"
+    end
+  end
 end
