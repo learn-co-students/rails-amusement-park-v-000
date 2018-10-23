7e7103e5753999b1d30ec780726507730bbc7276
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-23 08:33:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :verify_user_authenticated
+  skip_before_action :verify_user_authenticated, only:[:home]
+  helper_method :current_user
+
+  def home
+  end
+
+
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+
+  def user_authenticated
+    !!current_user
+  end
+
+  def verify_user_authenticated
+    redirect_to '/' unless user_authenticated
+  end
+
 end
---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,20 @@
+class RidesController < ApplicationController
+
+
+  def create
+
+    ride = Ride.create(ride_params)
+    message = ride.take_ride
+    redirect_to user_path(session[:user_id]), flash: {message: message}
+  end
+
+
+
+  private
+
+  def ride_params
+    params.require(:ride).permit(:user_id, :attraction_id)
+  end
+
+
+end
