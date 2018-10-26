89489e2756dae2c0bd5f58116bf214f7c84fae23
epic0448 <samuelogundiran@Yahoo.com>
2018-10-26 16:33:41 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,15 @@
 class Ride < ActiveRecord::Base
+    belongs_to :attraction
+    belongs_to :user
+
+    validates :user_id, :presence => true
+    validates :attraction_id, :presence => true
+
+    def take_ride
+        #accounts for the user not having enough ticket & user not tall enough
+        # Updates ticket number 
+        #update Users Nausea
+        #update user happiness
+        
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    validates :name, :presence => true
+    validates :min_height, :presence => true
+    validates :nausea, :presence => true
+    validates :height, :presence => true
+    validates :happiness, :presence => true
+    validates :tickets, :presence => true
+
+    def mood
+        # returns 'sad' when the user is more nauseous than happy
+    # else
+    # returns 'happy' when the user is more happy than nauseous
+
+    end
 end
