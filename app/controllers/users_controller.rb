31d1efaa2ec219741c4792e2a11e686be3a9c8a0
BrentBauman86 <brentbauman86@gmail.com>
2018-10-28 13:03:04 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+
+    if @user.save 
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user) 
+
+    else
+      params[:user_params].empty?
+      redirect_to "users#new", :notice => "Please fill in the blanks"
+    end 
   end
 
   def show
@@ -24,7 +33,7 @@ class UsersController < ApplicationController
   private 
 
   def user_params
-    params.require(:user).permit(:name, :password_digest, :nausea, :happiness, :tickets, :height, :admin)
+    params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
   end
 end
 
