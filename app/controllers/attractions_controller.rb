e2824e5760db0111f584220565876d2d079d1351
github username <github email address>
2018-10-28 23:06:10 -0400

    Reformatted

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,85 +2,43 @@ require 'pry'
 
 class AttractionsController < ApplicationController
 
-
-  # def new
-  #   @attraction = Attraction.new
-  #     if session[:user_id]
-  #       @user = User.find_by_id(session[:user_id])
-  #         if @user.admin
-  #           render "new"
-  #         end
-  #     end
-  # end
-  #
-  # def create
-  #   @attraction = Attraction.create(attraction_params)
-  #   redirect_to "/attractions/#{@attraction.id}"
-  # end
-  #
-  # def index
-  #   if !!session[:user_id]
-  #     @user = User.find_by_id(params[:id])
-  #     @attractions = Attraction.all
-  #   else
-  #     redirect_to '/'
-  #   end
-  # end
-  #
-  # def show
-  #   @attraction = Attraction.find_by_id(params[:id])
-  #   @user = User.find_by_id(session[:user_id])
-  #   @ride = Ride.create(user_id: @user.id, attraction_id: @attraction.id)
-  # end
-  #
-  # def edit
-  #   @attraction = Attraction.find_by_id(params[:id])
-  # end
-  #
-  # def update
-  #       @attraction = Attraction.find_by_id(params[:id])
-  #       @attraction.update(attraction_params)
-  #       @attraction.save
-  #       redirect_to "/attractions/#{@attraction.id}"
-  #   end
-  #
-  # private
-  #
-  #    def attraction_params
-  #       params.require(:attraction).permit(:name,:nausea_rating, :happiness_rating, :tickets, :min_height)
-  #    end
-
   def index
-      @attractions = Attraction.all
-      @user = current_user
-    end
-     def show
-      @attraction = Attraction.find(params[:id])
-    end
-     def new
-      @attraction = Attraction.new
-    end
-     def create
-      @attraction = Attraction.new(attraction_params)
+    @attractions = Attraction.all
+    @user = current_user
+  end
+
+  def show
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
       if @attraction.save
         redirect_to attraction_path(@attraction)
       else
         redirect_to new_attraction_path
       end
-    end
-     def edit
+  end
+
+  def edit
       @attraction = Attraction.find(params[:id])
-    end
-     def update
-      @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
       if @attraction.update(attraction_params)
         @attraction.save
         redirect_to attraction_path(@attraction)
       else
         render :edit
       end
-    end
-     private
+  end
+
+  private
        def attraction_params
         params.require(:attraction).permit(
             :name,
@@ -89,5 +47,5 @@ class AttractionsController < ApplicationController
             :happiness_rating,
             :nausea_rating
           )
-      end
+       end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,67 +1,30 @@
 class UsersController < ApplicationController
 
-  # before_action :authenticate_user, only: [:show]
-
-  # def new
-  #   @user = User.new
-  # end
-  #
-  # def create
-  #   @user= User.create(user_params)
-  #   session[:user_id]= @user.id
-  #
-  #     if params[:user][:admin] == 1
-  #         redirect_to "/users/#{@user.id}"
-  #     else
-  #         redirect_to "/users/#{@user.id}"
-  #     end
-  # end
-  #
-  # def show
-  #   if (session[:user_id]==params[:id].to_i)
-  #     @user = User.find(session[:user_id])
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
-  #
-  # def update
-  #   @ride = Ride.find_by_id(params[:id])
-  #   flash[:message] = "#{@ride.take_ride}"
-  #   @user = @ride.user
-  #     if @user.save
-  #       redirect_to @user
-  #     else
-  #       render "/attractions"
-  #     end
-  # end
-  #
-  # private
-  #
-  # def user_params
-  #   params.require(:user).permit(:name, :password, :happiness, :nausea, :height, :tickets)
-  # end
   def new
-      @user = User.new
+    @user = User.new
+  end
+
+  def show
+    if session[:user_id]
+      @user = User.find(params[:id])
+    else
+      redirect_to '/'
     end
-     def show
-      if session[:user_id]
-        @user = User.find(params[:id])
-      else
-        redirect_to '/'
-      end
-    end
-     def create
-      @user = User.new(user_params)
+  end
+
+  def create
+    @user = User.new(user_params)
       if @user.save
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
          render 'new'
       end
-    end
-     private
+  end
+
+  private
+
      def user_params
       params.require(:user).permit(:name, :password, :height, :tickets, :happiness, :nausea, :admin)
-    end
+     end
 end
