b7e87f8ba1011848b7626194e07ca6207626dbe6
Anu Khambete <anukhambete@gmail.com>
2018-10-29 22:25:30 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
+require 'pry'
 class SessionsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    #binding.pry
+    @user = User.find_by(id: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions
   root 'users#new'
    get 'signin' => 'sessions#new'
 end
