484e1b0192774e1c73906ea4481d99f2684950aa
Anu Khambete <anukhambete@gmail.com>
2018-10-29 20:48:48 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
+require 'pry'
 class User < ApplicationRecord
   has_secure_password
   has_many :rides
   has_many :attractions, through: :rides
 
+  def mood
+    #binding.pry
+    user = self
+    if user.nausea > user.happiness
+      "sad"
+    elsif user.nausea < user.happiness
+      "happy"
+    end
+
+  end
 
 end
