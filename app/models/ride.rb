ffc6f06a397cdd9f6d95a69fcaae3e49bff1721e
BrentBauman86 <brentbauman86@gmail.com>
2018-10-29 13:05:30 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,9 +2,44 @@ class Ride < ActiveRecord::Base
     belongs_to :user 
     belongs_to :attraction 
 
-    def take_ride
-      
-        end
+    def take_ride 
+        user_tickets = user_requirements
+        user_height = user_requirements
+      if user_tickets && user_height 
+        begin_ride 
+      elsif 
+        !user_tickets && user_height 
+        "Sorry partner, you broke AF"
+      elsif 
+        user_tickets && !user_height 
+        "Sorry pip squeak, you blow"
+      else 
+        "Super Sorry, you suck doubly hard!" 
+      end 
+    end 
+
+    def user_requirements
+        user_tickets = true 
+        user_height = true 
+        if user.tickets < attraction.tickets 
+            user_tickets = false
+        end 
+
+        if user.height < attraction.min_height
+            user_height = false 
+        end   
+
+        return [user_tickets, user_height]
+    end
+
 
         
+    def begin_ride 
+        updated_happiness = user.happiness + attraction.happiness_rating
+        updated_nausea = user.nausea + attraction.nausea_rating
+        updated_tickets = user.tickets + attraction.tickets 
+        user.update(happiness: "updated_happiness",
+        nausea: "updated_nausea",
+        tickets: "updated_tickets")
+    end
 end
