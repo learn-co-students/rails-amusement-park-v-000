d05f56cd5082605a2a80ad0bb8f59d01f6c5e205
Anu Khambete <anukhambete@gmail.com>
2018-10-30 15:33:35 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -3,6 +3,7 @@ class AttractionsController < ApplicationController
 
   def index
     @attractions = Attraction.all
+    @user = User.find_by(id: session[:user_id])
   end
 
   def show
@@ -12,6 +13,22 @@ class AttractionsController < ApplicationController
   end
 
   def new
+      @attraction = Attraction.new
+  end
+
+  def create
+    #binding.pry
+    @user = User.find_by(id: session[:user_id])
+    if @user.admin
+      @attraction = Attraction.create(att_params)
+      redirect_to attraction_path(@attraction)
+    end
+  end
+
+  private
+
+  def att_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 
 end
