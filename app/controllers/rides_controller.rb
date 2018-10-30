d6de145b902c07d744d51abdb609b530ab732f80
Anu Khambete <anukhambete@gmail.com>
2018-10-30 02:23:20 +0000

    Done.

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,6 @@
+require 'pry'
+class RidesController < ApplicationController
+  def create
+    #binding.pry
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions
   resources :attractions
+  resources :rides
   root 'users#new'
    get 'signin' => 'sessions#new'
    get 'logout' => 'sessions#logout'
