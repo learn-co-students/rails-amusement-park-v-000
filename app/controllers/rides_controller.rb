6aac75741d641ea8366f119aebcc2ca32c47f97f
Anu Khambete <anukhambete@gmail.com>
2018-10-30 03:26:51 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,10 +1,22 @@
 require 'pry'
+require 'rack-flash'
+
 class RidesController < ApplicationController
   def create
     #binding.pry
     @ride = Ride.create(attraction_id: params[:att_id], user_id: session[:user_id])
-    @ride.take_ride
+    @attraction = Attraction.find_by(id: params[:att_id])
     @user = User.find_by(id: session[:user_id])
-    redirect_to user_path(@user)
+    message = @ride.take_ride
+    #binding.pry
+      if message == true
+        flash[:notice] = "Thanks for riding the #{@attraction.name}!"
+        redirect_to user_path(@user)
+      else
+        flash[:notice] = message
+        redirect_to user_path(@user)
+      end
   end
+
+
 end
