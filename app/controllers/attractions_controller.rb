07935d23b3ff84d0347a1aea46f6475ef81ef800
Anu Khambete <anukhambete@gmail.com>
2018-10-30 02:05:31 +0000

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,7 +1,14 @@
+require 'pry'
 class AttractionsController < ApplicationController
 
   def index
     @attractions = Attraction.all
+  end
+
+  def show
+    #binding.pry
+    @attraction = Attraction.find_by(id: params[:id])
+    #redirect_to attraction_path(@attraction)
   end
 
 end
