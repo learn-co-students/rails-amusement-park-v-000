49a94d1ad6443fbf30c93cffc89409731c0b8d90
Anu Khambete <anukhambete@gmail.com>
2018-10-30 03:00:27 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,6 +1,10 @@
 require 'pry'
 class RidesController < ApplicationController
   def create
-    binding.pry
+    #binding.pry
+    @ride = Ride.create(attraction_id: params[:att_id], user_id: session[:user_id])
+    @ride.take_ride
+    @user = User.find_by(id: session[:user_id])
+    redirect_to user_path(@user)
   end
 end
