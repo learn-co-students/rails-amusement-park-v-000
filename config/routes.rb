44dd2c0ad834fcd0f5e498a42a75c4b8f209d02f
Anu Khambete <anukhambete@gmail.com>
2018-10-30 01:44:25 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   resources :sessions
+  resources :attractions
   root 'users#new'
    get 'signin' => 'sessions#new'
    get 'logout' => 'sessions#logout'
