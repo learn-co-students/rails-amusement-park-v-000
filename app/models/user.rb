a94ec016986f3382e79df91a2ca3f25d13cade20
epic0448 <samuelogundiran@Yahoo.com>
2018-11-01 13:07:33 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
         if self.nausea && self.happiness
         # returns 'sad' when the user is more nauseous than happy     
             if  nausea > happiness
+                binding.pry
                     return "sad"
             else
                     return "happy"
