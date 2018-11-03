1fae1a3bf5e6dfa3e0b68c8c44efa41f7073af23
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-11-02 20:25:00 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  
+  root 'sessions#home'
+  # get 'attractions/index'
+  # get 'attractions/new'
+  # get 'attractions/edit'
+  # get 'attractions/delete'
+  # get 'attractions/show'
+  # get 'users/new' => 'users#new'
+  # get 'users/:id' => 'users#show'
+  resources :users
+  resources :attractions
+  resources :sessions
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/logout' => 'users#destroy'
+  patch '/users/update' => 'users#update'
 end
