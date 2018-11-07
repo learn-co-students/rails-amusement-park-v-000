0db1f469d501931031397c40e7714ba81d3aabae
bkhartmire <bkhartmire@gmail.com>
2018-11-07 22:59:13 +0900

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   resources :attractions
   resources :users
   root 'welcome#home'
-  get '/signin': 'sessions#new'
-  post '/signin': 'sessions#create'
-  delete '/signout': 'sessions#destroy'
-  get '/rides/new' => 'rides#new'
-  post '/rides/new' => 'rides#create'
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+  get "/rides/new", to: "rides#new"
 end
