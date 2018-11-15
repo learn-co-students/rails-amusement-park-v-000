e425bf9491221bfdfd1686afe4a9642ee62f5608
emanuelbierman <emanuel.bierman@gmail.com>
2018-11-15 12:27:47 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if self.happiness > self.nausea
+      puts 'happy'
+    elsif self.nausea > self.happiness
+      puts 'sad'
+    end
+  end
 end
