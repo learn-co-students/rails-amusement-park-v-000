017114562df1600ffbdc7fa4f769e2e0a7b5b9f3
btmccollum <btmcco8@gmail.com>
2018-11-16 20:23:55 -0600

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,15 @@
 class Ride < ActiveRecord::Base
+    belongs_to :attraction
+    belongs_to :user
+
+    # def take_ride
+    
+    # end
+
+    # accounts for the user not having enough tickets
+    # accounts for the user not being tall enough
+    # accounts for the user not being tall enough and not having enough tickets
+    # updates ticket number
+    # updates the user's nausea
+    # updates the user's happiness
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+    has_many :rides
+    has_many :attractions, through: :rides
+
+    has_secure_password
+
+    def mood
+        self.happiness > self.nausea ? "happy" : "sad"
+    end
 end
