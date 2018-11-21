ab625c68d13090c571756c9426bbbf682e4eecbd
emanuelbierman <emanuel.bierman@gmail.com>
2018-11-21 12:43:39 -0500

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,91 @@
+class UsersController < ApplicationController
+
+  # GET /users
+  # GET /users.json
+  # def index
+  #   @users = User.all
+  # end
+
+  # GET /users/1
+  # GET /users/1.json
+
+  def index
+    render 'index'
+  end
+
+  def show
+    # if current_user is the same as @user
+    @user = User.find_by(id: params[:id])
+    if session[:user_id] == @user.id
+      render 'show'
+    else
+      redirect_to "/"
+    end
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  # def edit
+  # end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      session[:user_id] = @user.id
+      redirect_to "/users/#{@user.id}"
+    else
+      redirect_to '/users/new'
+    end
+
+    # respond_to do |format|
+    #   if @user.save
+    #     format.html { redirect_to @user, notice: 'User was successfully created.' }
+    #   else
+    #     format.html { render :new }
+    #   end
+    # end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  # def update
+  #   respond_to do |format|
+  #     if @user.update(user_params)
+  #       format.html { redirect_to @user, notice: 'User was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @user }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  # def destroy
+  #   @user.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
+  def user_params
+    params.require(:user).permit(:name, :password, :tickets, :nausea, :happiness, :height, :admin)
+  end
+  # private
+    # Use callbacks to share common setup or constraints between actions.
+    # def set_user
+    #   @user = User.find(params[:id])
+    # end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    # def user_params
+    #   params.fetch(:user, {:name, :password, :tickets, :nausea, :happiness, :height, :admin})
+    # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  
+  root 'users#index'
+  get "/signin", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
+  resources :users, only: [:new, :create, :show]
+  resources :attractions, only: [:new, :show]
 end
