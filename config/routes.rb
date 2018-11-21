8c797f1c1cf294094edcac03b52beb672de0130e
emanuelbierman <emanuel.bierman@gmail.com>
2018-11-21 14:17:26 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
   get "/signin", to: "sessions#new"
   post "/sessions/create", to: "sessions#create"
-  delete "/signout", to: "sessions#destroy"
+  get "/signout", to: "sessions#destroy"
   resources :users, only: [:new, :create, :show]
   resources :attractions, only: [:new, :show]
 end
