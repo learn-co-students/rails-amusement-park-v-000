630a170689acf9a44a9bd8da4119e9057859634c
Daniel <bluh@outlook.com>
2018-11-23 16:24:18 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:user][:name])
+    user = User.find_by(name: params[:user_name])
     if user && user.authenticate(params[:user][:password])
       session[:user_id] = user.id
       redirect_to user_path(user)
