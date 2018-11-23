77328d94ffa1c2167f154a53c8f5746b8b171ffa
Daniel <bluh@outlook.com>
2018-11-23 18:17:26 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,10 +1,10 @@
 class AttractionsController < ApplicationController
   def index
     @attractions = Attraction.all
-    @user = User.find(session[:user_id])
   end
 
   def show
     @attraction = Attraction.find(params[:id])
+    @user = User.find(session[:user_id])
   end
 end
