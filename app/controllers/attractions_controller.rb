eb632c41dbe0073b4986dc27fe88d70a24df1232
Daniel <bluh@outlook.com>
2018-11-23 19:55:54 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -11,5 +11,17 @@ class AttractionsController < ApplicationController
   def ride
     @attraction = Attraction.find(params[:id])
     @user = User.find(session[:user_id])
+    ride = Ride.new
+    ride.attraction = @attraction
+    ride.user = @user
+    ride.save
+    complete_ride = ride.take_ride
+    if complete_ride.is_a?(String)
+      flash[:notice] = complete_ride
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Thanks for riding the #{@attraction.name}!"
+      redirect_to user_path(@user)
+    end
   end
 end
