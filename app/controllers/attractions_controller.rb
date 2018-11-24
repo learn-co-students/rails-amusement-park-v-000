9e7623076f9367ef3f511457a4b352f2ba0e552f
Daniel <bluh@outlook.com>
2018-11-23 20:29:11 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,6 @@
 class AttractionsController < ApplicationController
   def index
+    @user = User.find(session[:user_id])
     @attractions = Attraction.all
   end
 
@@ -23,5 +24,32 @@ class AttractionsController < ApplicationController
       flash[:notice] = "Thanks for riding the #{@attraction.name}!"
       redirect_to user_path(@user)
     end
+  end
+
+  def new
+  end
+
+  def create
+    attraction = Attraction.new(attraction_params)
+    if attraction && attraction.save
+      redirect_to attraction_path(attraction)
+    else
+      redirect_to new_attraction_path
+    end
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+  end
+
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 end
