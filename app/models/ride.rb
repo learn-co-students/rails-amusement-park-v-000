d42f8f15ce597163a586819a4de262819e1f025a
Brian Birdwell <bbirdwell@gmail.com>
2018-11-30 18:09:53 +0000

    complete all model specs

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,23 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if self.user.tickets < self.attraction.tickets
+      if self.user.height < self.attraction.min_height
+       "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+      else
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+      end
+    elsif self.user.tickets >= self.attraction.tickets
+      if self.user.height < self.attraction.min_height
+        "Sorry. You are not tall enough to ride the #{attraction.name}."
+      else
+        self.user.tickets = self.user.tickets - self.attraction.tickets
+        self.user.nausea = self.user.nausea + self.attraction.nausea_rating
+        self.user.happiness = self.user.happiness + self.attraction.happiness_rating
+        self.user.save
+      end
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+  before_create :not_admin
+
+  def not_admin
+    self.admin = false
+  end
+
+  def mood
+    if self.nausea > self.happiness
+      "sad"
+    elsif self.happiness > self.nausea
+      "happy"
+    end
+  end
 end
