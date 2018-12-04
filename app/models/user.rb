423db14a555581e4662831148f75fb7a727430ec
Kachel Wittig <kjwittig@gmail.com>
2018-12-04 15:35:38 -0600

    set up models and model relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  # include ActiveModel::Validations
+  # validates :name, presence: true
+  # validates :password, presence: true
+  # validates :happiness, presence: true
+  # validates :nausea, presence: true
+  # validates :height, presence: true
+  # validates :tickets, presence: true
+
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    happiness > nausea ? "happy" : "sad"
+  end
 end
