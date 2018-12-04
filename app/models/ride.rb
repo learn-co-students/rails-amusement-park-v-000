1cfe94481dd079d549f9033b1cd5e6fa775f23b3
Kachel Wittig <kjwittig@gmail.com>
2018-12-04 16:17:29 -0600

    add ride helper methods in User model

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,17 @@
+require "pry"
+
 class Ride < ActiveRecord::Base
   belongs_to :user
   belongs_to :attraction
-end
+
+# height must be greater than x?
+# tickets must be greater than y?
+# increase happiness
+# increase nausea
+  def take_ride
+    if user.taller_than? && user.more_tickets_than?
+      rating_increase && ticket_decrease
+    end
+  end
+
+end # class Ride
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,22 @@ class User < ActiveRecord::Base
   def mood
     happiness > nausea ? "happy" : "sad"
   end
+
+  def taller_than?
+    self.height >= 40
+  end
+
+  def more_tickets_than?
+    self.tickets > 1
+  end
+
+  def rating_increase
+    self.happiness += 1
+    self.nausea += 1
+  end
+
+  def ticket_decrease
+    self.tickets -= 1
+  end
+
 end
