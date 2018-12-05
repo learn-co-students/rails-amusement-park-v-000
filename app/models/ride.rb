bab376198a0f899895f95ba8d9ea99a1258a1a16
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-05 15:48:15 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,32 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+
+  end
+
+  def meet_requirements
+    enough_tickets, tall_enough = false
+    if self.user.tickets >= self.attraction.tickets
+      enough_tickets = true
+    end
+    if self.user.height >= self.attraction.min_height
+      tall_enough = true
+    end
+    return [enough_tickets, tall_enough]
+  end
+
+  def start_ride
+    
+  end
+
+  def ticket_issue
+    "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+  end
+
+  def height_issue
+    "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :rides
+  has_many :attractions, :through => :rides
+
+  def mood
+    if self.nausea.to_i > self.happiness.to_i
+      mood = "sad"
+    else
+      self.happiness.to_i > self.nausea.to_i
+      mood = "happy"
+    end
+  end
+
 end
