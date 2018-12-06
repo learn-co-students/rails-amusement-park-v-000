59eb2db4fac70bcf9a055768d82b2a0660e7d4fe
Kachel Wittig <kjwittig@gmail.com>
2018-12-05 21:30:28 -0600

    add create method and strong params to usercontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,22 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
   end
-end
+
+  def new
+    @user = User.new
+    if @user.save
+      # woo saving
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:name, :password, :nausea, :happiness, :tickets, :height)
+    end
+
+end # class UsersController
