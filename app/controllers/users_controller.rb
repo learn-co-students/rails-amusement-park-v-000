4fdd581f2e6a66c22bf834a785ebb73ed0d023f9
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-07 20:55:28 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     if logged_in?
       @user = User.find_by(id: params[:id])
+      @message = 
     else
       redirect_to root_url
     end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get '/signin' => 'sessions#new'
   post '/sessions/create' => 'sessions#create'
   delete "/signout" => 'sessions#destroy'
+  post '/rides/new' => 'rides#new'
 
   resources :users
-  resources :attractions 
+  resources :attractions
 
 end
