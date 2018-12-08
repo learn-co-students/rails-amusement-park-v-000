2c55d86becb4ebe50e0c5d09eb75f6692bc2a1d8
Kachel Wittig <kjwittig@gmail.com>
2018-12-08 02:31:02 -0600

    donzo

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,19 @@
 class AttractionsController < ApplicationController
   def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+    if @attraction.save
+      redirect_to @attraction
+    else
+      render new_attraction_path
+    end
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
   end
 
   def show
@@ -12,5 +26,16 @@ class AttractionsController < ApplicationController
   end
 
   def update
+    @attraction = Attraction.find(params[:id])
+    if @attraction.update(attraction_params)
+      redirect_to @attraction
+    else
+      render edit_attraction_path
+    end
   end
+
+  private
+     def attraction_params
+       params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :tickets, :min_height)
+     end
 end
