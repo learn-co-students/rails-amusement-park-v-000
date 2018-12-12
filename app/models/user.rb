3ad4eeaed37ebd5583757ec87faac23d02f144bc
amitnyc83 <amit_nyc@live.com>
2018-12-12 17:00:54 -0500

    model methods working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
   has_many :rides
-  has_many :attractions, through: :rides 
+  has_many :attractions, through: :rides
   has_secure_password
+
+
+  def mood
+    if self.happiness < self.nausea
+      mood = "sad"
+    else
+     mood = "happy"
+    end
+  end
 end
