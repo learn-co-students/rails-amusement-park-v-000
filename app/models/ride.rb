5025f96665b23e9f3c4b9d7dd8abb16289bd83d9
JerYoMat <jeremy.emata@gmail.com>
2018-12-17 16:41:55 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,45 @@
+
 class Ride < ActiveRecord::Base
+   belongs_to :attraction
+   belongs_to :user
+
+   def take_ride
+     if ride_user.tickets < ride_attraction.tickets && ride_user.height < ride_attraction.min_height
+       sitrep = "too poor and short"
+     elsif ride_user.tickets < ride_attraction.tickets
+       sitrep = "too poor"
+     elsif ride_user.height < ride_attraction.min_height
+       sitrep = "too short"
+     else
+       update_user_after_ride
+     end
+
+     case sitrep
+     when "too poor and short"
+       "Sorry. You do not have enough tickets to ride the #{ride_attraction.name}. You are not tall enough to ride the #{ride_attraction.name}."
+     when "too poor"
+       "Sorry. You do not have enough tickets to ride the #{ride_attraction.name}."
+     when "too short"
+       "Sorry. You are not tall enough to ride the #{ride_attraction.name}."
+     end
+
+
+   end
+
+   def update_user_after_ride
+     ride_user.tickets = ride_user.tickets - ride_attraction.tickets
+     ride_user.nausea += ride_attraction.nausea_rating
+     ride_user.happiness += ride_attraction.happiness_rating
+     binding.pry 
+     ride_user.save
+
+   end
+
+   def ride_user
+     User.find(self.user_id)
+   end
+
+   def ride_attraction
+     Attraction.find(self.attraction_id)
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  
+  resources :users
+  resources :attractions
+  resources :rides 
+
 end
