97be487a240bfd80a7bbea4911dabd866e032cf8
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-24 09:13:43 +0000

    updated create method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
+    @users = User.all
     redirect_to '/' if in_session?
   end
 
   def create
-    @user = User.find_by(name: params[:name])
+    @user = User.find_or_create_by(name: params[:user_name])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
   end
