82fb4c8e51a6898662a7dbedc70faa89690d1250
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-24 06:09:50 +0000

    updated users controller and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,11 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     return redirect_to '/users/new' unless @user.save
     session[:user_id] = @user.id
-    redirect_to '/'
+    redirect_to '/users/<%= @user.id %>'
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root 'application#home'
   get 'users/new' => 'users#new'
   post 'users/new' => 'users#create'
+  resources :users, except: [:new]
+
 end
