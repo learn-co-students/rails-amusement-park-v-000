450b4edac66e5753a2739c01ac3f70c48acd79a2
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-24 06:57:06 +0000

    udpated sessions controller, routes, and views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,17 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    redirect_to '/' if in_session?
+  end
+
   def create
     @user = User.find_by(name: params[:name])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
+  end
+
+  def destroy
+    session.delete :user_id unless session[:user_id].nil?
+    redirect_to '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'application#home'
+  get '/signin' => 'sessions#new'
+
   get 'users/new' => 'users#new'
   post 'users/new' => 'users#create'
   resources :users, except: [:new]
