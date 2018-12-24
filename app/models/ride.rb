b5fbeddbad1b60b9f3e709af42e2a2d1e7297d5e
ryanmanchester <ryanm17@gmail.com>
2018-12-24 16:32:30 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,30 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    @ride = Ride.last
+    if @ride.user.tickets < @ride.attraction.tickets && @ride.user.height < @ride.attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{@ride.attraction.name}. You are not tall enough to ride the #{@ride.attraction.name}."
+
+    else
+      @ride.user.update(tickets: update_tickets, nausea: update_nausea, happiness: update_happiness)
+
+    end
+
+
+  end
+
+  private
+  def update_tickets
+    (@ride.user.tickets - @ride.attraction.tickets)
+  end
+
+  def update_nausea
+    @ride.user.nausea + @ride.attraction.nausea_rating
+  end
+
+  def update_happiness
+    @ride.user.happiness + @ride.attraction.happiness_rating
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :attractions
+  resources :rides
+  resources :users
 end
