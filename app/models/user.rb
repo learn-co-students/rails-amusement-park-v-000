de9620252df5b26cf35f5d538d5fb53f30575853
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-24 03:59:02 +0000

    added mood method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :rides
   has_many :attractions, through: :rides
+
+  def mood
+    if self.happiness > self.nausea
+      "happy"
+    else
+      "sad"
+    end
+  end
 end
