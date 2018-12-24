8d030c566606fd1b3625e0a8cfea8abb1e8e625d
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-24 05:19:26 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    return redirect_to '/signup'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password, :password_confirmation)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'application#home'
+  get '/signup' => 'users#new'
   resources :users
 end
