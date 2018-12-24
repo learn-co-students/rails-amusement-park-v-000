4fecb0d8f59508039531b310d6684affff76e653
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-24 05:55:49 +0000

    set up application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def in_session?
+    !!session[:user_id]
+  end
+
   def home
-    
+    if in_session?
+      @user = User.find(session[:user_id])
+    else
+      redirect_to '/login'
+    end
   end
 end
