1853a4b7acdfe895e0861098dcf87f00efb757b9
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-25 04:28:44 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'application#home'
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy', as: 'destroy_session'
 
   get 'users/new' => 'users#new'
   post 'users/new' => 'users#create'
   resources :users, except: [:new]
-
 end
