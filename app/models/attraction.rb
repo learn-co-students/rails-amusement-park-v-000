0ced4d5fb3455a1e45045f16f119cf68ca0c5f5b
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-25 03:25:38 +0000

    Done.

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class Attraction < ActiveRecord::Base
+=======
+class Attraction < ApplicationRecord
+>>>>>>> 63d5bce3d95950b032993ffd470b136d5b3b8206
   has_many :rides
   has_many :users, through: :rides
 end
