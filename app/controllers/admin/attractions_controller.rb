e466199f6c84c3d8a2e035a92d5503ba0ab7d9fe
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-12-29 07:48:01 +0000

    Done.

--- app/controllers/admin/attractions_controller.rb
+++ app/controllers/admin/attractions_controller.rb
@@ -3,17 +3,27 @@ class Admin::AttractionsController < ApplicationController
     @attractions = Attraction.all
   end
 
-  def new
-    @attraction = Attraction.new
-  end
-
   def show
     @attraction = Attraction.find(params[:id])
+  end
+
+  def new
+    @attraction = Attraction.new
   end
 
   def create
     @attraction = Attraction.create(attraction_params)
     return redirect_to '/attractions/new' unless @attraction.save
-    redirect_to attraction_path(@attraction)
+    redirect_to admin_attraction_path(@attraction)
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction.update(attraction_params)
+    return edit_admin_attraction_path(@attraction) unless @attraction.save
+    redirect_to admin_attraction_path(@attraction)
   end
 end
