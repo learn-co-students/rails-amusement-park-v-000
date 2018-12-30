bca64cbdf9a4db9e6e47037127e0dd475a6e306a
github username <github email address>
2018-12-29 17:52:41 -0800

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,4 +5,40 @@ class Ride < ActiveRecord::Base
 
 
 
+  def take_ride
+    if ticket_check && height_check
+      ride_taken
+    elsif ticket_check && !height_check
+      not_tall_enough
+    elsif height_check && !ticket_check
+      not_enough_tickets
+    else
+      not_tall_enough + not_enough_tickets
+    end
+  end
+
+  def ride_taken
+    self.user.update(tickets: @user.tickets - @attraction.tickets,
+      happiness: @user.happiness + @attraction.happiness_rating,
+    nausea: @user.nausea + @attraction.nausea_rating)
+     "Thanks for riding the #{self.attraction.name}!"
+  end
+
+  def ticket_check
+   self.user.tickets >= self.attraction.tickets
+  end
+
+  def height_check
+    self.user.height >= self.attraction.min_height
+  end
+
+  def not_enough_tickets
+    "You do not have enough tickets to ride the #{self.attraction.name}"
+  end
+
+  def not_tall_enough
+    "You are not tall enough to ride the #{self.attraction.name}"
+  end
+
+
 end
