f8be654f2c8486ab3642d7badadb450fcfa5d5dc
github username <github email address>
2018-12-30 21:59:20 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    if User.find(params[:id])
+    if User.find(params[:id]) && session[:user_id]
       @user = User.find(params[:id])
       render :show
     else
