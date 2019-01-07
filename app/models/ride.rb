019ef57c8dc1e25e442b747630352c2aed62ea25
Taylor Williams <taylor@thewilliams.net>
2019-01-07 00:05:15 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -5,17 +5,17 @@ class Ride < ActiveRecord::Base
   def take_ride
     ride_err = ""
     if self.user.height < self.attraction.min_height
-      ride_err << "Sorry. You are not tall enough to ride the #{attraction.name}."
+      ride_err << " You are not tall enough to ride the #{attraction.name}."
     end
     if self.user.tickets < self.attraction.tickets
-      ride_err << "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+      ride_err << " You do not have enough tickets to ride the #{attraction.name}."
     end
     if ride_err.empty?
       self.user.tickets -= self.attraction.tickets
       self.user.happiness += self.attraction.happiness_rating
       self.user.nausea += self.attraction.nausea_rating
     else
-      ride_err
+      "Sorry." + ride_err
     end
   end
 end
