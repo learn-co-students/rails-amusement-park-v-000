5c5cd4fbc9345fe67b2d8e3276f4e0148d74b93f
Taylor Williams <taylor@thewilliams.net>
2019-01-07 00:27:24 +0000

    Automatically backed up by Learn

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'users/new'
+  get 'users/create'
+  get 'users/show'
+  get 'users/destroy'
+  get 'users/edit'
+  get 'users/update'
 end
