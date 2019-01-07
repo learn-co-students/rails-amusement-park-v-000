d1ffed5f19d2cfc113d0507a02e44c21c68d1c4c
Taylor Williams <taylor@thewilliams.net>
2019-01-07 03:45:02 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
+      puts @user.id
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
