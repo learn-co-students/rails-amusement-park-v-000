3d10dde8b9b92d25c611cf34a3c33ec6f5c8c9c5
Taylor Williams <taylor@thewilliams.net>
2019-01-07 00:46:16 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'static#home'
   get 'static/login'
-  get 'static/signup'
+  get 'static/signup' => 'users#new'
   get 'static/logout'
 
   resources :users, only:[:show, :new, :create, :destroy, :edit, :update]
