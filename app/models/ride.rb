b837c20c0cd2b2cd3176286d7ade0646707e159e
Taylor Williams <taylor@thewilliams.net>
2019-01-07 00:01:50 +0000

    Automatically backed up by Learn

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,20 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    ride_err = ""
+    if self.user.height < self.attraction.min_height
+      ride_err << "Sorry. You are not tall enough to ride the #{attraction.name}."
+    end
+    if self.user.tickets < self.attraction.tickets
+      ride_err << "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    end
+    else
+      self.user.tickets -= self.attraction.tickets
+      self.user.happiness += self.attraction.happiness_rating
+      self.user.nausea += self.attraction.nausea_rating
+    end
+    ride_err.empty? ? self : ride_err
+  end
 end
