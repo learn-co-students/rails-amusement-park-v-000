490dcec69ba9957aa3234b0d64b14cf229920445
Taylor Williams <taylor@thewilliams.net>
2019-01-07 04:48:19 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,4 +1,13 @@
 class RidesController < ApplicationController
   def create
+    if helpers.current_user == params[:user_id].to_i
+      ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
+      if ride.valid?
+        ride.take_ride
+        redirect_to user_path(params[:user_id])
+      end
+    end
+    redirect_to attraction_path(params[:attraction_id]) if Attraction.find(params[:attraction_id])
+    redirect_to root_path
   end
 end
