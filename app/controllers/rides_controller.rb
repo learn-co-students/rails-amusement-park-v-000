5021f89c1ce4bb90dc9775dd003a2f358acf3d8f
Taylor Williams <taylor@thewilliams.net>
2019-01-07 05:35:09 +0000

    Automatically backed up by Learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,8 @@
 class RidesController < ApplicationController
   def create
     ride = Ride.create(user_id: params[:user_id], attraction_id: params[:attraction_id])
-    ride.take_ride
+    error = ride.take_ride
+    flash[:error] = error if error.start_with?("Sorry.")
     redirect_to user_path(params[:user_id])
   end
 end
