a8b753777c832d5d4ddc8b7547d3efb897f8b399
Taylor Williams <taylor@thewilliams.net>
2019-01-07 04:18:43 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  get 'attractions/index'
+  get 'attractions/show'
+  get 'attractions/new'
+  get 'attractions/create'
+  get 'attractions/edit'
+  get 'attractions/update'
+  get 'attractions/destroy'
   root 'static#home'
   get '/signin' => 'static#signin'
   post '/login' => 'static#login'
