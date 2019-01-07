52c6b3031945678e4cdf2915c1a3af03622e5936
Elizabeth <efkarst@outlook.com>
2019-01-06 18:23:50 -0800

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  has_secure_password
+
+  def mood
+    self.nausea > self.happiness ? "sad" : "happy"
+  end
+
+  def update_stats(attraction_stats)
+    self.nausea += attraction_stats[:nausea]
+    self.happiness += attraction_stats[:happiness]
+    self.tickets -= attraction_stats[:tickets]
+    
+    self.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  
+  # resource :users, only: [:new, :create, :show]
+  get '/users/new', to: 'users#new', as: 'signup'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as:'user'
+  get '/', to: 'users#welcome', as: 'root'
+
+  get '/signin', to: 'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get '/attractions', to: 'attractions#index', as: 'attractions'
+  get '/attraction/:id', to: 'attractions#show', as: "attraction"
+
 end
