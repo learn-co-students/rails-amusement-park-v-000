a36ab6a4479963b58a0885da3d4abf19d0062da4
Taylor Williams <taylor@thewilliams.net>
2019-01-07 02:17:09 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_login, only: [:show, :destroy, :edit, :update]
+  skip_before_action :require_login, only: [:new, :create]
 
   def new
     @user = User.new
