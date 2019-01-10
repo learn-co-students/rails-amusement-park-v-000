84f65cfa8794b469e71125f57be0181163ea789e
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-09 22:11:25 -0600

    created attractions table and updated ride spec and model

---
+++ db/migrate/20190110040231_create_attractions.rb
@@ -0,0 +1,13 @@
+class CreateAttractions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :attractions do |t|
+      t.string :name
+      t.integer :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
+
+      t.timestamps
+    end
+  end
+end
