879af9f1dfc2d2b00ec51abd8394b645b3a2884d
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-10 12:00:49 -0600

    added admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_10_040756) do
+ActiveRecord::Schema.define(version: 2019_01_10_175437) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_01_10_040756) do
     t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
