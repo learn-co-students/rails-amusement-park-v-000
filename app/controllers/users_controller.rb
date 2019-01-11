ec469f0dc036bff9c7b434c99895df57eea3795f
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-10 21:11:34 -0600

    updated users controller with create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,45 @@
 class UsersController < ApplicationController
+
+    def home 
+    end
     
     def new 
+        @user = User.new
     end 
+
+    def create 
+        # binding.pry
+        @user = User.new(user_params)
+        if @user.save 
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new 
+        end
+    end 
+
+    def show
+        # binding.pry 
+        @user = User.find(session[:user_id])
+    end 
+
+    private
+    
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :password,
+        :height,
+        :tickets,
+        :happiness,
+        :nausea,
+        :admin
+      )
+    end
     
 end
