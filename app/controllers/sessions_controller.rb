1d938a924c3eb24a4b1a599baabc9921ab194e29
ryanmanchester <ryanm17@gmail.com>
2019-01-11 14:57:59 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,16 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
+    @users = User.all
   end
 
   def create
-    byebug
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:message] =  "Invalid Username and/or Password"
+      redirect_to signin_path
+    end
   end
 end
