dc3670404c82c6f05ef43e53eb23354c93766074
Akshay Dhingra <akshay1dhingra@gmail.com>
2019-01-12 14:38:15 -0600

    clearned up sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,16 @@
 class SessionsController < ApplicationController
 
     def new 
-        # binding.pry
         @user = User.new
         @users = User.all
-
     end 
 
     def create 
-        # binding.pry
         @user = User.find_by(name: params[:user][:name])
-        # binding.pry
         if @user && @user.authenticate(params[:user][:password])
           session[:user_id] = @user.id
           @user.save
-        #   binding.pry
-          redirect_to user_path(@user), notice: "Welcome back to the theme park!"
+          redirect_to user_path(@user)
         else
           redirect_to signin_path
         end
