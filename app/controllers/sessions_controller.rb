96cb568916f385dbaee446c59192af7d61919974
kev112 <werewaswas@live.com>
2019-01-13 23:48:03 -0800

    ;

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+    def new
+        
+    end
+
+
+    def create
+          @user = User.find_by(name: params[:user][:name])
+          if @user &&  @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+          else
+            redirect_to '/'
+          end
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+    def show
+        if User.find(params[:id]) && session[:user_id]
+          @user = User.find(params[:id])
+        else
+            redirect_to '/'
+        end
+    end
 end
