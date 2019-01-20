8c0a6a249ef054093e1264add0dd79e07822b986
corytveten <corytveten@gmail.com>
2019-01-20 12:18:06 -0800

    Done.

--- db/migrate/20190120200437_create_rides.rb
+++ db/migrate/20190120200437_create_rides.rb
@@ -1,6 +1,9 @@
 class CreateRides < ActiveRecord::Migration[5.2]
   def change
     create_table :rides do |t|
+      t.string :type
+      t.integer :user_id
+      t.integer :attraction_id
 
       t.timestamps
     end
