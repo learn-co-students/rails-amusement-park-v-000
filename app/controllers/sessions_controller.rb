b361ebf7aed2961da278d7582f24894fe50e6831
Shmully Wolfson <shmullywolfson@gmail.com>
2019-01-22 15:15:54 -0500

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def sign_in
+    @user = User.new
+    @users = User.all
+  end
+
+  def create
+
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :sign_in
+    end
+
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  
+  root to: 'application#home'
+
+  get '/signin', to: 'sessions#sign_in', as: 'signin'
+  post '/signin', to: 'sessions#create'
+
+
   resources :attractions
+  resources :users
 end
