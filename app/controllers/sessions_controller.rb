cecd8242e16710b1a19bb849f39b23e4e037a186
ellefaraday <faraday.elle@gmail.com>
2019-01-22 15:56:09 -0500

    sign in form made but not connected

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
 
   def new
+    @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
 
   get "/" => 'application#welcome'
-  get "/login" => 'sessions#new'
-  post '/login' => 'sessions#create'
+  get "/signin" => 'sessions#new'
+  post '/signin' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
 end
