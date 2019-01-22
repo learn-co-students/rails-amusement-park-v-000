86fe407a7c4b7fda7189c2cad1f261f0cda3f866
ellefaraday <faraday.elle@gmail.com>
2019-01-22 12:01:45 -0500

    add user class method mood

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :rides
   has_many :attractions, through: :rides
   has_secure_password
+
+  def mood
+    if self.happiness - self.nausea > 0
+      "happy"
+    elsif self.happiness - self.nausea < 0
+      "sad"
+    end
+  end
 end
