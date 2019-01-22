9ef16ce9138789294a4e31025ac8616e571e8688
ellefaraday <faraday.elle@gmail.com>
2019-01-22 12:13:48 -0500

    ride class method take_ride

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,21 @@
 class Ride < ApplicationRecord
   belongs_to :attraction
   belongs_to :user
+
+  def take_ride
+    user = User.find_by_id(self.user_id)
+    attraction = Attraction.find_by_id(self.attraction_id)
+    if user.tickets < attraction.tickets && user.height < attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+    elsif user.tickets < attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif user.height < attraction.min_height
+      "Sorry. You are not tall enough to ride the #{attraction.name}."
+    else
+      user.tickets -= attraction.tickets
+      user.happiness += attraction.happiness_rating
+      user.nausea += attraction.nausea_rating
+      user.save
+    end
+  end
 end
