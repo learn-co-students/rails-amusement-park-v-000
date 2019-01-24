053a1442830c715e6d25b748622d066b54790cae
Marc Mills <mdmills1990@gmail.com>
2019-01-24 10:48:12 -0500

    builds migrations and model ascociations

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,13 @@
 class Ride < ActiveRecord::Base
+  belongs_to :attraction
+  belongs_to :user
+
+  def take_ride
+    # I'm missing something conceptually to understand how @ride can access the attractions and users info
+    if @ride.attraction.tickets > @ride.user.tickets
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    elsif @ride.attraction.
+    end
+  end
+
 end
