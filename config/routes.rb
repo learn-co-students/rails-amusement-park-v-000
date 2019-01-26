569f1dd826bbc31b7b3d4075df911a76e4d99f41
corytveten <corytveten@gmail.com>
2019-01-26 09:05:50 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  
+ 
+    root 'static_pages#home'
+    get '/signin' => 'sessions#new'
+    post '/signin' => 'sessions#create'
+    post 'logout', to: 'sessions#destroy'
+    resources :attractions
+    resources :rides
+    resources :users, only: [:new, :create, :show]
 end
