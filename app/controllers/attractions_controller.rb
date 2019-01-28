8dc9e236b4895ee9ffb14b5f287579840b8b12c6
Shmully Wolfson <shmullywolfson@gmail.com>
2019-01-28 13:53:15 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -16,10 +16,23 @@ class AttractionsController < ApplicationController
   def create
     @attraction = Attraction.new(attraction_params)
     if @attraction.save
-      redirect_to attraction_path
+      redirect_to attraction_path(@attraction)
     else
       render :new
     end
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+
+    @attraction.update(attraction_params)
+
+    redirect_to attraction_path(@attraction)
+
   end
 
 
