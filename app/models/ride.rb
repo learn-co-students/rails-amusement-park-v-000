ff84cf64b534ca790daf2b722830920e52051d61
Naudria <rima.jean@gmail.com>
2019-02-07 09:32:39 -0600

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,32 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if !enough_tickets? && !tall_enough?
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    elsif !enough_tickets?
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    elsif !tall_enough?
+      "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    else
+      allow_entry
+      "Thanks for riding the #{self.attraction.name}!"
+    end
+  end
+
+  def allow_entry
+    self.user.tickets -= self.attraction.tickets
+    self.user.nausea += self.attraction.nausea_rating
+    self.user.happiness += self.attraction.happiness_rating
+    self.user.save
+  end
+
+  def enough_tickets?
+    self.user.tickets >= self.attraction.tickets
+  end
+
+  def tall_enough?
+    self.user.height >= self.attraction.min_height
+  end
 end
