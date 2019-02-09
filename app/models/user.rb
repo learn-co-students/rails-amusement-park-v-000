926bf60ed78f59ac462672eac2b3bc01d61d1fee
zlehmann <zlehmann@gmail.com>
2019-02-09 07:36:56 -0800

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+    validates :password, presence: true
+    has_many :rides
+    has_many :attractions, through: :rides 
+
+    def mood
+        mood = 'normal'
+        if self.happiness > self.nausea
+            mood = 'happy'
+        else
+            mood = 'sad'
+        end
+        mood
+    end
+
 end
