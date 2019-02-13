0b0fc2ffeba1c66cab45aac0d9f7a933876bb329
maxwellbenton <maxwellbenton@gmail.com>
2019-02-13 09:11:14 -0500

    rails app:update

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
   # and recreated between test runs. Don't rely on the data there!
-  config.middleware.use RackSessionAccess::Middleware
   config.cache_classes = true
 
   # Do not eager load code on boot. This avoids loading your whole application
@@ -16,7 +15,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{1.hour.seconds.to_i}"
+    'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
   # Show full error reports and disable caching.
@@ -28,6 +27,10 @@ Rails.application.configure do
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
+
+  # Store uploaded files on the local file system in a temporary directory
+  config.active_storage.service = :test
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
