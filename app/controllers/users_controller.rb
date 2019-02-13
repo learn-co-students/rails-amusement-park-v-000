1f4293d20daa5eb028e9a85ab9fcf87b9ad0b267
Mika88 <mikaspivak@gmail.com>
2019-02-12 19:52:21 -0800

    routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,19 @@
 class UsersController < ApplicationController
+
     def new
         @user = User.new
     end
 
     def create
         @user = User.create(user_params)
-
+        session[:user_id] = @user.id
         redirect_to user_path(@user)
     end
-    
+
     def show 
+      @user = User.find(params[:id])
     end
-    
+
     def edit
     end
 
