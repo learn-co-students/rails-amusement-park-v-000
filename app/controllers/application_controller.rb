67aa34edbe1214c1da61826f941b5f648b0e630d
Josely Castro <joselycas@gmail.com>
2019-02-14 17:35:07 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-   before_action :require_login
+  before_action :require_logged_in, except: [:new, :create, :home]
+
 
 
 
