94e27010a29dc8472ef470e0ffe880c613b3420d
VincePimentel <contact@vincep.me>
2019-02-13 19:03:30 -0800

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.valid?
+      @user.save
+
+      session[:user_id] = @user.id
+
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def show; end
+
+  private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  
+
+  root 'application#index'
+  resources :users
 end
