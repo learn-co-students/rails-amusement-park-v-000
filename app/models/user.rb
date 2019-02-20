8d576a04417e4acb96f1012b3f881c107c63e2d8
Bruce Hillsworth <brucehillsworth@Bruces-MacBook-Air.local>
2019-02-20 14:32:40 -0700

    Passed Tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
 
   validates :password, presence: true
+
+  def mood
+    unless admin
+      happiness > nausea ? 'happy' : 'sad'
+    end
+  end
 end
