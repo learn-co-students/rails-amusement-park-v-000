bd1e3fdd2f945f0b39b8129732d7ae1b9837718b
George  Pianka <gpianka1@gmail.com>
2019-02-25 05:05:39 +0000

    Initial Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
+  
   protect_from_forgery with: :exception
-end
+
+
+  def home
+  end
+  
+  helper_method :current_user, :logged_in?, :require_logged_in
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  
+  def logged_in?
+    !!current_user
+  end
+
+  def require_logged_in
+    redirect_to root_path unless logged_in?
+  end
+
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   
+  root 'application#home'
+  
+  controller :sessions do
+    get '/login' => :new
+    post '/login' => :create
+    post '/logout' => :destroy
+  end
+
+  resources :rides, only: :new
+
+  resources :attractions
+  resources :users
+  
 end
