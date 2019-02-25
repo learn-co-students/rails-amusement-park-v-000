25e33f7430eb66767632ab21b0b48e926303ca2a
Ayana Bando <ayanaflax@yahoo.com>
2019-02-25 16:47:33 +0000

    Done.

---
+++ db/migrate/20190225152736_users.rb
@@ -0,0 +1,13 @@
+class Users < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :password
+      t.string :password_digest
+      t.integer :nausea
+      t.integer :happiness
+      t.integer :tickets
+      t.integer :height
+    end
+  end
+end
