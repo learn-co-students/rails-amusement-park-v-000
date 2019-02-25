5526073d64e4d6e33063bf8d07fd700c5003cc5a
Ayana Bando <ayanaflax@yahoo.com>
2019-02-25 18:33:11 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Ride < ActiveRecord::Base
-  belongs_to :attractions
+  belongs_to :attraction
+  belongs_to :user
+  def take_ride
+    fail_req = []
+    
+     if !!meet_requirements
+      # update_user
+      return "Thanks for riding the #{self.attraction.name}!"
+    end
+    
+    if user.tickets < attraction.tickets
+      fail_req <<  "You do not have enough tickets to ride the #{attraction.name}."
+    end
+    if user.height < attraction.min_height
+      fail_req << "You are not tall enough to ride the #{attraction.name}."
+    end
+    if !fail_req.empty?
+        "Sorry. " + fail_req.join(" ")
+    end
+  end
+  
+   def meet_requirements
+    self.user.tickets >= self.attraction.tickets && self.user.height >= self.attraction.min_height
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   has_secure_password
+  
+  def mood
+    if self.nausea > self.happiness
+      return "sad"
+    else
+      return "happy"
+    end
+  end
 end
