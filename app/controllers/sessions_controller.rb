a779bde1969decb88dae1fccf80f86cefd34cfd0
NotoriousCottonball <gpianka1@gmail.com>
2019-02-24 22:35:01 -0800

    Finished Lab on Local Repo

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #@user = User.find_by(name: params[:user][:name])
-    #if @user && @user.authenticate(params[:password])
-      #session[:user_id] = @user.id
-      #redirect_to user_path(@user)
-    #else
-      flash.notice = "Incorrect password"
+    @user = User.find_by(name: params[:user][:name])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash.alert = "Incorrect password"
       redirect_to login_path
-    #end
+    end
   end
 
   def destroy
