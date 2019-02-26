6a2c7d7a5acfa3c2280927f941edbc008b9c070a
Ayana Bando <ayanaflax@yahoo.com>
2019-02-26 02:27:50 +0000

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,32 +3,18 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
   belongs_to :user
   def take_ride
-    fail_req = []
-    
-     if !!meet_requirements
-       update_user
-     "Thanks for riding the #{self.attraction.name}!"
-    end
-    
-    if self.user.tickets < self.attraction.tickets
-      fail_req <<  "You do not have enough tickets to ride the #{attraction.name}."
-    end
-    if self.user.height < self.attraction.min_height
-      fail_req << "You are not tall enough to ride the #{attraction.name}."
-    end
-    if !fail_req.empty?
-        "Sorry. " + fail_req.join(" ")
-    end
-  end
-  
-   def meet_requirements
-    self.user.tickets >= self.attraction.tickets && self.user.height >= self.attraction.min_height
-  end
-  
-  def update_user
-    self.user.tickets -= self.attraction.tickets
-    self.user.nausea += self.attraction.nausea_rating
-    self.user.happiness += self.attraction.happiness_rating
-    self.user.save
- end
+    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+			"Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+		elsif self.user.tickets < self.attraction.tickets
+			"Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+		elsif self.user.height < self.attraction.min_height
+			"Sorry. You are not tall enough to ride the #{self.attraction.name}."
+		else
+			self.user.tickets -= self.attraction.tickets
+			self.user.nausea += self.attraction.nausea_rating
+			self.user.happiness += self.attraction.happiness_rating
+			self.user.save
+			"Thanks for riding the #{self.attraction.name}!"
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  
+  root 'application#index'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/signout' => 'sessions#destroy'
+  delete '/signout' => 'sessions#destroy'
+
   resources :users
+
+  resources :rides
+  resources :attractions
 end
