907bf74f1357da555180e73661b2668ee56539b8
James Borocz <thesilentc@me.com>
2019-03-10 21:49:04 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_11_005152) do
+ActiveRecord::Schema.define(version: 2019_03_11_012718) do
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "name"
+    t.integer "tickets"
+    t.integer "nausea_rating"
+    t.integer "happiness_rating"
+    t.integer "min_height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "attraction_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attraction_id"], name: "index_rides_on_attraction_id"
+    t.index ["user_id"], name: "index_rides_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
