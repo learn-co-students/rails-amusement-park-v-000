89b226513da8c742aa52216fc40b9d64b5b45b5a
kjdub01 <kjewettwilliams@gmail.com>
2019-03-13 11:50:15 -0700

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+    @message = params[:message] if params[:message]
+    @message ||= false
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+
+      redirect_to user_path(@user), notice: "Welcome to the theme park!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :password,
+      :height,
+      :nausea,
+      :happiness,
+      :tickets,
+      :admin
+    )
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  def mood
+    if self.happiness && self.nausea
+      mood = self.happiness - self.nausea
+      mood > 0 ? "happy" : "sad"
+    end
+  end
+
+  def num_of_rides
+    self.attractions.count
+  end
 end
