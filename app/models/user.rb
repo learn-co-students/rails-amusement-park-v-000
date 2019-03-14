610ce549b659a5f41c91886cc8a0930001326f9d
Rachel Wyatt <rw126@evansville.edu>
2019-03-14 22:30:20 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  has_many :rides 
+  has_many :attractions, through: :rides
+  validates :password, presence: true
+
+  def mood
+    if self.nausea > self.happiness 
+      return "sad"
+    else 
+      return "happy"
+    end
+  end
 end
