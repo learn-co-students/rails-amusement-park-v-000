d7f6d9216ca32e7d66ce2c61048cc3c2dac8f340
github username <github email address>
2019-03-18 12:40:48 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,5 @@
 class Ride < ActiveRecord::Base
+
+  belongs_to :user
+  belongs_to :attraction
 end
---
+++ db/migrate/20190318193817_add_nausea_happiness_tickets_heights_to_users.rb
@@ -0,0 +1,8 @@
+class AddNauseaHappinessTicketsHeightsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :nausea, :integer
+    add_column :users, :happiness, :integer
+    add_column :users, :tickets, :integer
+    add_column :users, :height, :integer
+  end
+end
