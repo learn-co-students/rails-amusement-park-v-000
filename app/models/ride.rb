0e4f50c3cb1fb498f79df7fbb6a14382e3fc22ef
github username <github email address>
2019-03-18 13:15:03 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,20 @@ class Ride < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :attraction
+
+  def take_ride
+    if self.user.tickets < self.attraction.tickets  && self.user.height < self.attraction.min_height
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    elsif self.user.height < self.attraction.min_height
+      "Sorry. You are not tall enough to ride the #{self.attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
+    else
+      self.user.tickets -= self.attraction.tickets
+      self.user.nausea += self.attraction.nausea_rating
+      self.user.happiness += self.attraction.happiness_rating
+      binding.pry
+    end
+  end
+
 end
