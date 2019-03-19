cd96d425a3f552cfaaa992fb57b87ad0caa66250
Rachel Wyatt <rw126@evansville.edu>
2019-03-19 11:23:01 +0000

    add functionality to edit attraction

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,4 +8,17 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.find(params[:id])
   end
 
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    if @attraction.update(attraction_params)
+        redirect_to attraction_path(@attraction, notice: 'Attraction was successfully updated.')
+    else
+        redirect_to edit_attraction_path(@attraction)
+    end
+  end
+
 end
