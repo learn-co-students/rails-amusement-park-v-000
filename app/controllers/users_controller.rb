abc932afa59e38341b54b30ad8e516c838111be1
github username <github email address>
2019-03-20 13:33:18 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(name: params[:user][:name],
+        password: params[:user][:password],
+        nausea: params[:user][:nausea],
+        happiness: params[:user][:hapiness],
+        tickets: params[:user][:tickets],
+        height: params[:user][:height],
+        admin: params[:user][:admin])
+    log_in(@user)
+    redirect_to user_path(@user)
+  end
+
+  def show
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'welcome/home'
   get 'users/new'
 
   root 'welcome#home'
 
   resources :users, only: [:show, :new, :create]
+
+  get '/login', to: "sessions#new"
+  get '/login', to: "sessions#create"
+  get '/logout', to: "sessions#logout"
 
 end
