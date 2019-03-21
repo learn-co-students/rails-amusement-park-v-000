9b8267e4eaef2e26661c9bdb17d97389729bfaea
Judy McGoldrick <jjeanius@verizon.net>
2019-03-21 22:55:11 +0000

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,2 +1,18 @@
 class RidesController < ApplicationController
+
+  def index
+    @rides = Ride.all
+  end
+
+  def new
+    @ride = Ride.create(params[:ride])
+  #  @ride = @ride.take_ride
+    redirect_to user_path(@ride)
+  end
+
+
+#  def show
+#    @ride = Ride.find_by(id: params[:id])
+#  end
+
 end
