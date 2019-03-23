53fe016f6a2784c87a134d58e9af740dcc99b653
nmarini <nmarini44@gmail.com>
2019-03-23 12:20:21 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   
+  root 'static#homepage'
+  get '/signin', to: 'sessions#new'
+  post '/sessions/create', to: 'sessions#create'
+  delete '/sessions/delete', to: 'sessions#destroy'
+  get '/sessions/delete', to: 'sessions#destroy'
+  resources :rides
+  resources :attractions
+  resources :users
+
 end
