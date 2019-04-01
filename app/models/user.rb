af6fecd9927dc48722752fcd4174aa1927950611
Erin Green <erin.y.green@gmail.com>
2019-04-01 19:37:38 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :rides
+  has_many :attractions, through: :rides
+
+def mood
+  if self.nausea > self.happiness
+    "sad"
+  else
+    "happy"
+  end
+end
+
+
+
+  it "has a method 'mood' that returns 'sad' when the user is more nauseous than happy" do
+    expect(user.mood).to eq("sad")
+  end
+
+  it "has a method 'mood' that returns 'happy' when the user is more happy than nauseous" do
+    user.update(:happiness => 7)
+    expect(user.mood).to eq("happy")
+
 end
