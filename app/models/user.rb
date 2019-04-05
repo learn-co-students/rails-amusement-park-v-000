c19e8e2ec4e7dcb0ba0ad7b0f72df64711ce695d
Erin Green <erin.y.green@gmail.com>
2019-04-05 19:28:15 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
 
-  validates :name, presence: true
-  validates :height, presence: true, :numericality => { :greater_than_or_equal_to => 0}
+  validates :name, presence: true, on: :create
+  validates :height, presence: true, :numericality => { :greater_than_or_equal_to => 0}, on: :create
   validates :happiness, presence: true #:inclusion => {in: 1..5}
   validates :nausea, presence: true #:inclusion => {in: 1..5}
   validates :tickets, presence: true, :numericality => { :greater_than_or_equal_to => 0}
-  validates :password, presence: true
+  validates :password, presence: true, on: :create
 
 def mood
   if self.nausea > self.happiness
