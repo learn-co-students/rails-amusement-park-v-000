0246d6fb1b80e5546460ede6a6f28b5ae07a7c6a
lpzraf <lpz.raf@gmail.com>
2019-04-15 22:38:45 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_user, :logged_in?, :authenticate_user
 
-  def home
-  end
+
 
   private
 
