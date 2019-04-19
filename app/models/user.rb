190182d35c8b63f2086d75c1d942e37a71c8a646
github username <github email address>
2019-04-19 15:42:27 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  validates :password, presence: true
+
+
+  def mood
+    if self.nausea > self.happiness
+      return "sad"
+    elsif self.nausea < self.happiness
+      return "happy"
+    end
+  end
 end
