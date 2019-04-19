3b547b664dcff4a6b1f8da661dc7433e876bbfd2
github username <github email address>
2019-04-19 16:20:34 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :rides, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :attractions, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  #get '/' => 'application#index'
+
+  resources :users #, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :rides#, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :attractions#, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
