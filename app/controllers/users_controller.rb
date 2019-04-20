98ec74b5e36af8b79e7c2ca70ac268191d831be2
Tammy Metz <tammy@tammymetz.com>
2019-04-20 01:36:46 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.create(user_params)
+    return redirect_to controller: 'users', action: 'new' unless @user.save
+    session[:user_id] = @user.name
+    redirect_to controller: 'users', action: 'home'
+  end
+
+  def home
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :rides
   resources :users
   resources :attractions
+
+  get '/home' => 'users#home'
+
+  root 'users#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
