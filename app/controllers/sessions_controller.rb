149b9d5e9417e24d4cad31714e94209df19a6b31
Tammy Metz <tammy@tammymetz.com>
2019-04-21 22:28:27 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
+  def new
+    @users = User.all
+  end
+
+  def show
+  end
+
   def create
     @user = User.find_by(name: params[:name])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
+    redirect_to '/'
+  end
+
+  def destroy
+    session.delete :name
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get '/home' => 'users#home'
 
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
   root 'users#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
