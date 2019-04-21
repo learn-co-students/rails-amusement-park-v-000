a143602ae719cce948c36559a75f47e1ab5512b7
Tammy Metz <tammy@tammymetz.com>
2019-04-21 15:53:34 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_18_232715) do
+ActiveRecord::Schema.define(version: 2019_04_21_155042) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2019_04_18_232715) do
     t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
