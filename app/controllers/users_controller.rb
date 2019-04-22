02225208707bf7ced2e2dd65e87db7c441be134d
github username <github email address>
2019-04-22 16:56:04 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,6 @@ class UsersController < ApplicationController
 
   def create
     # binding.pry
-
     @user = User.create(user_params)
     session[:user_id] = @user.id
     redirect_to user_path(@user)
