048206e3d53d6db24a4bd1837720157e49b6cb84
Tammy Metz <tammy@tammymetz.com>
2019-04-27 17:54:45 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
   has_secure_password
 
+
   def mood
     if self.happiness > self.nausea
       "happy"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_21_155042) do
+ActiveRecord::Schema.define(version: 2019_04_27_174849) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -32,10 +32,10 @@ ActiveRecord::Schema.define(version: 2019_04_21_155042) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password_digest"
-    t.integer "nausea"
-    t.integer "happiness"
-    t.integer "tickets"
-    t.integer "height"
+    t.integer "nausea", default: 1
+    t.integer "happiness", default: 3
+    t.integer "tickets", default: 5
+    t.integer "height", default: 60
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
