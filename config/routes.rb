8d6feb9658c30bd6e761a5743343b1c9e39dc03a
Heather Miller <heather.miller114@gmail.com>
2019-05-06 21:08:58 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'attractions#index'
+  resources :users
+
+  #  get '/login' => 'sessions#new'
+  #  post '/login' => 'sessions#create'
 end
