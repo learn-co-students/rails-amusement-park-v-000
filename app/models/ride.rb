cbf2eb283e1faf8f3ae52b5772dd89c71ab932b1
sarawall1187 <sarawall1187@gmail.com>
2019-05-09 13:19:58 -0700

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,10 @@
+require 'pry'
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+     self.user.tickets < self.attraction.tickets ? "Sorry. You do not have enough tickets to ride the #{attraction.name}." : nil 
+    #  self.user.height >= self.attraction.min_height ? "Sorry. You are not tall enough to ride the #{attraction.name}." : nil 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :attractions
+  resources :rides
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
