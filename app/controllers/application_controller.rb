a5d5ac1504edd773b3db062e6af079b8554e36eb
Heather Miller <heather.miller114@gmail.com>
2019-05-10 18:19:59 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_user
-    if session[:user_id]
+    return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
-    end
   end
 
 end
--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,11 +1,30 @@
 class AttractionsController < ApplicationController
+    before_action :require_login
+    
     def index
         @attractions = Attraction.all
+        @user = current_user
     end
 
     def show
         #binding.pry
         @attraction = Attraction.find_by(id: params[:id])
         
+    end
+
+    def new
+
+    end
+
+    def create
+        
+    end
+
+    private
+
+    def require_login
+        if !session.include? :user_id
+            redirect_to '/'
+        end
     end
 end
