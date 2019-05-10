f8107a37ae1c863775a59e6087ffa2f39a5ad099
Ashley <ashleywong678@gmail.com>
2019-05-10 14:39:26 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user
-      session[:id] = @user.id
+      session[:user_id] = @user.id
       redirect_to user_path(@user)
     else 
       render :new
