9b3407ed5b54f37ad8dbe05799b3b01b6dea9b2c
Ashley <ashleywong678@gmail.com>
2019-05-10 12:04:04 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_or_create_by(user_params)
+    if @user
+      redirect_to user_path
+    else 
+      render :new
+    end
   end
-  
+
+  # "user"=>{"name"=>"Amy Poehler", "height"=>"58", "happiness"=>"3", "nausea"=>"2", "tickets"=>"15", "password"=>"password"}
+
+  def show
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :height, :happiness, :nausea, :tickets, :password)
+    end
+
 end
--- db/migrate/20190510151835_users.rb
+++ db/migrate/20190510151835_users.rb
@@ -3,10 +3,10 @@ class Users < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
-      t.intger :nausea
-      t.intger :happiness
-      t.intger :tickets
-      t.intger :height
+      t.integer :nausea
+      t.integer :happiness
+      t.integer :tickets
+      t.integer :height
     end
   end
 end
