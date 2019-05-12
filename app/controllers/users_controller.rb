197eefbf14ab331f10d1ef226e9c24225bed7107
Ashley <ashleywong678@gmail.com>
2019-05-12 15:19:12 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    # binding.pry
     @user = User.create(user_params)
     @user.admin = true if params[:user][:admin] == true
     @user.save
