7fbbb6ac505818f10299f6245d725a2a780a107a
Ashley <ashleywong678@gmail.com>
2019-05-12 16:49:59 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get "/logout", to: "sessions#destroy"
   delete "/logout", to: "sessions#destroy"
   resources :users, only: [:new, :create, :show]
+  resources :attractions, only: [:index, :show]
 
 end
