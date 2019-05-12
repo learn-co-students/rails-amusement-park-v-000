708b2e37f6bb210a8d8a5d5873cfe3785b11e5b3
Ashley <ashleywong678@gmail.com>
2019-05-12 15:37:08 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :password, :height, :happiness, :nausea, :tickets, :admin)
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root to: 'sessions#home'
-  # get "/signin", to: "sessions#new"
-  # post "/signin", to: "sessions#create"
-  resources :sessions, only: [:new, :create, :destroy]
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
   resources :users, only: [:new, :create, :show]
 
 end
