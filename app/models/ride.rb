10af75f00dbaced3077d9c8843e4d7607ba50481
Ashley <ashleywong678@gmail.com>
2019-05-13 13:30:47 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,11 +4,11 @@ class Ride < ActiveRecord::Base
 
   def take_ride
     if self.user.height < self.attraction.min_height && self.user.tickets < self.attraction.tickets
-      "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."  
     elsif self.user.height < self.attraction.min_height
       "Sorry. You are not tall enough to ride the #{self.attraction.name}."
-    elsif self.user.tickets < self.attraction.tickets
-      "Sorry. You do not have enough tickets to ride the Roller Coaster."
     else
       self.user.update(
         tickets: self.user.tickets - self.attraction.tickets,
