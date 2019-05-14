863425885dca21e0a6c2e1ef2e65c11a35d6a267
Rebecca Tong <rtong@buffalo.edu>
2019-05-13 22:32:15 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,4 +8,19 @@ class AttractionsController < SessionsController
       @attraction = Attraction.find_by(id: params[:id])
   end
 
+  def create
+    @attraction = Attraction.new(attraction_params)
+    end
+
+
+  def update
+    @attraction = Attraction.update(attraction_params)
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :nausea_rating, :happiness_rating, :tickets)
+  end
+
 end
