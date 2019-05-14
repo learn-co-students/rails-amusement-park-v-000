d5c7c030071398a6b66a544d80cfdd13cadf433c
Heather <heather.miller114@gmail.com>
2019-05-14 12:30:40 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -13,11 +13,25 @@ class AttractionsController < ApplicationController
     end
 
     def new
-
+        @attraction = Attraction.new
     end
 
     def create
-        
+        @attraction = Attraction.create(attraction_params)
+
+        redirect_to attraction_path(@attraction)
+    end
+
+    def edit
+        @attraction = Attraction.find_by(id: params[:id])
+    end
+
+    def update
+
+        @attraction = Attraction.find_by(id: params[:id])
+        @attraction.update(attraction_params)
+
+        redirect_to attraction_path(@attraction)
     end
 
     private
@@ -26,5 +40,9 @@ class AttractionsController < ApplicationController
         if !session.include? :user_id
             redirect_to '/'
         end
+    end
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
     end
 end
