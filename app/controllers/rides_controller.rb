2bf6a02096d241da30e66caf963804fa802299e3
Grace Choi <oiograceoio@gmail.com>
2019-05-17 21:03:03 +0000

    Done.

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,8 @@
+class RidesController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/signin", to: "sessions#new"
   post "/signin", to: "sessions#create"
   delete "/signout", to: "sessions#destroy"
+  post "/attractions/show", to: "rides#new"
   resources :users
   resources :attractions
 end
