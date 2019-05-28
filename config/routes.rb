da85b3b171939409a262dbfc39d920f52aa55287
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-28 17:11:18 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/users/new' => 'users#new'
+  resources :users
+  resources :rides
+  resources :attractions
+
+  root 'welcome#home'
+
 end
