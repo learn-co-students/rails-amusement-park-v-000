a0b26e78dbd2a8a276a262b65ade59b5fb8a4608
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-28 16:52:30 -0500

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,37 @@
 class Ride < ActiveRecord::Base
     belongs_to :user
     belongs_to :attraction
+
+    def take_ride
+        if not_enough_tickets && !not_tall_enough
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+        elsif not_tall_enough && !not_enough_tickets
+            "Sorry. You are not tall enough to ride the #{attraction.name}."
+        elsif not_enough_tickets && not_tall_enough
+            "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+        else
+            get_on_ride
+        end
+    end
+
+    def get_on_ride
+        new_happiness = self.user.happiness + self.attraction.happiness_rating
+        new_nausea = self.user.nausea + self.attraction.nausea_rating
+        new_tickets = self.user.tickets - self.attraction.tickets
+        self.user.update(
+            happiness: new_happiness,
+            nausea: new_nausea,
+            tickets: new_tickets
+        )
+
+        "Thanks for riding the #{attraction.name}"
+    end
+
+    def not_enough_tickets
+        self.user.tickets < self.attraction.tickets
+    end
+
+    def not_tall_enough
+        self.user.height < self.attraction.min_height
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 
     has_many :rides
     has_many :attractions, through: :rides  
+
+    def mood
+        self.nausea > self.happiness ? "sad" : "happy"
+    end
 end
