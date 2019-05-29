a52af4b64a072c0e9a56a34bcc59a16bbc66dc32
github username <github email address>
2019-05-29 17:44:08 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :logged_in?
+  helper_method :logged_in?, :current_user
   private
 
   def logged_in?
     !!session[:user_id]
+  end
+
+  def current_user
+    @user = User.find(session[:user_id])
+    #binding.pry
   end
 end
--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,9 +1,16 @@
 class AttractionsController < ApplicationController
   def index
     @attractions = Attraction.all
+
   end
 
   def show
     @attraction = Attraction.find(params[:id])
+    @ride = Ride.new
+    @ride.user.build
+
+    # @ride.take_ride
+    # @ride.current_user
+    # @ride.save
   end
 end
