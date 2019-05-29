9cf450ba8a5f5096076398580c8ab4fa4f5905a0
Leslie Stevens <flymaven@protonmail.com>
2019-05-29 18:42:06 -0500

    edit rides_controller

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -12,19 +12,19 @@ class RidesController < ApplicationController
         #byebug
         @ride = Ride.new(user_id: current_user.id, attraction_id: params["attraction_id"])
         @ride.save
-        attraction = Attraction.find(params["attraction_id"])
-        user = current_user
-        if attraction.min_height > user.height && attraction.tickets > user.tickets
-          flash[:message] = "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
-        elsif attraction.min_height > user.height
-          flash[:message] = "Sorry. You are not tall enough to ride the #{attraction.name}."
-        elsif attraction.tickets > user.tickets
-          flash[:message] = "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-        else
-          flash[:message] = "Thanks for riding the #{@ride.attraction.name}!"
-        end
+        #attraction = Attraction.find(params["attraction_id"])
+        #user = current_user
+      #  if attraction.min_height > user.height && attraction.tickets > user.tickets
+        #  flash[:message] = "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+      #  elsif attraction.min_height > user.height
+        #  flash[:message] = "Sorry. You are not tall enough to ride the #{attraction.name}."
+      #  elsif attraction.tickets > user.tickets
+      #    flash[:message] = "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+      #  else
+      #    flash[:message] = "Thanks for riding the #{@ride.attraction.name}!"
+      #  end
         #@
-        @ride.take_ride
+        flash[:message] = @ride.take_ride
 
         redirect_to user_path(current_user)
       end
