34aa1be76e52d96f7c11323b57c756a3c8e005b0
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-30 15:33:44 -0500

    Moved view/added create to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,28 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.create(user_params)
+
+        if @user
+            session[:user_id] = @user.user_id
+            redirect_to user_path(@user)
+        else
+            render 'new'
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(
+            :name,
+            :nausea,
+            :happiness,
+            :tickets,
+            :height,
+            :password,
+            :admin
+        )
+    end
+
 end
