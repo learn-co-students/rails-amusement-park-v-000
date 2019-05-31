ec81af440f850f1c02774f7cf2a9a873a99e089d
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-31 17:42:36 +0000

    first commit

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,47 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    @user = self.user
+    @attraction = self.attraction
+
+    if @user.tickets >= @attraction.tickets && @user.height >= @attraction.min_height
+      @user.tickets -= @attraction.tickets
+      @user.happiness += @attraction.happiness_rating
+      @user.nausea += @attraction.nausea_rating
+      @user.save
+    elsif @user.tickets >= @attraction.tickets
+      return "Sorry. You are not tall enough to ride the #{@attraction.name}."
+    elsif @user.height >= @attraction.min_height
+      return "Sorry. You do not have enough tickets to ride the #{@attraction.name}."
+    else
+      return "Sorry. You do not have enough tickets to ride the #{@attraction.name}. You are not tall enough to ride the #{@attraction.name}."
+    end
+  end
+
+  def enough_tickets?
+    if self.user.tickets >= self.attraction.tickets
+      return true
+    else
+      return false
+    end
+  end
+
+  def tall_enough?
+    if self.user.height >= self.attraction.min_height
+      return true
+    else
+      return false
+    end
+  end
+
+  def update_tickets
+    self.user.tickets -= self.attraction.tickets
+  end
+
+  def update_mood
+    self.user.happiness = self.attraction.happiness_rating
+    self.user.nausea = self.attraction.nausea_rating
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  has_secure_password
+
+  def mood
+    if self.nausea.to_i < self.happiness.to_i
+      return "happy"
+    else
+      return "sad"
+    end
+  end
 end
