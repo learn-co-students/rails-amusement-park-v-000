5d85948a7af4caed105e2814860e64d963fac9e0
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-30 21:53:21 -0500

    Added new/create/link for creating attraction

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,4 +8,19 @@ class AttractionsController < ApplicationController
         @attraction = Attraction.find_by(id: params[:id])
         @ride = @attraction.rides.build(user_id: current_user.id)
     end
+
+    def new
+        @attraction = Attraction.new
+    end
+
+    def create
+        @attraction = Attraction.create(attraction_params)
+        redirect_to attraction_path(@attraction)
+    end
+
+    private
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :tickets, :nausea_rating, :happiness_rating, :min_height)
+    end
 end
