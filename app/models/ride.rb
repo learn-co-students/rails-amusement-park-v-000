14eb90a7d6a05ccf17ac49abcafa253f51a6bbc7
debjohnson33 <debjohnson_11@yahoo.com>
2019-05-30 21:40:49 -0500

    Edited ride model/attraction index

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -4,11 +4,11 @@ class Ride < ActiveRecord::Base
 
     def take_ride
         if not_enough_tickets && !not_tall_enough
-            "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+            "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
         elsif not_tall_enough && !not_enough_tickets
-            "Sorry. You are not tall enough to ride the #{attraction.name}."
+            "Sorry. You are not tall enough to ride the #{self.attraction.name}."
         elsif not_enough_tickets && not_tall_enough
-            "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+            "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{attraction.name}."
         else
             get_on_ride
         end
@@ -18,13 +18,10 @@ class Ride < ActiveRecord::Base
         new_happiness = self.user.happiness + self.attraction.happiness_rating
         new_nausea = self.user.nausea + self.attraction.nausea_rating
         new_tickets = self.user.tickets - self.attraction.tickets
-        self.user.update(
-            happiness: new_happiness,
-            nausea: new_nausea,
-            tickets: new_tickets
-        )
+        self.user.update(happiness: new_happiness, nausea: new_nausea, tickets: new_tickets)
+        puts self.user.tickets
 
-        "Thanks for riding the #{attraction.name}"
+        "Thanks for riding the #{self.attraction.name}!"
     end
 
     def not_enough_tickets
