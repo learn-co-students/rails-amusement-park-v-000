1725cfab760e98038517c753436fa7cf0301ce36
github username <github email address>
2019-06-01 14:00:09 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,7 +1,18 @@
 class AttractionsController < ApplicationController
+
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+
+
   def index
     @attractions = Attraction.all
-
   end
 
   def show
@@ -12,5 +23,10 @@ class AttractionsController < ApplicationController
     # @ride.take_ride
     # @ride.current_user
     # @ride.save
+  end
+
+  private
+  def attraction_params
+   params.require(:attraction).permit(:name, :min_height, :nausea_rating, :happiness_rating, :tickets)
   end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,22 +6,16 @@ class Ride < ActiveRecord::Base
 
     if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
       return "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
-    end
-
-    if self.user.tickets > self.attraction.tickets
+    elsif self.user.height < self.attraction.min_height
+      return "Sorry. You are not tall enough to ride the #{attraction.name}."
+    elsif self.user.tickets < self.attraction.tickets
+      return "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    else
       self.user.update(:tickets => self.user.tickets - self.attraction.tickets)
       self.user.update(:nausea => self.user.nausea + self.attraction.nausea_rating)
       self.user.update(:happiness => self.user.happiness + self.attraction.happiness_rating)
-    else
-      return "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+      return "Thanks for riding the #{self.attraction.name}!"
     end
-
-    if self.user.height > self.attraction.min_height
-      self.user.height
-    else
-      return "Sorry. You are not tall enough to ride the #{attraction.name}."
-    end
-
   end
 
 end
