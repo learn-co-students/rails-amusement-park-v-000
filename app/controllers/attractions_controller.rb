e058c8fbc94af74d36ebb1261e2f08d2c45c7323
github username <github email address>
2019-06-09 18:05:57 -0400

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -10,4 +10,29 @@ class AttractionsController < ApplicationController
     @ride = @attraction.rides.build(user_id:current_user.id)
   end
 
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :tickets, :min_height)
+  end
+
 end
