c8cbf28ef19a6a87d90c1fd60549910fb0938bb6
learn <vocalise@gmail.com>
2019-06-08 20:48:45 -0700

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,9 +1,34 @@
 class AttractionsController < ApplicationController
     def index
         @attractions = Attraction.all
+        @user = current_user
+    end
+
+    def new
+        @attraction = Attraction.new
     end
 
     def show
         @attraction = Attraction.find_by(:id => params[:id])
+        @user = current_user
+    end
+
+    def create
+        @attraction = Attraction.new(attraction_params)
+        @attraction.save
+        redirect_to attraction_path(@attraction)
+    end
+
+    def edit
+        @attraction = Attraction.find_by(:id => params[:id])
+        @user = current_user
+    end
+
+    def update
+    end
+
+    private
+    def attraction_params
+        params.require(:attraction).permit(:name, :nausea_rating, :min_height, :tickets, :happiness_rating)        
     end
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,26 +3,13 @@ class Ride < ActiveRecord::Base
     belongs_to :attraction
 
     def take_ride
-        #user.tickets < attraction.tickets ? errors[:tickets] = "You do not have enough tickets to ride the #{attraction.name}."
-        #user.height < attraction.min_height ? errors[:height] = "You are not tall enough to ride the #{attraction.name}."
-        if user.tickets < attraction.tickets
+       if user.tickets < attraction.tickets
             errors.messages[:tickets] = " You do not have enough tickets to ride the #{attraction.name}."
         end
 
         if user.height < attraction.min_height
             errors.messages[:height] = " You are not tall enough to ride the #{attraction.name}"
         end
-        #if self.user.tickets < self.attraction.tickets
-        #     error = error + "Sorry. You do not have enough tickets to ride the #{self.attraction.name}."
-        #end
-        #if self.user.height < self.attraction.min_height
-        #    errormsg = "You are not tall enough to ride the #{self.attraction.name}."
-        #    if error.empty?
-        #        error = "Sorry. " + errormsg
-        #    else
-        #        error = error + " " + errormsg
-        #    end
-        #end
 
         if errors.empty?
             @user = User.find_by(:id => self.user.id)
@@ -32,11 +19,13 @@ class Ride < ActiveRecord::Base
             @user.save
             "Thanks for riding the #{attraction.name}!"
         else
-            binding.pry
-            error.messages.each do |m|
-                puts m.key
+            if errors.messages[:tickets].empty?
+                "Sorry." + errors.messages[:height]
+            elsif errors.messages[:height].empty?
+                "Sorry." + errors.messages[:tickets]
+            else
+                "Sorry." + errors.messages[:tickets] + errors.messages[:height]
             end
-           "Sorry." + errors.messages[:tickets] + errors.messages[:height]
         end
     end
 end
