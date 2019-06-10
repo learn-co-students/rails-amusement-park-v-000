aee144029500677bd25f677a39cb596c9342ffb0
terryblue99 <terryblue@comcast.net>
2019-06-10 14:51:20 -0500

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -3,14 +3,13 @@ class RidesController < ApplicationController
   def create
     @ride = Ride.new(ride_params)
     ride_result = @ride.take_ride
-    if ride_result = "successful ride" && @ride.save
-      debugger
+    if ride_result == true
       flash[:successful_ride] = "Thanks for riding the #{Attraction.find(params[:ride][:attraction_id]).name}!"
-      redirect_to user_path(User.find(params[:ride][:user_id]))
+      redirect_to user_path(current_user)
     else
       debugger
-      flash.now[:failed_ride] = ride_result
-      redirect_to user_path(User.find(params[:ride][:user_id]))
+      flash[:failed_ride] = ride_result
+      redirect_to user_path(current_user)
     end
   end
 
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -8,8 +8,6 @@ class Ride < ActiveRecord::Base
       self.user.happiness+= self.attraction.happiness_rating
       self.user.tickets -= self.attraction.tickets
       self.user.save
-      debugger
-      "successful ride"
     else
       if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
         "Sorry. You do not have enough tickets to ride the #{self.attraction.name}. You are not tall enough to ride the #{self.attraction.name}."
