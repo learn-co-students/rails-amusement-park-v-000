6e4c2d67a2e704730cc11e31e149b1f364d6ead5
RonSala <ron.sala@gmail.com>
2019-06-12 12:57:14 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,52 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
-    render 'users/new'
+    @user = User.new
   end
 
   def create
-    
+    @user = User.new(user_params)
+# byebug
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:notice] = "user deleted."
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
   end
 end
