914ef12857de4f08adefc824060fac52edd043df
RonSala <ron.sala@gmail.com>
2019-06-13 19:00:01 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-# byebug
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user
     else
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'users_controller/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-
-  # get '/', to: 'application#home'
-
   root 'application#home'
+  get 'users_controller/new'
+  get '/session', to: 'users#delete'
+  resources :users
 end
