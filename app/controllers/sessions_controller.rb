6a82f2bb9bf98e963ee013dfbb43481b21c1c16a
RonSala <ron.sala@gmail.com>
2019-06-16 22:21:31 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,10 @@
 class SessionsController < ApplicationController
   def new
+    @users = User.all
+    render '/sessions/signin'
+  end
+
+  def create
     @user = User.find_by(name: params[:user_name])
     session[:user_id] = @user.id
     redirect_to @user
