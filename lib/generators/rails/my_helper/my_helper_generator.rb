b6f11729cab3514fe096c66c5057a94c12acbc70
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-21 16:01:13 -0400

    Create helper generator

---
+++ lib/generators/rails/my_helper/my_helper_generator.rb
@@ -0,0 +1,9 @@
+class Rails::MyHelperGenerator < Rails::Generators::NamedBase
+  def create_helper_file
+    create_file "app/helpers/#{file_name}_helper.rb", <<-FILE
+module #{class_name}Helper
+  attr_reader :#{plural_name}, :#{plural_name.singularize}
+end
+    FILE
+  end
+end
