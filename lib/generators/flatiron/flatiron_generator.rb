48255ddaa98e27ef32c012bd3845dc32535c086f
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-21 15:51:31 -0400

    Create copy at core_extensions.rb via generator

--- lib/generators/flatiron/flatiron_generator.rb
+++ lib/generators/flatiron/flatiron_generator.rb
@@ -1,3 +1,7 @@
 class FlatironGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
+
+  def copy_flatiron_file
+    copy_file "flatiron.rb", "config/flatiron/#{file_name}.rb"
+  end
 end
