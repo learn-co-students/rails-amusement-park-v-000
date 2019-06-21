03760638d2bc17d0e4cb719c128ddf66b4763a96
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-21 16:40:30 -0400

    Run scaffold generator for Rides class

---
+++ db/migrate/20190621203654_create_rides.rb
@@ -0,0 +1,13 @@
+class CreateRides < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rides do |t|
+      t.string :name
+      t.integer :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
+
+      t.timestamps
+    end
+  end
+end
