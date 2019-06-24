2f8edd77249bf7e557b1ff5d1facb49389181935
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-24 19:25:46 -0400

    Code out sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,27 @@
 class SessionsController < ApplicationController
   def home
-    
+    # Placeholder for home page
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    if params[:name] == nil || params[:name].empty?
+      redirect_to login_path
+    else
+      @user = User.find_by(name: params[:name])
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        redirect_to login_path
+      end
+    end
+  end
+
+  def destroy
+    session.delete :name
   end
 end
