c19391297be8d0457c83998e900e00f6b125d1aa
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-27 16:25:14 -0400

    Add login requirement check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    session[:user_id]
+  end
+
+  private
+    def require_login
+      redirect_to root_path unless session.include? :user_id
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_login, only: [:show]
 
   # GET /users
   def index
