ed81fdc7d1ca5eda96fb36d8db7705bcf863286a
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-28 13:27:05 -0400

    Scope user#mood method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   has_many :attractions, through: :rides
 
   def mood
-    self.nausea > self.happiness ? "sad" : "happy"
+    if !admin?
+      nausea > happiness ? "sad" : "happy"
+    end
   end
 end
