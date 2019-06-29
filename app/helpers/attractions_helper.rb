bc34d27e007d05a5958823ab51f3df9bf649360e
Meg Gutshall <meghan.gutshall@gmail.com>
2019-06-29 15:41:37 -0400

    Create helper to display attractions table

--- app/helpers/attractions_helper.rb
+++ app/helpers/attractions_helper.rb
@@ -1,2 +1,9 @@
 module AttractionsHelper
+  def table_display(attraction)
+    if current_user.admin
+      "Show #{attraction.name}"
+    else
+      "Go on #{attraction.name}"
+    end
+  end
 end
