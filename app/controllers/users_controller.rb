78bed84d8cdfd527b93a62121979121c19662afe
RonSala <ron.sala@gmail.com>
2019-06-29 22:17:40 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    byebug
+    # byebug
     @user = User.find(params[:id])
 
     @user.update(user_params)
@@ -64,3 +64,9 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
   end
 end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :password_digest, :nausea, :happiness, :tickets, :height, :admin)
+  end
