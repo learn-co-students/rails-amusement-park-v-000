5e0b30caca6ee7e5f4583984e3edd6e04dddd47c
Meg Gutshall <meghan.gutshall@gmail.com>
2019-07-05 13:19:18 -0400

    Add helper method in application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :admin_only
 
   def current_user
     current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
