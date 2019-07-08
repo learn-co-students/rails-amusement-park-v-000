76cc1dfb7d17fa962b47460f2d3743473250c6f2
christianavdm <christianavdm@gmail.com>
2019-07-08 14:32:42 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,10 @@ class SessionsController < ApplicationController
       @users = User.all
     end
   
-    def create
-      return redirect_to(controller: 'sessions',
-                         action: 'new') if !params[:name] || params[:name].empty?           
-      @user = User.find_by(name: params[:name])
+    def create          
+      @user = User.find(params[:user][:name])
       session[:user_id] = @user.id
-      redirect_to controller: 'application', action: 'hello'
+      redirect_to user_path(@user)
     end
   
     def destroy
