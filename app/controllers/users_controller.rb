f53ed516d462c2a8a36a0baad3876fb065517dc7
christianavdm <christianavdm@gmail.com>
2019-07-08 12:37:26 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+    def new 
+        @user = User.new
+    end
+
+    def create 
+        @user = User.new(user_params)
+        @user.save
+
+        redirect_to :root
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height, :admin)
+    end
 end
