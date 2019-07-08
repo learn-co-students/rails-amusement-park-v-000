b7beae6427fea54b4b637b76064242b96ca47c05
christianavdm <christianavdm@gmail.com>
2019-07-08 12:22:39 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
     has_many :rides
     has_many :attractions, through: :rides
 
+    validates :password, presence: true
+
     def mood
         if self.nausea > self.happiness
             "sad" 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :attractions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 end
