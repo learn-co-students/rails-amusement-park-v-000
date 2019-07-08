516d0908200bd8a40b854bc6daaa5bae1b49ac30
christianavdm <christianavdm@gmail.com>
2019-07-08 12:52:39 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         @user.save
-
+        session[:user_id] = @user.id
         redirect_to user_path(@user)
     end
 
