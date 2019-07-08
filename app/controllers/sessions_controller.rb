1b4bce5aeffcf19960a234685a8228f61dfef445
christianavdm <christianavdm@gmail.com>
2019-07-08 14:18:37 -0700

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,18 @@
 class SessionsController < ApplicationController
     def new
+      @users = User.all
     end
   
     def create
       return redirect_to(controller: 'sessions',
-                         action: 'new') if !params[:name] || params[:name].empty?
-      session[:name] = params[:name]
+                         action: 'new') if !params[:name] || params[:name].empty?           
+      @user = User.find_by(name: params[:name])
+      session[:user_id] = @user.id
       redirect_to controller: 'application', action: 'hello'
     end
   
     def destroy
-      session.delete :name
+      session.delete :user_id
       redirect_to controller: 'application', action: 'hello'
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :attractions, :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  post '/logout' => 'sessions#destroy'
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  post '/signout' => 'sessions#destroy'
 end
