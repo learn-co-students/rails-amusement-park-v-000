240cc9e0c046da31d0f5d0733ce89794da5c5eff
christianavdm <christianavdm@gmail.com>
2019-07-08 14:55:24 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,13 @@ class UsersController < ApplicationController
 
     def show 
         @user = User.find(params[:id])
+        if session[:user_id] != @user.id 
+            redirect_to :root
+        else 
+        end
+    end
+
+    def hello
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :attractions, :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#hello'
   get '/signin' => 'sessions#new'
   post '/signin' => 'sessions#create'
   post '/signout' => 'sessions#destroy'
