80e0bdcae0f03cf2ab59d22330f3acb29d3dc1c7
christianavdm <christianavdm@gmail.com>
2019-07-08 18:09:17 -0700

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -7,4 +7,18 @@ class AttractionsController < ApplicationController
     def show 
         @attraction = Attraction.find(params[:id])
     end
+
+    def new 
+        @attraction = Attraction.new
+    end
+
+    def create 
+        @attraction = Attraction.new(attraction_params)
+        @attraction.save
+        redirect_to attraction_path(@attraction)
+    end
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :password, :nausea_rating, :happiness_rating, :tickets, :min_height)
+    end
 end
