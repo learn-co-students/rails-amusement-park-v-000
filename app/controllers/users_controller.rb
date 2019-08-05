18886f3c06de005fcd9caf46ed8d4c17144d9dee
BusterStatus <matthew.serre@gmail.com>
2019-08-04 17:26:59 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :password, :password_confirmation, :nausea, :happiness, :tickets, :height)
+        params.require(:user).permit(:name, :password, :nausea, :happiness, :tickets, :height)
     end
 
 end
