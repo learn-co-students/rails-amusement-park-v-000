85d5ea31d0c78f77a6a61fd03b83ab2127b0e805
Monica Marziano <mmarziano1@gmail.com>
2019-08-05 17:49:48 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def index 
+    render 'welcome/index'
+  end 
+  
+
 end
