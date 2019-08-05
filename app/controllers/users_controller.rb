9241adce7bf77b96ead49d61e7734ca06623f4f0
BusterStatus <matthew.serre@gmail.com>
2019-08-04 17:02:01 -0700

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+    def index
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        redirect_to user_path(@user)
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation, :nausea, :happiness, :tickets, :height)
+    end
+
+end
+
---
+++ db/migrate/20190804231854_create_rides.rb
@@ -0,0 +1,15 @@
+class CreateRides < ActiveRecord::Migration[5.2]
+  def change
+    create_table :rides do |t|
+      t.string :name
+      t.integer :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.integer :min_height
+      t.belongs_to :user, foreign_key: true
+      t.belongs_to :attraction, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
