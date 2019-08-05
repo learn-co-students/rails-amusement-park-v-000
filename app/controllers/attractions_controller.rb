c1817dbfa92c653e2cf4a54fa7b07be72b273ffc
BusterStatus <matthew.serre@gmail.com>
2019-08-05 12:37:56 -0700

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,6 +1,27 @@
 class AttractionsController < ApplicationController
 
     def index
+        @attractions = Attraction.all
+    end
+
+    def show
+        @attraction = Attraction.find(params[:id])
+    end
+
+    def update
+        user = User.find(session[:user_id])
+        @ride = Ride.create(user_id: user.id, attraction_id: params[:id])
+        @ride.take_ride
+        if @ride.user.height < @ride.attraction.min_height && @ride.user.tickets < @ride.attraction.tickets
+            flash[:notice] = "You are not tall enough to ride the #{@ride.attraction.name} You do not have enough tickets to ride the #{@ride.attraction.name}"
+        elsif @ride.user.height < @ride.attraction.min_height
+            flash[:notice] = "You are not tall enough to ride the #{@ride.attraction.name}"
+        elsif @ride.user.tickets < @ride.attraction.tickets
+            flash[:notice] = "You do not have enough tickets to ride the #{@ride.attraction.name}"
+        else
+            flash[:notice] = "Thanks for riding the #{@ride.attraction.name}!"
+        end
+        redirect_to user_path(@ride.user)
     end
 
 end
--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -1,4 +1,19 @@
 class Attraction < ApplicationRecord
     has_many :rides
     has_many :users, through: :rides
+
+    def take_ride
+        if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+          "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
+        elsif self.user.height < self.attraction.min_height
+          "Sorry. You are not tall enough to ride the #{attraction.name}."
+        elsif self.user.tickets < self.attraction.tickets
+          "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+        elsif self.user.tickets > self.attraction.tickets
+          self.user.tickets -= self.attraction.tickets
+          self.user.nausea += self.attraction.nausea_rating
+          self.user.happiness += self.attraction.happiness_rating
+          self.user.save
+        end
+      end
 end
