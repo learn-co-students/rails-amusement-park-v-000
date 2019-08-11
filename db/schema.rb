dc69a701bbffbfbdeb594e82309c0d01d077cb68
Kristin Ortiz <itskristin.ortiz@gmail.com>
2019-08-11 09:22:41 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_09_191646) do
+ActiveRecord::Schema.define(version: 2019_08_11_160928) do
 
   create_table "attractions", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 2019_08_09_191646) do
     t.integer "tickets"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rides", force: :cascade do |t|
+    t.integer "attraction_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["attraction_id"], name: "index_rides_on_attraction_id"
+    t.index ["user_id"], name: "index_rides_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
