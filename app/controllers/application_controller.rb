5d89058d19ef6e4d1ec5e4021c6d8ac3024a76eb
Nate Dietz <nsdietz@gmail.com>
2019-08-13 00:20:36 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def authentication_required
-    if !logged_in?
-      redirect_to root_path
-    end
-  end
-
   def logged_in?
     session[:user_id]
   end
