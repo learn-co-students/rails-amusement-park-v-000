47cc2f0a6804d035c464dfc02129c0e8866607d7
Terry-Thompson <terry.lyn.thompson@gmail.com>
2019-08-15 12:48:27 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :attractions
   resources :users
   get '/', to: 'application#home'
-  get '/signing', to: 'users#new'
+  get '/signin', to: 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
