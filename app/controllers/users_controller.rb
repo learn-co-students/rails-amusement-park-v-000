dd5aa519c8e1819fa0006c365b6855f7b093de3f
Jessica Romans <RomansHomeComputer@Jessicas-iMac.local>
2019-08-20 15:33:40 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   
   def create
       @user = User.new(user_params)
+    #   binding.pry
       if @user.save
           session[:user_id] = @user.id
           redirect_to user_path(@user)
