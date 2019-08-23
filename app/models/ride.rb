a7132b6dee8d8c062d1c9321294cc5c310aa814b
dhintz <dhintz89@gmail.com>
2019-08-22 21:26:35 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,7 @@
 class Ride < ActiveRecord::Base
+    belongs_to :user
+    belongs_to :attraction
+
+    def take_ride
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+    has_many :rides
+    has_many :attractions, through: :rides
+    has_secure_password
+
+    def mood
+        if self.happiness < self.nausea
+            "sad"
+        else
+            "happy"
+        end
+    end
+
+    # make admin default to false in new/edit form
 end
