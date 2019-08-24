b3bb0972bb33a60512a37eb659a4474c2126c418
dhintz <dhintz89@gmail.com>
2019-08-23 22:01:06 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,5 +3,22 @@ class Ride < ActiveRecord::Base
     belongs_to :attraction
 
     def take_ride
+        error = ["Sorry."]
+        if self.attraction.tickets > self.user.tickets
+            error << ["You do not have enough tickets to ride the #{self.attraction.name}."]
+        end
+        if self.attraction.min_height > self.user.height
+            error << "You are not tall enough to ride the #{self.attraction.name}."
+        end
+
+        if error.length > 1
+            error.join(' ')
+        else
+            self.user.tickets -= self.attraction.tickets
+            self.user.happiness += self.attraction.happiness_rating
+            self.user.nausea += self.attraction.nausea_rating
+            self.user.save
+        end
+
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
+  get '/users/:id' => 'users#show'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+  root 'users#welcome'
 end
