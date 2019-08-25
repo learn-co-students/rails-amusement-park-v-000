3e4a644a87f78c60712b5f6297c26138527ba9ef
github username <github email address>
2019-08-25 07:28:09 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
+  get '/signup' => 'users#new'
+  post '/signup' => 'user#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  delete '/signout' => 'sessions#destroy'
+
+  post '/ride' => 'rides#create'
+
+  resources :rides
+  resources :attractions
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ spec/controllers/attractions_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe AttractionsController, type: :controller do
+
+end
