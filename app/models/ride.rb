76d7fd858d27e20b2a21a231266c22ced2be9ed8
Paul Fell <swellpf@gmail.com>
2019-09-13 17:29:36 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,18 +2,24 @@ class Ride < ActiveRecord::Base
     belongs_to :user
     belongs_to :attraction
 
+    def new
+    end
+
+    def create
+        @ride = Ride.new
+    end
 
     def take_ride
-        if @user.tickets < ride.tickets
+        if @user.tickets < @ride.tickets
             "Sorry. You do not have enough tickets to ride the #{attraction.name}."
             elsif
-                @user.height < ride.height 
+                @user.height < @ride.height 
                 "Sorry. You are not tall enough to ride the #{attraction.name}."
-            elsif @user.tickets < ride.tickets && @user.height < ride.height
+            elsif @user.tickets < @ride.tickets && @user.height < @ride.height
                 "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
                 else
                     binding.pry
-                    @user.tickets -= ride.tickets
+                    @user.tickets -= attraction.tickets
                     @user.happiness += attraction.happiness_rating
                     @user.nausea += attraction.nausea_rating
                 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'signin' => 'users#create'
   root 'application#hello'
   resources :users
+  resources :rides
   # resources :attractions
 end
