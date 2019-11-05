de2236d9b0ffeef42b3034e7a7742f5f5b334fc7
Scott Silverman <scottms724@gmail.com>
2019-11-05 16:13:56 +0000

    Done.

--- app/helpers/attractions_helper.rb
+++ app/helpers/attractions_helper.rb
@@ -1,2 +1,22 @@
 module AttractionsHelper
+  def display_new_attraction_link
+    if @user.admin
+      link_to("/attractions/new"){"New Attraction"}
+    end
+  end
+
+  def display_attraction_link(attraction)
+    if @user.admin
+      link_to("Show #{attraction.name}", "/attractions/#{attraction.id}")
+    else
+      link_to("Go on #{attraction.name}", "/attractions/#{attraction.id}")
+    end
+  end
+
+
+  def edit_attraction
+    if @user.admin
+      link_to("Edit Attraction", edit_attraction_path)
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ActiveRecord::Base
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  validates :name, presence: true
+  has_secure_password
+
+  def mood
+    if self.has_attributes?
+      self.nausea > self.happiness ? "sad" : "happy"
+    else
+      "unavailable"
+    end
+  end
+
+  def has_attributes?
+    self.nausea && self.happiness
+  end
 end
