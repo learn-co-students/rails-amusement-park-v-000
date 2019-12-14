4808fe1dda780040025f2b82d8a3ddb4f68c8949
GiancarloZ <gzabaleta48@gmail.com>
2019-12-14 16:51:19 -0500

    Done.

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,46 @@
+class AttractionsController < ApplicationController
+    def index
+        @attractions = Attraction.all
+      end
+    
+      def show
+        @attraction = Attraction.find(params[:id])
+      end
+    
+      def ride
+        flash[:alert] = Ride.create(ride_params).take_ride
+    
+        redirect_to user_path(current_user)
+      end
+    
+      def new
+        @attraction = Attraction.new
+      end
+    
+      def create
+        attraction = Attraction.create(attraction_params)
+        redirect_to attraction_path(attraction)
+      end
+    
+      def edit
+        @attraction = Attraction.find(params[:id])
+      end
+
+      def update
+        attraction = Attraction.find(params[:id])
+        attraction.update(attraction_params)
+        redirect_to attraction_path(attraction)
+      end
+
+    
+      private
+    
+      def ride_params
+        params.permit(:user_id, :attraction_id)
+      end
+    
+      def attraction_params
+        params.require(:attraction).permit(:name, :min_height,
+                                          :happiness_rating, :nausea_rating, :tickets)
+      end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+
+    def signin
+    end
+  
+    def login
+      user = User.find_by(name: params[:user][:name])
+      if user
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        redirect_to signin_path
+      end
+    end
+  
+    def logout
+      session.delete(:user_id)
+      redirect_to root_path
+    end
+  
+    def new
+      @user = User.new
+    end
+  
+    def show
+      return redirect_to root_path unless logged_in?
+      @user = User.find_by(id: params[:id])
+    end
+  
+    def create
+      User.create(user_params)
+      session[:user_id] = User.last.id
+      redirect_to user_path(current_user)
+    end
+  
+    def update
+      user = User.find(params[:id])
+      user.update(user_params)
+      redirect_to user_path(user)
+    end
+  
+    def edit
+      @user = User.find_by(id: params[:id])
+    end
+  
+   
+    private
+ 
+    def user_params
+      params.require(:user).permit(:name, :password, :password_confirmation, :height, :happiness, :nausea, :tickets, :admin)
+    end
+ 
+  
+end
