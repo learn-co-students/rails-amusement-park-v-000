fe289aa36447034b60c259ace0f8b82b7a8eeb59
github username <github email address>
2019-12-29 13:00:52 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def index
+  end 
+
+  def loggin_in?
+    session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+
+  root 'application#index'
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy'
+
+  get 'rides/create'
+  
+  resources :users, only:[:index, :new, :create, :show]
+  resources :attractions
+  resources :rides, only:[:create]
+
+
+  
+ 
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
