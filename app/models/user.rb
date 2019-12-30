5466bf8dff4badfdcc4b0ba294ab86efc7b291e5
Tina Hang <mstinahang@gmail.com>
2019-12-30 04:57:59 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
+  has_many :rides
+  has_many :attractions, through: :rides
+  has_secure_password
+
+  def mood
+    if nausea > happiness
+      "sad"
+    elsif nausea < happiness
+      "happy"
+    end
+  end
 end
