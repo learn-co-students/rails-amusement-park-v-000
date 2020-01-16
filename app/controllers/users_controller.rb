e54e2c6a6a8d0852460aa7fc6a4584951d37945d
MisterSenor <nathan.visak@gmail.com>
2020-01-16 11:16:56 -0600

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def home
+    render "/welcome"
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to user_path(@user)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :height, :nausea, :happiness, :tickets, :password)
+    end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :rides
   has_many :attractions, through: :rides
+  
+
+
 
   def mood
     if self.nausea > self.happiness
