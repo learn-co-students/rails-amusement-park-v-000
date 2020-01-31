24159829465a8a893ee39cf61b2af01687b04ad4
sdcro <sdcrouse91@gmail.com>
2020-01-31 01:28:55 -0800

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -2,12 +2,7 @@ class AttractionsController < ApplicationController
   before_action :user_must_be_logged_in # Defined in ApplicationController
 
   def index
-    # if logged_in?
-      @attractions = Attraction.all
-    # else
-      # flash.alert = "You must be logged in to view this page."
-      # redirect_to root_path
-    # end
+    @attractions = Attraction.all
   end
 
   def show
