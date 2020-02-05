6b1ef094065091f01a3fbc86aa82f3ce795c99ac
NathalieSantana <nathalie.scruz@gmail.com>
2020-02-05 07:56:48 -0500

    Done.

---
+++ app/controllers/rides_controller.rb
@@ -0,0 +1,9 @@
+class RidesController < ApplicationController
+    def create
+        @attraction = Attraction.find_by(id: params[:attraction_id])
+        @ride = current_user.rides.create(attraction: @attraction)
+        flash[:message] = @ride.take_ride
+        redirect_to user_path(@ride.user, :message => @message)
+    end
+
+end
