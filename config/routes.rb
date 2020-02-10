82be88441d00ac722959dec89e836e9d468b2c46
gigi <mmgamao1@gmail.com>
2020-02-10 12:14:40 -0600

    feature specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
- # resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show]
  
-  root to: 'application#home'
+ # root to: 'application#home'
   root to: 'users#index'
   
 
