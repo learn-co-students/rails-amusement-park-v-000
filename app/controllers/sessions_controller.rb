1da9456d40f8a5b842810154e17d6bdf8b87fa35
Paul Fell <swellpf@gmail.com>
2020-02-14 11:33:29 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+        raise params.inspect
+        return redirect_to(controller: 'sessions', action: 'new') if !params[:name] || params[:name].empty?
         session[:name] = params[:name]
         redirect to '/'
     end
 
     def destroy
-        session.delete :user_id
+        session.delete :name
         redirect_to controller: 'sessions', action: 'new'
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post 'logout' => 'sessions#destroy'
-  get 'signup' => 'users#new'
-  get 'signin' => 'sessions#new'
-  post 'signin' => 'sessions#create'
   root 'application#hello'
+  get '/signin' => 'sessions#new'
+  post '/logout' => 'sessions#destroy'
+  post '/signin' => 'sessions#create'
+  get '/signup' => 'users#new'
   resources :users
   resources :rides
   resources :attractions
