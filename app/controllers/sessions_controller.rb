9cba2549f7747eec36eaab52f2f9881420961354
Paul Fell <swellpf@gmail.com>
2020-02-17 21:13:08 -0500

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
 
     def create
-      @user = User.find_by(name: params[:name])
+      @user = User.find_by(id: params[:id])
       if @user && user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to user_path(@user)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'application#hello'
   get '/signin' => 'sessions#new'
-  post '/logout' => 'sessions#destroy'
+  get '/logout' => 'sessions#destroy'
   post '/signin' => 'sessions#create'
   get '/signup' => 'users#new'
   resources :users
