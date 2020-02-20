76bf5d35c0eca47d9106d065bce287a7120d755b
Paul Fell <swellpf@gmail.com>
2020-02-19 19:50:06 -0500

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -4,15 +4,30 @@ def index
     @attractions = Attraction.all
 end
 
-def show(attraction)
-    @attraction = Attraction.find_by(attraction.id)
+def show
+   # raise params.inspect
+    @attraction = Attraction.find_by(id: params[:id])
 end
 
 def new
+    @attraction = Attraction.new
 end
 
 def create
-
+  #  raise params.inspect
+  @attraction = Attraction.new(attraction_params)
+  if @attraction.save 
+              redirect_to attraction_path(@attraction)
+          else
+              render 'new'
+          end
+    
 end
 
+
+private
+
+    def attraction_params
+        params.require(:attraction).permit(:name, :nausea_rating, :happiness_rating, :min_height, :tickets)
+    end
 end
