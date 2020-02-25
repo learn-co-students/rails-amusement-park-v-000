a4e449285a423fa30672df51e8195f28850e0619
Paul Fell <swellpf@gmail.com>
2020-02-24 20:28:44 -0500

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,6 +6,9 @@ class RidesController < ApplicationController
     def create
        raise params.inspect
         @ride = Ride.new(ride_params)
+        @attraction = Attraction.find_by(id: params[:ride][:attraction])
+        @user = current_user
+        binding.pry
         @ride.take_ride
     end
 
