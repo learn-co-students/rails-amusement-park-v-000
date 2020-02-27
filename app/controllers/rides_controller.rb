9ceb3817643e2cffb91ee5d669655b9cf76b9014
Paul Fell <swellpf@gmail.com>
2020-02-26 19:48:25 -0500

    Done.

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -11,8 +11,8 @@ class RidesController < ApplicationController
 
     def update
        # binding.pry
-        # @ride = Ride.new
-        
+         @ride = Ride.new
+
         @ride.attraction_id = params[:ride][:attraction_id]
         @ride.user_id = params[:ride][:user_id]
         @ride.take_ride
