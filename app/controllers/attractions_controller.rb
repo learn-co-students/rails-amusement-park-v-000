cae398368f4856a508912894628da338368ca6ff
gigi <mmgamao1@gmail.com>
2020-03-01 17:42:41 -0600

    Done.

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,5 +1,9 @@
 class AttractionsController < ApplicationController
 
+  def new
+    @attraction = Attraction.new
+  end
+
   def index
     @attractions = Attraction.all
   end
@@ -16,12 +20,23 @@ class AttractionsController < ApplicationController
 
   def update
     @attraction = Attraction.find_by(params[:id])
-    @attraction.update(user_params)
+    @attraction.update(attraction_params)
     redirect_to attraction_path(@attraction)
   end
 
   def edit
     @attraction = Attraction.find_by(params[:id])
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(@attraction)
+  end
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :happiness_rating, :nausea_rating, :tickets)
   end
 
 end
