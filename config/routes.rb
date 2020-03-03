b85b759ad8ed9eec92b777f54b6f68230552eedd
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-03-03 14:25:54 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root :to => 'welcome#index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
   resources :users, only: [:index, :show, :new, :create, :edit, :update]
   resources :attractions, only: [:index, :show, :new, :create, :edit, :update]
 
