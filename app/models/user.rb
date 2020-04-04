0d578d3e361c499fa4a1be801aeec259f24abc1e
iamtamtamiam <vutam_a@hotmail.com>
2020-04-04 15:38:05 -0500

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+    has_many :rides
+    has_many :attractions, through: :rides
+    has_secure_password
+
+    def mood
+        if self.nausea > self.happiness
+            mood = "sad"
+        else
+            mood = "happy"
+        end 
+    end 
+
 end
---
+++ db/migrate/20200404200537_add_attraction_id_to_ride.rb
@@ -0,0 +1,5 @@
+class AddAttractionIdToRide < ActiveRecord::Migration[5.2]
+  def change
+    add_column :rides, :attraction_id, :integer
+  end
+end
