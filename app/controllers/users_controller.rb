314e82efc11348496bcd7544d9db8a1cdb6b9046
Amil <github email address>
2020-04-13 10:11:14 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    if (user = User.create(user_params))
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    if (@user = User.create(user_params))
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+      
     else
       render :new
     end
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    user = User.find_by(id: params[:id])
   end
 
   private
