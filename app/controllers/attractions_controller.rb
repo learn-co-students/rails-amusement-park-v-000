398720a27b94332c866a5610a5ebdbaa732039de
Amil <github email address>
2020-04-25 06:43:15 -0400

    Done.

---
+++ app/controllers/attractions_controller.rb
@@ -0,0 +1,24 @@
+class AttractionsController < ApplicationController
+  def index
+    @attractions = Attraction.all
+  end
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.create(attraction_params)
+    redirect_to attraction_path(attraction)
+  end
+
+  def show
+    @attraction = Attraction.find_by(id: params[:id])
+  end
+
+
+  private
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :min_height, :tickets, :happiness_rating, :nausea_rating)
+  end
+end
