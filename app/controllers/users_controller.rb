a3b7c6694c80c5ab27278d26b9e5445dc0b89a8a
Lynie McClimon <vmcclimon33@yahoo.com>
2020-05-27 13:03:42 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def user_params
+  end
+
+end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,16 +3,21 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-    if self.attraction.tickets > self.user.tickets && self.attraction.min_height > self.user.height
+    attraction = self.attraction
+    user = self.user
+
+    if attraction.tickets > user.tickets && attraction.min_height > user.height
       "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
-    elsif self.attraction.tickets > self.user.tickets
+    elsif attraction.tickets > user.tickets
       "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-    elsif self.attraction.min_height > self.user.height
+    elsif attraction.min_height > user.height
       "Sorry. You are not tall enough to ride the #{attraction.name}."
     else
-      self.user.update(tickets: self.user.tickets - self.attraction.tickets,
-      nausea: self.user.nausea + self.attraction.nausea_rating,
-      happiness: self.user.happiness + self.attraction.happiness_rating)
+      current_tickets = user.tickets - attraction.tickets
+      current_nausea = user.nausea + attraction.nausea_rating
+      current_happiness = user.happiness + attraction.happiness_rating
+
+      user.update(tickets: current_tickets, nausea: current_nausea, happiness: current_happiness)
    end
   end
 end
