2621c86ed30f22092a652fd8aa1eac79619ad72e
lrh2daizzo <herrera.lrh@gmail.com>
2020-06-10 16:01:23 -0500

    Done.

---
+++ db/migrate/20200609022156_create_attractions.rb
@@ -0,0 +1,14 @@
+class CreateAttractions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :attractions do |t|
+      t.string :name
+      t.integer :tickets
+      t.integer :nausea_rating
+      t.integer :happiness_rating
+      t.string :min_height
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
