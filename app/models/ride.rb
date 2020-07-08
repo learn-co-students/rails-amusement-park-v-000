ae26a8cfd4cd1fe5fb284076655aadca5d8ca1fa
Argonity <maurice.argoetti@gmail.com>
2020-07-07 20:14:30 -0400

    Done.

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,26 +1,17 @@
 class Ride < ActiveRecord::Base
-  belongs_to :user
   belongs_to :attraction
+  belongs_to :user
 
   def take_ride
-    attraction = self.attraction
-    user = self.user
-    
-    if attraction.tickets > user.tickets &&
-    attraction.min_height > user.height
+    if attraction.tickets > user.tickets && attraction.min_height > user.height
       "Sorry. You do not have enough tickets to ride the #{attraction.name}. You are not tall enough to ride the #{attraction.name}."
     elsif attraction.tickets > user.tickets
       "Sorry. You do not have enough tickets to ride the #{attraction.name}."
     elsif attraction.min_height > user.height
       "Sorry. You are not tall enough to ride the #{attraction.name}."
     else
-      current_tickets = user.tickets - attraction.tickets
-      current_nausea = user.nausea + attraction.nausea_rating
-      current_happiness = user.happiness + attraction.happiness_rating
-
-      user.update(tickets: current_tickets, nausea: current_nausea, happiness: current_happiness)
+      user.update(tickets: user.tickets -= attraction.tickets, nausea: user.nausea += attraction.nausea_rating, happiness: user.happiness += attraction.happiness_rating)
+      "Thanks for riding the #{attraction.name}!"
     end
   end
-
-
 end
