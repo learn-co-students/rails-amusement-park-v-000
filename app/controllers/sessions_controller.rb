8f7bb079d1b681ed41c8dd9b15bf758c6e729a51
osxgirl <osxgirl@mac.com>
2020-07-29 17:14:55 -0400

    route update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
    
    def welcome
    end
+   
 
    def create
      user = User.find_by_email(params[:email])
      if user && user.authenticate(params[:password])
        session[:user_id] = user.id
-       redirect_to root_url, notice: "Logged in!"
+       redirect_to "/users/index/", notice: "Logged in!"
      else
        flash.now[:alert] = "Email or password is invalid"
        render "/users/show/"
