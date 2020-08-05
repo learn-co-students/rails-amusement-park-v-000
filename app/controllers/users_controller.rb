05b3850b5fd361a4fd52da87caece53d1f771a14
yurigurlie207 <adrienne.yao@gmail.com>
2020-08-05 11:20:49 -0400

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+    skip_before_action :verified_user, only: [:new, :create]
+  
+    def new
+      @user = User.new
+    end
+  
+    def create
+      if (user = User.create(user_params))
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        render 'new'
+      end
+    end
+  
+    def show
+      @user = User.find_by(id: params[:id])
+    end
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(
+        :name,
+        :height,
+        :nausea,
+        :tickets,
+        :admin,
+        :password,
+        :happiness
+      )
+    end
+  end
---
+++ db/migrate/04_change_column.rb
@@ -0,0 +1,5 @@
+class ChangeColumn < ActiveRecord::Migration[5.2]
+  def change
+    change_column :users, :admin, :boolean, :default => false
+  end
+end
