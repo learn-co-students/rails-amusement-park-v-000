7fe4a51b51c1d628f894e8fcaf9018075ddfee05
Paul Fell <swellpf@gmail.com>
2021-03-25 17:49:43 -0400

    Corrected user authentication and added flash msg

--- Gemfile
+++ Gemfile
@@ -16,6 +16,11 @@ gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
+
+# Paul added the two gems below
+gem 'execjs'
+gem 'therubyracer'
+
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,11 @@ class SessionsController < ApplicationController
 
     def create
       @user = User.find_by(name: params[:user][:name])
-      if @user #&& @user.authenticate(params[:user][:password])
+      if @user.authenticate(params[:user][:password])
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
+        flash[:notice] = "Invalid username or password."
         render 'new'
       end
     end
