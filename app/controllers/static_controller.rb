03782db4bd976098c039eef2e46aa9f1356e1e5a
parisdolfman <parisdolfman@gmail.com>
2021-04-13 11:32:53 -0400

    Done.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
-    skip_before_action :verified_user, only: [:home]
-  
-    def home
-    end
+  skip_before_action :verified_user, only: [:home]
+
+  def home
   end
+end
