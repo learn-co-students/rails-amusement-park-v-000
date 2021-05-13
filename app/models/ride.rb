0e4621240ebb902c8102104cf24e85fe31892ba3
Jesse Wenger <wengerj@gmail.com>
2021-05-13 23:19:48 +0000

    create tables and models

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,2 +1,13 @@
 class Ride < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+
+      "Sorry. You do not have enough tickets to ride the #{attraction.name}."
+    end
+  end
+
+  def mood
+  end
 end
