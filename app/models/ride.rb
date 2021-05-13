7dbd178f46d0877df6f9cfcfeea4a20614a3d24f
Jesse Wenger <wengerj@gmail.com>
2021-05-13 23:37:10 +0000

    add db tables and models

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,9 +3,7 @@ class Ride < ActiveRecord::Base
   belongs_to :attraction
 
   def take_ride
-
       "Sorry. You do not have enough tickets to ride the #{attraction.name}."
-    end
   end
 
   def mood
