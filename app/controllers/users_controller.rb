fcbedf9b7d85d45ef3638500e991658ad726cdc4
Jesse Wenger <wengerj@gmail.com>
2021-05-14 17:10:22 +0000

    add views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def home
+  end
+
   def index
   end
 
@@ -6,5 +9,11 @@ class UsersController < ApplicationController
   end
 
   def create
+    #@user = User.new
+    #@user.save
+    #redirect_to user_path(@user.id)
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-  resources :users, only: [:new, :create]
+  root 'users#home'
+  resources :users, only: [:new, :show]
+  #get '/users/new', to: 'users#new'
+  post '/users/new', to: 'users#create'
 end
