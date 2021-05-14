13ebcb9607bd52dd9721538110b11822c058711d
Jesse Wenger <wengerj@gmail.com>
2021-05-14 17:22:15 +0000

    add static

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#home'
-  resources :users, only: [:new, :show]
+  root 'static#home'
+  resources :users, only: [:new]
   #get '/users/new', to: 'users#new'
-  post '/users/new', to: 'users#create'
+  #post '/users/new', to: 'users#create'
 end
